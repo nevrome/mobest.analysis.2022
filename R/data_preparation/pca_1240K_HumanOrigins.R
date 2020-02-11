d48a62693a2e784cd309b0a9a38071c49eeb942d
Clemens Schmid <clemens@nevrome.de>
2020-02-11 17:12:09 +0100

    pca result preparation

--- R/data_preparation/pca_1240K_HumanOrigins.R
+++ R/data_preparation/pca_1240K_HumanOrigins.R
@@ -23,25 +23,23 @@ readr::write_lines(c(
 
 system(paste("smartpca -p", file.path(data_path, "pca", paste0("pca.", dataset, ".params.txt"))))
 
-# #### read and modify smartpca output ####
-# 
-# pca <- readr::read_delim(
-#   file.path(data_path, "pca", paste0("pca.", dataset, ".evec")),
-#   " ", 
-#   trim_ws = T
-# )
-# 
-# eigenvalues <- colnames(pca)[2:(ncol(pca) - 1)]
-# colnames(pca) <- c("Name", paste0("PC", 1:10), "Group")
-# 
-# # save pca results
-# save(pca, file = "data/pca/pca_europe_result.RData") 
-# 
-# #### define and store static group dataset ####
-# static_groups <- readLines("data/population_lists/PCA_6.pops")
-# 
-# pca_static <- pca %>% dplyr::filter(
-#   Group %in% static_groups
-# )
-# 
-# save(pca_static, file = "data/pca_europe_result_static.RData") 
+#### read and modify smartpca output ####
+
+# smartpca output
+pca <- readr::read_delim(
+  file.path(data_path, "pca", paste0("pca.", dataset, ".evec")),
+  " ",
+  trim_ws = T
+)
+eigenvalues <- colnames(pca)[2:(ncol(pca) - 1)]
+colnames(pca) <- c("Name", paste0("PC", 1:10), "group")
+
+# reference groups
+reference_groups <- readLines(file.path(data_path, "population_lists", "PCA_6.pops"))
+pca <- pca %>%
+  dplyr::mutate(
+    reference_group = group %in% reference_groups
+  )
+
+# save pca results
+save(pca, file = file.path(data_path, "pca", "pca.1240K_HumanOrigins.RData"))
