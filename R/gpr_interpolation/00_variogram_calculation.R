c3e7636a538c65ea4c650dbf9ae781ff33e1934d
Clemens Schmid <clemens@nevrome.de>
2020-02-18 23:04:10 +0100

    further experiments with the variogram

--- R/gpr_interpolation/00_variogram_calculation.R
+++ R/gpr_interpolation/00_variogram_calculation.R
@@ -4,8 +4,6 @@ load("data/anno_1240K_and_anno_1240K_HumanOrigins_filtered.RData")
 anno <- anno_1240K_and_anno_1240K_HumanOrigins_filtered
 
 #### fit spatiotemporal covariance function ####
-anno_ordered <- anno %>% dplyr::arrange(calage_center)
-
 space <- anno %>% dplyr::select(x, y)
 sp::coordinates(space) <- ~x+y
 sp::proj4string(space) <- sp::CRS("+proj=aea +lat_1=43 +lat_2=62 +lat_0=30 +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs")
@@ -13,25 +11,40 @@ sp::proj4string(space) <- sp::CRS("+proj=aea +lat_1=43 +lat_2=62 +lat_0=30 +lon_
 
 stfdf <- spacetime::STIDF(
   sp = space,
-  time = lubridate::as_date("1970-01-01", "Y-M-D") + lubridate::dseconds(anno_ordered$calage_center),
+  time = lubridate::as_date("0000-01-01", "Y-M-D") + lubridate::dseconds(anno$calage_center),
   data = data.frame(PC1 = anno$PC1)
 )
 
+spacetime::stplot(stfdf)
+
 var <- gstat::variogramST(
   PC1~1, data = stfdf,
-  tunit = "secs",
+  tunit = "mins",
+  tlags = 1:10,
   assumeRegular = F,
   na.omit = T
 )
 
-plot(var)
 plot(var, map=F)
+plot(var)
 plot(var, wireframe=T) 
-# 
-# gstat::variogram(PC1~1, stfdf)
-# metric <- vgmST(
-#   "metric",
-#   joint = vgm(psill = 1,"Exp", range=5e3, nugget = 1e1),
-#   stAni = 1
+ 
+metric1 <- gstat::vgmST("metric", joint = gstat::vgm(0.01, "Exp", 1000000, 0), stAni = 100000)
+metric2 <- gstat::vgmST("metric", joint = gstat::vgm(0.01, "Exp", 500000, 0), stAni = 50000)
+
+pars.l <- c(sill.s = 0, range.s = 10, nugget.s = 0,sill.t = 0, range.t = 1, nugget.t = 0,sill.st = 0, range.st = 10, nugget.st = 0, anis = 0)
+metric_Vgm1 <- gstat::fit.StVariogram(var, metric1, method = "L-BFGS-B", lower = pars.l)
+metric_Vgm2 <- gstat::fit.StVariogram(var, metric2, method = "L-BFGS-B", lower = pars.l)
+
+attr(metric_Vgm1, "MSE")
+attr(metric_Vgm2, "MSE")
+
+plot(var, metric_Vgm1, map = F) 
+plot(var, metric_Vgm1, wireframe = T) 
+
+# plot(
+#   var,
+#   list(separable_Vgm, prodSumModel_Vgm, metric_Vgm, sumMetric_Vgm, SimplesumMetric_Vgm),
+#   all=T,
+#   wireframe=T
 # )
-# metric_Vgm <- fit.StVariogram(var, metric, method="L-BFGS-B",lower=pars.l,tunit="mins")
