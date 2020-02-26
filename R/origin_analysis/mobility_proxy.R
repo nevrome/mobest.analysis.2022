e6f6d1ec19d3abc4870ead40dc453f795ba99c2a
Clemens Schmid <clemens@nevrome.de>
2020-02-26 15:27:08 +0100

    first code to run the mobility estimation many times with different temporal resampling runs

--- R/origin_analysis/mobility_proxy.R
+++ R/origin_analysis/mobility_proxy.R
@@ -1,20 +1,46 @@
-load("data/pri_ready_spatial.RData")
+library(magrittr)
+
+load("data/gpr/gpr_pred_grid_temporal_sampling_v3.RData")
+load("data/pri_ready.RData")
 load("data/spatial/mobility_regions.RData")
 
-pri <- pri_ready_spatial %>%
-  sf::st_intersection(mobility_regions)
+points_regions <- pred_grid %>% 
+  dplyr::select(x_real, y_real, point_id) %>%
+  sf::st_as_sf( 
+    coords = c("x_real", "y_real"), 
+    crs = "+proj=aea +lat_1=43 +lat_2=62 +lat_0=30 +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs"
+  ) %>%
+  sf::st_intersection(mobility_regions) %>%
+  tibble::as_tibble() %>%
+  dplyr::select(-geometry)
+
+pri <- pri_ready %>%
+  dplyr::left_join(points_regions, by = "point_id")
 
 pri_mean <- pri %>%
   dplyr::group_by(
-    age_sample, region_id
+    independent_table_id, age_sample, region_id
   ) %>%
   dplyr::summarise(
-    mean_km_per_decade = mean(spatial_distance)/1000/10
+    mean_km_per_decade = mean(spatial_distance)/1000/10,
+    mean_angle = mean(circular::circular(angle_degree, type="angles", units="degrees",modulo="2pi", template='geographics'), na.rm = T)
   )
 
 library(ggplot2)
 pri_mean %>%
   ggplot() +
-  geom_line(aes(x = age_sample, y = mean_km_per_decade, group = region_id, color = region_id)) +
+  geom_line(
+    aes(x = age_sample, y = mean_km_per_decade, group = independent_table_id, color = region_id),
+    alpha = 0.1
+  ) +
+  facet_wrap(~region_id)
+
+pri_mean %>%
+  ggplot() +
+  coord_polar() +  
+  geom_point(
+    aes(x = mean_angle, y = age_sample, group = independent_table_id, color = age_sample),
+    alpha = 0.1
+  ) +
   facet_wrap(~region_id)
 
--- R/origin_analysis/spatial_origin.R
+++ R/origin_analysis/spatial_origin.R
@@ -1,27 +1,8 @@
 library(magrittr)
 
-load("data/gpr/pred_grid.RData")
-pri <- pred_grid %>%
-  dplyr::filter(
-    kernel_setting_id == "ds200_dt800_g01",
-    independent_table_id == "age_center"
-  ) %>%
+load("data/gpr/pred_grid_ind.RData")
+pri <- pred_grid_ind %>%
   tidyr::pivot_wider(names_from = "dependent_var_id", values_from = c("mean", "sd"))
-
-# reduce input grid to simplifiy visualization later
-# pri <- pred_grid_mean%>% dplyr::group_by(
-#   age_sample
-# ) %>%
-#   dplyr::filter(dplyr::row_number() %% 2 == 0) %>%
-#   dplyr::filter(dplyr::row_number() %% 2 == 0) %>%
-#   dplyr::ungroup()
-
-
-# pri %>%
-#   dplyr::select(
-#     x_real, y_real
-#   ) %>%
-#   unique %>% plot
 
 # add new columns for output dataset
 pri %<>%
@@ -37,61 +18,71 @@ pri %<>%
     y_real_origin = NA,
   )
 
-# split dataset by age slice
-time_pris <- pri %>% split(pri$age_sample)
+age_sample_run_pris <- pri %>% split(pri$independent_table_id)
 
-for (p1 in 2:length(time_pris)) {
+pri_ready_large <- pbapply::pblapply(age_sample_run_pris, function(age_sample_run_pri) {
+
+  # split dataset by age slice
+  time_pris <- age_sample_run_pri %>% split(age_sample_run_pri$age_sample)
   
-  # calculate spatial distance matrix between past and current points
-  current_pri_spatial <- as.matrix(time_pris[[p1]][c("x_real", "y_real")])
-  past_pri_spatial <- as.matrix(time_pris[[p1 - 1]][c("x_real", "y_real")])
-  spatial_distance <- fields::rdist(current_pri_spatial, past_pri_spatial)
-  
-  # calculate PCA distance matrix between past and current points
-  current_pri_genetics <- as.matrix(time_pris[[p1]][c("mean_PC1", "mean_PC2", "mean_PC3", "mean_PC4")])
-  past_pri_genetics <- as.matrix(time_pris[[p1 - 1]][c("mean_PC1", "mean_PC2", "mean_PC3", "mean_PC4")])
-  genetic_distance <- fields::rdist(current_pri_genetics, past_pri_genetics)
-  
-  # get points with least distance in the past
-  closest_point_indezes <- sapply(1:nrow(current_pri_genetics), function(x) { 
-    gendists <- genetic_distance[x,]
-    gendists[spatial_distance[x,] > 500000] <- NA
-    which.min(gendists) 
-  })
-  
-  # add closest points info to current age slice points
-  time_pris[[p1]] <- time_pris[[p1]] %>% dplyr::mutate(
-    mean_PC1_origin = time_pris[[p1 - 1]]$mean_PC1[closest_point_indezes],
-    mean_PC2_origin = time_pris[[p1 - 1]]$mean_PC2[closest_point_indezes],
-    mean_PC3_origin = time_pris[[p1 - 1]]$mean_PC3[closest_point_indezes],
-    mean_PC4_origin = time_pris[[p1 - 1]]$mean_PC4[closest_point_indezes],
-    x_real_origin = time_pris[[p1 - 1]]$x_real[closest_point_indezes],
-    y_real_origin = time_pris[[p1 - 1]]$y_real[closest_point_indezes]
-  )
-  time_pris[[p1]]$spatial_distance <- purrr::map2_dbl(1:817, closest_point_indezes, function(i, j) { spatial_distance[i, j] })
-  time_pris[[p1]]$genetic_distance <- purrr::map2_dbl(1:817, closest_point_indezes, function(i, j) { genetic_distance[i, j] })
-  
-  # get spatial position of entangled points
-  A <- as.matrix(time_pris[[p1]][c("x_real", "y_real")])
-  B <- as.matrix(time_pris[[p1]][c("x_real_origin", "y_real_origin")])
-  
-  # calculate angle between points
-  AB <- B - A
-  AC <- c(1, 0)
-  time_pris[[p1]]$angle <- sapply(
-    1:nrow(time_pris[[p1]]), function(i) {
-      if (time_pris[[p1]]$y_real_origin[i] < time_pris[[p1]]$y_real[i]) {
-        2*pi - matlib::angle(AB[i,], AC, degree = FALSE)
-      } else {
-        matlib::angle(AB[i,], AC, degree = FALSE)
+  for (p1 in 2:length(time_pris)) {
+    
+    # calculate spatial distance matrix between past and current points
+    current_pri_spatial <- as.matrix(time_pris[[p1]][c("x_real", "y_real")])
+    past_pri_spatial <- as.matrix(time_pris[[p1 - 1]][c("x_real", "y_real")])
+    spatial_distance <- fields::rdist(current_pri_spatial, past_pri_spatial)
+    
+    # calculate PCA distance matrix between past and current points
+    current_pri_genetics <- as.matrix(time_pris[[p1]][c("mean_PC1", "mean_PC2", "mean_PC3", "mean_PC4")])
+    past_pri_genetics <- as.matrix(time_pris[[p1 - 1]][c("mean_PC1", "mean_PC2", "mean_PC3", "mean_PC4")])
+    genetic_distance <- fields::rdist(current_pri_genetics, past_pri_genetics)
+    
+    # get points with least distance in the past
+    closest_point_indezes <- sapply(1:nrow(current_pri_genetics), function(x) { 
+      gendists <- genetic_distance[x,]
+      gendists[spatial_distance[x,] > 500000] <- NA
+      which.min(gendists) 
+    })
+    
+    # add closest points info to current age slice points
+    time_pris[[p1]] <- time_pris[[p1]] %>% dplyr::mutate(
+      mean_PC1_origin = time_pris[[p1 - 1]]$mean_PC1[closest_point_indezes],
+      mean_PC2_origin = time_pris[[p1 - 1]]$mean_PC2[closest_point_indezes],
+      mean_PC3_origin = time_pris[[p1 - 1]]$mean_PC3[closest_point_indezes],
+      mean_PC4_origin = time_pris[[p1 - 1]]$mean_PC4[closest_point_indezes],
+      x_real_origin = time_pris[[p1 - 1]]$x_real[closest_point_indezes],
+      y_real_origin = time_pris[[p1 - 1]]$y_real[closest_point_indezes]
+    )
+    time_pris[[p1]]$spatial_distance <- purrr::map2_dbl(1:817, closest_point_indezes, function(i, j) { spatial_distance[i, j] })
+    time_pris[[p1]]$genetic_distance <- purrr::map2_dbl(1:817, closest_point_indezes, function(i, j) { genetic_distance[i, j] })
+    
+    # get spatial position of entangled points
+    A <- as.matrix(time_pris[[p1]][c("x_real", "y_real")])
+    B <- as.matrix(time_pris[[p1]][c("x_real_origin", "y_real_origin")])
+    
+    # calculate angle between points
+    AB <- B - A
+    AC <- c(1, 0)
+    time_pris[[p1]]$angle <- sapply(
+      1:nrow(time_pris[[p1]]), function(i) {
+        if (time_pris[[p1]]$y_real_origin[i] < time_pris[[p1]]$y_real[i]) {
+          2*pi - matlib::angle(AB[i,], AC, degree = FALSE)
+        } else {
+          matlib::angle(AB[i,], AC, degree = FALSE)
+        }
       }
-    }
-  )
-  
-}
+    )
+    
+  }
 
-# rowbind distance table
-pri_ready <- time_pris[2:length(time_pris)] %>% do.call(rbind, .)
+  # rowbind distance table
+  pri_ready <- time_pris[2:length(time_pris)] %>% do.call(rbind, .)
+  
+  return(pri_ready)
+  
+}, cl = 8)
+
+pri_ready <- pri_ready_large %>% dplyr::bind_rows()
 
 # add angle in degrees
 pi_rad <- units::as_units(pri_ready$angle, "radians")
