f16752e21e9e86c0396db5b5ac16317d1ce2ccd7
Clemens Schmid <clemens@nevrome.de>
2020-02-26 11:34:55 +0100

    rescaling of plot

--- R/origin_analysis/mobility_proxy.R
+++ R/origin_analysis/mobility_proxy.R
@@ -9,12 +9,12 @@ pri_mean <- pri %>%
     age_sample, region_id
   ) %>%
   dplyr::summarise(
-    mean_spatial_distance = mean(spatial_distance)
+    mean_km_per_decade = mean(spatial_distance)/1000/10
   )
 
 library(ggplot2)
 pri_mean %>%
   ggplot() +
-  geom_line(aes(x = age_sample, y = mean_spatial_distance, group = region_id, color = region_id)) +
+  geom_line(aes(x = age_sample, y = mean_km_per_decade, group = region_id, color = region_id)) +
   facet_wrap(~region_id)
 
