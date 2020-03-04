884749c5b4ac7b6b69ea0de53c64b256cac12bfe
Clemens Schmid <clemens@nevrome.de>
2020-03-04 15:48:36 +0100

    adjusted third 3D plot: gpr

--- R/plot_scripts/3D_plot_gpr.R
+++ R/plot_scripts/3D_plot_gpr.R
@@ -3,23 +3,23 @@ library(ggplot2)
 
 load("data/anno_1240K_and_anno_1240K_HumanOrigins_filtered.RData")
 anno <- anno_1240K_and_anno_1240K_HumanOrigins_filtered
-load("data/gpr/pred_grid_ind.RData")
-pri <- pred_grid_ind %>%
-  tidyr::pivot_wider(names_from = "dependent_var_id", values_from = c("mean", "sd"))
-pri <- pri %>% dplyr::filter(
-  kernel_setting_id == "ds200_dt800_g01", independent_table_id == "age_sample_1"
-) %>%
+load("data/gpr/pred_grid_filled_grouped.RData")
+pri <- pred_grid_filled_grouped %>% dplyr::filter(
+    independent_table_type == "age_center", kernel_setting_id == "ds200_dt400_g01"
+  ) %>%
+  tidyr::pivot_wider(names_from = "dependent_var_id", values_from = c("mean", "sd")) %>%
   dplyr::select(x_real, y_real, age_sample, mean_PC1, sd_PC1)
 
-load("data/spatial/research_area.RData")
-raps <- sf::st_coordinates(sf::st_cast(research_area, "POINT"))[1:4,]/1000
+min_PC1 <- min(c(anno$PC1, pri$mean_PC1))
+max_PC1 <- max(c(anno$PC1, pri$mean_PC1))
+PC1_seq <- seq(min_PC1, max_PC1, length.out = 50)
 
 threed <- anno %>%
   dplyr::transmute(
     x = x/1000, 
     y = y/1000,
     z = calage_center,
-    color = viridis::viridis(50)[as.numeric(cut(PC1, breaks = 50))]
+    color = viridis::viridis(50)[as.numeric(cut(PC1, breaks = PC1_seq))]
   )
 
 threedinter <- pri %>%
@@ -30,7 +30,7 @@ threedinter <- pri %>%
     x = x_real/1000, 
     y = y_real/1000,
     z = age_sample,
-    color = viridis::viridis(50)[as.numeric(cut(mean_PC1, breaks = 50))],
+    color = viridis::viridis(50)[as.numeric(cut(mean_PC1, breaks = PC1_seq))],
     alpha = sd_PC1
   )
 
@@ -39,10 +39,14 @@ png(filename = "plots/3D_plot_gpr.png", width = 22, height = 18, units = "cm", r
 
 s <- scatterplot3d::scatterplot3d(
   threed$x, threed$y, threed$z, color = threed$color, 
-  cex.symbols = 0.7, angle = 70,
-  xlab = "x", ylab = "y", zlab = "time calBC",
+  cex.symbols = 1.5, 
+  angle = 70,
+  xlab = "x [km]", ylab = "y [km]", zlab = "time calBC [y]",
+  cex.axis = 0.8,
+  cex.lab = 1.5,
   col.axis = "grey",
-  zlim = c(-7500, -500)
+  zlim = c(-7500, -500),
+  mar = c(3,3,0,2)
 )
 
 #### gpr ####
