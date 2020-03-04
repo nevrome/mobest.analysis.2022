efb80fa4a41542839666b40b079842db4b6b5824
Clemens Schmid <clemens@nevrome.de>
2020-03-04 22:55:40 +0100

    experiments with angle visualisation

--- R/plot_scripts/mobility_estimation_regional_timeseries.R
+++ R/plot_scripts/mobility_estimation_regional_timeseries.R
@@ -2,7 +2,7 @@ library(ggplot2)
 
 load("data/mobility_estimation/pri_mean.RData")
 
-p <- pri_mean %>%
+pri_mean %>%
   dplyr::mutate(
     kernel_setting_id = dplyr::recode(
       kernel_setting_id, "ds100_dt200_g01" = "small kernel", "ds200_dt400_g01" = "big kernel"
@@ -17,6 +17,11 @@ p <- pri_mean %>%
     ),
     alpha = 0.1
   ) +
+  # geom_point(
+  #   data = pri_mean %>% dplyr::summarise()
+  #   aes(x = age_sample, y = 20, fill = mean_angle),
+  #   shape = 21
+  # ) +
   facet_wrap(~region_id, nrow = 3) +
   theme_bw() +
   theme(
@@ -32,7 +37,14 @@ p <- pri_mean %>%
       "small kernel" = "orange",
       "big kernel" = "darkgreen"
     )
-  )
+  ) +
+  # scale_fill_gradient2(
+  #   low = "blue",
+  #   mid = "red",
+  #   high = "blue",
+  #   midpoint = 180
+  # ) +
+  NULL
 
 ggsave(
   paste0("plots/mobility_estimation_regional_timeseries.jpeg"),
