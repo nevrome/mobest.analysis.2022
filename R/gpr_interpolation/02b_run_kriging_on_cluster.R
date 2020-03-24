987d7ed341526c52aa4af2bd317cfe97e96b3598
Clemens Schmid <clemens@nevrome.de>
2020-03-24 17:09:02 +0100

    removed ugly cluster hack - I have to find a better solution for that

--- R/gpr_interpolation/02b_run_kriging_on_cluster.R
+++ R/gpr_interpolation/02b_run_kriging_on_cluster.R
@@ -1,20 +1,3 @@
-#### load correct data and software for this cluster node ####
-
-v <- floor(as.numeric(R.Version()$minor))
-if (v == 4) {
-  .libPaths( c( "/projects1/clusterhomes/schmid/R/x86_64-pc-linux-gnu-library/3.4" , .libPaths() ) )
-  load("/projects1/coest_mobility/coest.interpol.2020/data/gpr/gpr_pred_grid_temporal_sampling_v2.RData")
-  load("/projects1/coest_mobility/coest.interpol.2020/data/gpr/gpr_model_grid_temporal_sampling_v2.RData")
-} else if ( v == 5) {
-  .libPaths( c( "/projects1/clusterhomes/schmid/R/x86_64-pc-linux-gnu-library/3.5" , .libPaths() ) )
-  load("/projects1/coest_mobility/coest.interpol.2020/data/gpr/gpr_pred_grid_temporal_sampling_v3.RData")
-  load("/projects1/coest_mobility/coest.interpol.2020/data/gpr/gpr_model_grid_temporal_sampling_v3.RData")
-} else if ( v == 6) {
-  .libPaths( c( "/projects1/clusterhomes/schmid/R/x86_64-pc-linux-gnu-library/3.6" , .libPaths() ) )
-  load("/projects1/coest_mobility/coest.interpol.2020/data/gpr/gpr_pred_grid_temporal_sampling_v3.RData")
-  load("/projects1/coest_mobility/coest.interpol.2020/data/gpr/gpr_model_grid_temporal_sampling_v3.RData")
-}
-  
 #### run kriging ####
 
 prediction <- lapply(1:nrow(model_grid), function(i) {
