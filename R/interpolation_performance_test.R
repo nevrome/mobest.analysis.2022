414ead78fbf52ff6196e22fc9295a42b0e31c847
Clemens Schmid <clemens@nevrome.de>
2020-04-08 15:10:49 +0200

    run performance test for multiple sampling reruns

--- R/interpolation_performance_test.R
+++ R/interpolation_performance_test.R
@@ -9,118 +9,130 @@ load("data/spatial/mobility_regions.RData")
 
 #### split ####
 
-anno_mixed <- anno[sample(1:nrow(anno), replace = F), ]
-
 n <- 10
-nr <- nrow(anno_mixed)
-anno_10 <- split(anno_mixed, rep(1:n, times = diff(floor(seq(0, nr, length.out = n + 1)))))
+nr <- nrow(anno)
+anno_mixed_list <- lapply(1:3, function(i) { anno[sample(1:nrow(anno), replace = F), ] })
 
-anno_9_training <- lapply(
-  1:n, function(i) {
-    dplyr::bind_rows(anno_10[-i])
-  }
-)
+lapply(anno_mixed_list, function(anno_mixed) {
 
-anno_9_test <- lapply(
-  1:n, function(i) {
-    anno_10[[i]]
-  }
-)
-
-#### prepare model grid ####
-
-model_grid <- lapply(
-  1:n, function(i) {
-    
-    anno <- anno_9_training[[i]]
-    
-    # prep independent variables with temporal sampling
-    independent_tables <- tibble::tibble(
-      independent_table = list(dplyr::transmute(.data = anno, x = x, y = y, z = calage_center)),
-      independent_table_id = i
-    )
-    
-    # prep dependent vars
-    dependent_vars <- tibble::tibble(
-      dependent_var_id = c("PC1", "PC2", "PC3", "PC4")
-    ) %>%
-      dplyr::mutate(
-        dependent_var = lapply(dependent_var_id, function(x) { anno[[x]] })
-      )
-    
-    # create kernel parameters
-    kernel_settings <- tibble::tibble(
-      kernel_setting = list(
-        ds50_dt100_g01 = list(auto = F, d = c(50000, 50000, 100), g = 0.1),
-        ds100_dt200_g01 = list(auto = F, d = c(100000, 100000, 200), g = 0.1),
-        ds200_dt400_g01 = list(auto = F, d = c(200000, 200000, 400), g = 0.1)
-      ),
-      kernel_setting_id = names(kernel_setting)
-    )
-    
-    # create spatiotemporal prediction grid
-    pred_grids <- tibble::tibble(
-      pred_grid = list(anno_9_test[[i]] %>% dplyr::transmute(x, y, z = calage_center, point_id = 1:nrow(.))),
-      pred_grid_id = i
-    )
-    
-    # merge info in prepare model grid
-    model_grid <- mobest::create_model_grid(
-      independent_tables = independent_tables, 
-      dependent_vars = dependent_vars,
-      kernel_settings = kernel_settings,
-      pred_grids = pred_grids
-    )
-    
-  }
-) %>% dplyr::bind_rows()
- 
-#### run interpolation on model grid ####
-
-model_grid_result <- mobest::run_model_grid(model_grid)
-
-#### unnest prediction to get a point-wise prediction table ####
-
-interpol_grid <- mobest::unnest_model_grid(model_grid_result)
-
-#### compare prediction and real values #### 
-
-hu <- interpol_grid %>% tidyr::pivot_wider(
-  names_from = "dependent_var_id",
-  values_from = c("mean", "sd")
-)
-
-spu <- hu %>% split(hu$pred_grid_id)
-
-gnu <- lapply(
-  unique(hu$pred_grid_id), function(i) {
-    spu[[i]] %>%
-      dplyr::left_join(
-        anno_9_test[[i]] %>% dplyr::transmute(PC1, PC2, PC3, PC4, point_id = 1:nrow(.)),
-        by = "point_id"
-      )
-  }
-) %>% dplyr::bind_rows()
-
-plu <- gnu %>% 
-  dplyr::mutate(
-    PC1_dist = PC1 - mean_PC1,
-    PC2_dist = PC2 - mean_PC2,
-    PC3_dist = PC3 - mean_PC3,
-    PC4_dist = PC4 - mean_PC4,
-    PC1_dist_norm = PC1_dist / diff(range(PC1)),
-    PC2_dist_norm = PC2_dist / diff(range(PC2)),
-    PC3_dist_norm = PC3_dist / diff(range(PC3)),
-    PC4_dist_norm = PC4_dist / diff(range(PC4))
-  ) %>%
-  dplyr::select(
-    kernel_setting_id, tidyselect::contains("_dist")
-  ) %>%
-  tidyr::pivot_longer(
-    cols = tidyselect::starts_with("PC"),
-    names_to = "PC",
-    values_to = "difference"
+  anno_10 <- split(anno_mixed, rep(1:n, times = diff(floor(seq(0, nr, length.out = n + 1)))))
+  
+  anno_9_training <- lapply(
+    1:n, function(i) {
+      dplyr::bind_rows(anno_10[-i])
+    }
   )
+  
+  anno_9_test <- lapply(
+    1:n, function(i) {
+      anno_10[[i]]
+    }
+  )
+  
+  #### prepare model grid ####
+  
+  model_grid <- lapply(
+    1:n, function(i) {
+      
+      anno <- anno_9_training[[i]]
+      
+      # prep independent variables with temporal sampling
+      independent_tables <- tibble::tibble(
+        independent_table = list(dplyr::transmute(.data = anno, x = x, y = y, z = calage_center)),
+        independent_table_id = i
+      )
+      
+      # prep dependent vars
+      dependent_vars <- tibble::tibble(
+        dependent_var_id = c("PC1", "PC2", "PC3", "PC4")
+      ) %>%
+        dplyr::mutate(
+          dependent_var = lapply(dependent_var_id, function(x) { anno[[x]] })
+        )
+      
+      # create kernel parameters
+      kernel_settings <- tibble::tibble(
+        kernel_setting = list(
+          ds50_dt50_g001 = list(auto = F, d = c(50000, 50000, 50), g = 0.01),
+          ds50_dt50_g01 = list(auto = F, d = c(50000, 50000, 50), g = 0.1),
+          ds50_dt50_g05 = list(auto = F, d = c(50000, 50000, 50), g = 0.5),
+          ds200_dt200_g001 = list(auto = F, d = c(200000, 200000, 200), g = 0.01),
+          ds200_dt200_g01 = list(auto = F, d = c(200000, 200000, 200), g = 0.1),
+          ds200_dt200_g05 = list(auto = F, d = c(200000, 200000, 200), g = 0.5),
+          ds1000_dt1000_g001 = list(auto = F, d = c(1000000, 1000000, 1000), g = 0.01),
+          ds1000_dt1000_g01 = list(auto = F, d = c(1000000, 1000000, 1000), g = 0.1),
+          ds1000_dt1000_g05 = list(auto = F, d = c(1000000, 1000000, 1000), g = 0.5)
+        ),
+        kernel_setting_id = factor(names(kernel_setting), levels = names(kernel_setting))
+      )
+      
+      # create spatiotemporal prediction grid
+      pred_grids <- tibble::tibble(
+        pred_grid = list(anno_9_test[[i]] %>% dplyr::transmute(x, y, z = calage_center, point_id = 1:nrow(.))),
+        pred_grid_id = i
+      )
+      
+      # merge info in prepare model grid
+      model_grid <- mobest::create_model_grid(
+        independent_tables = independent_tables, 
+        dependent_vars = dependent_vars,
+        kernel_settings = kernel_settings,
+        pred_grids = pred_grids
+      )
+      
+    }
+  ) %>% dplyr::bind_rows()
+   
+  #### run interpolation on model grid ####
+  
+  model_grid_result <- mobest::run_model_grid(model_grid)
+  
+  #### unnest prediction to get a point-wise prediction table ####
+  
+  interpol_grid <- mobest::unnest_model_grid(model_grid_result)
+  
+  #### compare prediction and real values #### 
+  
+  hu <- interpol_grid %>% tidyr::pivot_wider(
+    names_from = "dependent_var_id",
+    values_from = c("mean", "sd")
+  )
+  
+  spu <- hu %>% split(hu$pred_grid_id)
+  
+  gnu <- lapply(
+    unique(hu$pred_grid_id), function(i) {
+      spu[[i]] %>%
+        dplyr::left_join(
+          anno_9_test[[i]] %>% dplyr::transmute(PC1, PC2, PC3, PC4, point_id = 1:nrow(.)),
+          by = "point_id"
+        )
+    }
+  ) %>% dplyr::bind_rows()
+  
+  plu <- gnu %>% 
+    dplyr::mutate(
+      PC1_dist = PC1 - mean_PC1,
+      PC2_dist = PC2 - mean_PC2,
+      PC3_dist = PC3 - mean_PC3,
+      PC4_dist = PC4 - mean_PC4,
+      PC1_dist_norm = PC1_dist / diff(range(PC1)),
+      PC2_dist_norm = PC2_dist / diff(range(PC2)),
+      PC3_dist_norm = PC3_dist / diff(range(PC3)),
+      PC4_dist_norm = PC4_dist / diff(range(PC4))
+    ) %>%
+    dplyr::select(
+      kernel_setting_id, tidyselect::contains("_dist")
+    ) %>%
+    tidyr::pivot_longer(
+      cols = tidyselect::starts_with("PC"),
+      names_to = "PC",
+      values_to = "difference"
+    )
+  
+  return(plu)
+  
+}) %>% dplyr::bind_rows() -> plu
 
 library(ggplot2)
 
@@ -128,9 +140,9 @@ plu %>%
   dplyr::filter(!grepl("norm", PC)) %>%
   ggplot() +
   geom_histogram(
-    aes(x = difference, fill = kernel_setting_id), bins = 30
+    aes(x = difference, fill = kernel_setting_id), bins = 100
   ) +
-  facet_grid(rows = vars(PC), cols = vars(kernel_setting_id)) +
+  facet_grid(cols = vars(PC), rows = vars(kernel_setting_id)) +
   geom_vline(aes(xintercept = 0)) +
   theme_bw() +
   xlab("")
@@ -139,8 +151,8 @@ plu %>%
   dplyr::filter(grepl("norm", PC)) %>%
   ggplot() +
   geom_histogram(
-    aes(x = difference, fill = kernel_setting_id), bins = 30
+    aes(x = difference, fill = kernel_setting_id), bins = 100
   ) +
-  facet_grid(rows = vars(PC), cols = vars(kernel_setting_id)) +
+  facet_grid(cols = vars(PC), rows = vars(kernel_setting_id)) +
   geom_vline(aes(xintercept = 0)) +
   theme_bw()
