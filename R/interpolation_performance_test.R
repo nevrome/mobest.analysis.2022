c4716a51b57f39de8b0232bb907fbd5a8856710c
Clemens Schmid <clemens@nevrome.de>
2020-04-08 17:13:46 +0200

    more love for the test script and its plots

--- R/interpolation_performance_test.R
+++ R/interpolation_performance_test.R
@@ -1,4 +1,5 @@
 library(magrittr)
+library(ggplot2)
 
 #### data ####
 
@@ -7,29 +8,35 @@ anno <- anno_1240K_and_anno_1240K_HumanOrigins_filtered
 load("data/spatial/area.RData")
 load("data/spatial/mobility_regions.RData")
 
-#### split ####
+#### compile randomly reordered versions of anno ####
 
-n <- 10
-nr <- nrow(anno)
-anno_mixed_list <- lapply(1:3, function(i) { anno[sample(1:nrow(anno), replace = F), ] })
+anno_mixed_list <- lapply(1:2, function(i) { anno[sample(1:nrow(anno), replace = F), ] })
+
+#### run prediction test for each of this versions ####
 
 lapply(anno_mixed_list, function(anno_mixed) {
 
+  #### split anno into 10 sections ####
+  
+  n <- 10
+  nr <- nrow(anno_mixed)
   anno_10 <- split(anno_mixed, rep(1:n, times = diff(floor(seq(0, nr, length.out = n + 1)))))
   
+  # 9 sections are used as a training dataset for the GP model
   anno_9_training <- lapply(
     1:n, function(i) {
       dplyr::bind_rows(anno_10[-i])
     }
   )
   
+  # 1 section is used as a test dataset
   anno_9_test <- lapply(
     1:n, function(i) {
       anno_10[[i]]
     }
   )
   
-  #### prepare model grid ####
+  #### prepare model grid for current 9:1 comparison with different kernels ####
   
   model_grid <- lapply(
     1:n, function(i) {
@@ -78,7 +85,8 @@ lapply(anno_mixed_list, function(anno_mixed) {
         dependent_vars = dependent_vars,
         kernel_settings = kernel_settings,
         pred_grids = pred_grids
-      )
+      ) %>% 
+        dplyr::select(-independent_table_type)
       
     }
   ) %>% dplyr::bind_rows()
@@ -91,18 +99,20 @@ lapply(anno_mixed_list, function(anno_mixed) {
   
   interpol_grid <- mobest::unnest_model_grid(model_grid_result)
   
-  #### compare prediction and real values #### 
+  #### merge prediction and real values #### 
   
-  hu <- interpol_grid %>% tidyr::pivot_wider(
+  # make wide for mean and sd PC values
+  interpol_grid_wide <- interpol_grid %>% tidyr::pivot_wider(
     names_from = "dependent_var_id",
     values_from = c("mean", "sd")
   )
   
-  spu <- hu %>% split(hu$pred_grid_id)
+  # split by run training+test run setup to be able to merge with real test values
+  interpol_grid_wide_split <- interpol_grid_wide %>% split(interpol_grid_wide$pred_grid_id)
   
-  gnu <- lapply(
-    unique(hu$pred_grid_id), function(i) {
-      spu[[i]] %>%
+  interpol_grid_merged <- lapply(
+    unique(interpol_grid_wide$pred_grid_id), function(i) {
+      interpol_grid_wide_split[[i]] %>%
         dplyr::left_join(
           anno_9_test[[i]] %>% dplyr::transmute(PC1, PC2, PC3, PC4, point_id = 1:nrow(.)),
           by = "point_id"
@@ -110,44 +120,75 @@ lapply(anno_mixed_list, function(anno_mixed) {
     }
   ) %>% dplyr::bind_rows()
   
-  plu <- gnu %>% 
-    dplyr::mutate(
-      PC1_dist = PC1 - mean_PC1,
-      PC2_dist = PC2 - mean_PC2,
-      PC3_dist = PC3 - mean_PC3,
-      PC4_dist = PC4 - mean_PC4,
-      PC1_dist_norm = PC1_dist / diff(range(PC1)),
-      PC2_dist_norm = PC2_dist / diff(range(PC2)),
-      PC3_dist_norm = PC3_dist / diff(range(PC3)),
-      PC4_dist_norm = PC4_dist / diff(range(PC4))
-    ) %>%
-    dplyr::select(
-      kernel_setting_id, tidyselect::contains("_dist")
-    ) %>%
-    tidyr::pivot_longer(
-      cols = tidyselect::starts_with("PC"),
-      names_to = "PC",
-      values_to = "difference"
-    )
+  return(interpol_grid_merged)
   
-  return(plu)
-  
-}) %>% dplyr::bind_rows() -> plu
+}) %>% dplyr::bind_rows() -> interpol_grid_merged_all
 
-library(ggplot2)
 
-plu %>%
-  dplyr::filter(!grepl("norm", PC)) %>%
-  ggplot() +
+#### prepare for plotting ####
+
+# calculate distances
+interpol_grid_dist <- interpol_grid_merged_all %>% 
+  dplyr::mutate(
+    PC1_dist = PC1 - mean_PC1,
+    PC2_dist = PC2 - mean_PC2,
+    PC3_dist = PC3 - mean_PC3,
+    PC4_dist = PC4 - mean_PC4,
+    PC1_dist_norm = PC1_dist / diff(range(PC1)),
+    PC2_dist_norm = PC2_dist / diff(range(PC2)),
+    PC3_dist_norm = PC3_dist / diff(range(PC3)),
+    PC4_dist_norm = PC4_dist / diff(range(PC4))
+  ) %>%
+  dplyr::select(
+    kernel_setting_id, tidyselect::contains("_dist")
+  ) %>%
+  tidyr::pivot_longer(
+    cols = tidyselect::starts_with("PC"),
+    names_to = "PC",
+    values_to = "difference"
+  ) 
+
+interpol_comparison <- interpol_grid_dist %>%
+  tidyr::separate(
+    kernel_setting_id, 
+    c("ds", "dt", "g"),
+    sep = "_",
+    remove = F
+  ) 
+
+interpol_comparison_sd <- interpol_comparison %>%
+  dplyr::group_by(kernel_setting_id, ds, dt, g, PC) %>%
+  dplyr::summarise(
+    sd_difference = sd(difference)
+  ) %>%
+  dplyr::ungroup()
+
+
+ggplot() +
   geom_histogram(
-    aes(x = difference, fill = kernel_setting_id), bins = 100
+    data = interpol_comparison %>% dplyr::filter(!grepl("norm", PC)),
+    mapping = aes(x = difference, fill = kernel_setting_id), bins = 100
   ) +
   facet_grid(cols = vars(PC), rows = vars(kernel_setting_id)) +
-  geom_vline(aes(xintercept = 0)) +
+  geom_vline(
+    data = interpol_comparison_sd %>% dplyr::filter(!grepl("norm", PC)), 
+    mapping = aes(xintercept = sd_difference),
+    color = "red"
+  ) +
+  geom_vline(
+    data = interpol_comparison_sd %>% dplyr::filter(!grepl("norm", PC)), 
+    mapping = aes(xintercept = 0),
+    color = "black"
+  ) +
+  geom_vline(
+    data = interpol_comparison_sd %>% dplyr::filter(!grepl("norm", PC)), 
+    mapping = aes(xintercept = -sd_difference),
+    color = "red"
+  ) +
   theme_bw() +
-  xlab("")
+  xlim(-0.05, 0.05)
 
-plu %>%
+interpol_comparison %>%
   dplyr::filter(grepl("norm", PC)) %>%
   ggplot() +
   geom_histogram(
