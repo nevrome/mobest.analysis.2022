99f0c2a5bc6306b8cc47d9cdc5a2e1ad1ea816ab
Clemens Schmid <clemens@nevrome.de>
2020-04-09 16:30:29 +0200

    second crossvalidation run

--- R/crossvalidation/interpolation_performance_test.R
+++ R/crossvalidation/interpolation_performance_test.R
@@ -60,12 +60,19 @@ lapply(anno_mixed_list, function(anno_mixed) {
       # create kernel parameters
       
       # run 1
-      ks <- expand.grid(
-        ds = seq(10, 1010, 100)*1000,
-        dt = seq(10, 1010, 100),
-        g = c(0.001, 0.01, 0.1, 0.5)
-      )
+      # ks <- expand.grid(
+      #   ds = seq(10, 1010, 100)*1000,
+      #   dt = seq(10, 1010, 100),
+      #   g = c(0.001, 0.01, 0.1, 0.5)
+      # )
       
+      # run 2
+      ks <- expand.grid(
+        ds = seq(1, 51, 10)*1000,
+        dt = seq(200, 600, 50),
+        g = seq(0.05, 0.15, 0.02)
+      )
+
       kernel_settings <- tibble::tibble(
         kernel_setting = lapply(
           1:nrow(ks), function(i) {
@@ -171,6 +178,6 @@ interpol_comparison_group <- interpol_comparison %>%
   ) %>%
   dplyr::ungroup()
 
-save(interpol_comparison_group, file = "data/crossvalidation/interpol_comparison_group_1.RData")
+save(interpol_comparison_group, file = "data/crossvalidation/interpol_comparison_group_2.RData")
 
 # scp schmid@cdag2-new.cdag.shh.mpg.de:/projects1/coest_mobility/coest.interpol.2020/data/crossvalidation/interpol_comparison_group_1.RData interpol_comparison_group_1.RData
