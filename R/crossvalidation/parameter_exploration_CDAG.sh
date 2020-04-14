7eb949401f86beccfc63017dd564894b32575a75
Clemens Schmid <schmid@shh.mpg.de>
2020-04-14 16:05:50 +0200

    adjusting run script

---
+++ R/crossvalidation/parameter_exploration_CDAG.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash ~/singularity/slurm_nevrome_coest.sh short 4 50 R/crossvalidation/interpolation_performance_test.R 1 0.001
+#bash ~/singularity/slurm_nevrome_coest.sh short 16 50 R/crossvalidation/interpolation_performance_test.R 2 0.005
+#bash ~/singularity/slurm_nevrome_coest.sh short 16 50 R/crossvalidation/interpolation_performance_test.R 3 0.01
+#bash ~/singularity/slurm_nevrome_coest.sh short 16 50 R/crossvalidation/interpolation_performance_test.R 4 0.05
+#bash ~/singularity/slurm_nevrome_coest.sh short 16 50 R/crossvalidation/interpolation_performance_test.R 5 0.1
+
--- R/crossvalidation/parameter_exploration_SDAG.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-~/singularity/slurm_nevrome_coest.sh short 64 100 R/crossvalidation/interpolation_performance_test.R 1 0.001
-~/singularity/slurm_nevrome_coest.sh short 64 100 R/crossvalidation/interpolation_performance_test.R 2 0.005
-~/singularity/slurm_nevrome_coest.sh short 64 100 R/crossvalidation/interpolation_performance_test.R 3 0.01
-~/singularity/slurm_nevrome_coest.sh short 64 100 R/crossvalidation/interpolation_performance_test.R 4 0.05
-~/singularity/slurm_nevrome_coest.sh short 64 100 R/crossvalidation/interpolation_performance_test.R 5 0.1
-
