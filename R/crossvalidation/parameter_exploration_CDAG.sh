ed4a43c863308727806f5cec47eda32979a4775b
Clemens Schmid <schmid@shh.mpg.de>
2020-04-15 16:09:46 +0200

    work on a job array script that only sends a certain number of jobs to the cluster

--- R/crossvalidation/parameter_exploration_CDAG.sh
+++ R/crossvalidation/parameter_exploration_CDAG.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
-i=0
-for g in 0.001 0.005 0.01 0.05 0.1
+jobcount=0
+simuljobs=4
+simuljobcount=0
+gstotal=(0.001 0.005) #0.005 0.01 0.05 0.1
+dtstotal=(20 40 60 80 100) #{20..300..20}
+jobs=${#gstotal[@]}*${#dtstotal[@]} 
+gs=()
+dts=() 
+for g in "${gstotal[@]}"
 do
-	for dt in {20..300..20}
+	for dt in "${dtstotal[@]}"
 	do
-		echo "bash ~/singularity/slurm_nevrome_coest.sh short 8 10 R/crossvalidation/interpolation_performance_test.R $i $dt $g"
-		((i=i+1))
+		gs+=($g)
+		dts+=($dt)
+		(( simuljobcount=simuljobcount+1 ))
+
+		if (( simuljobcount == simuljobs ))
+		then
+			for (( i=0; i<simuljobs; i++ ))
+			do
+				echo "bash ~/singularity/slurm_nevrome_coest.sh short 8 10 --wait R/crossvalidation/interpolation_performance_test.R $jobcount ${dts[$i]} ${gs[$i]}"
+				(( jobcount=jobcount+1 ))
+			done
+                        for (( i=0; i<simuljobs; i++ ))
+                        do
+                                unset -v 'gs[$i]'
+				unset -v 'dts[$i]'
+                        done
+			(( simuljobcount=0 ))
+			wait
+		fi
+
 	done
 done
 
+#do rest
+
+for (( i=0; i<${#gs[@]}; i++ ))
+do
+	
+	echo "bash ~/singularity/slurm_nevrome_coest.sh short 8 10 --wait R/crossvalidation/interpolation_performance_test.R $jobcount ${dts[$i]} ${gs[$i]}"
+	(( jobcount=jobcount+1 ))
+done
