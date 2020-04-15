c9149675e64705b2e95a728f0bce0cfa4582ecc7
Clemens Schmid <schmid@shh.mpg.de>
2020-04-15 16:50:03 +0200

    and further work on this script

--- R/crossvalidation/parameter_exploration_CDAG.sh
+++ R/crossvalidation/parameter_exploration_CDAG.sh
@@ -2,12 +2,16 @@
 
 jobcount=0
 simuljobs=4
+sleeptime=10  #10*60
 simuljobcount=0
+
 gstotal=(0.001 0.005) #0.005 0.01 0.05 0.1
 dtstotal=(20 40 60) # 80 100) #{20..300..20}
-jobs=${#gstotal[@]}*${#dtstotal[@]} 
+jobs=$((${#gstotal[@]}*${#dtstotal[@]}))
+
 gs=()
 dts=() 
+
 for g in "${gstotal[@]}"
 do
 	for dt in "${dtstotal[@]}"
@@ -20,8 +24,9 @@ do
 		then
 			for (( i=0; i<simuljobs; i++ ))
 			do
-				bash ~/singularity/slurm_nevrome_coest.sh short 8 10 --wait R/crossvalidation/interpolation_performance_test.R $jobcount ${dts[$i]} ${gs[$i]}
+				bash ~/singularity/slurm_nevrome_coest.sh short 8 10 R/crossvalidation/interpolation_performance_test.R $jobcount ${dts[$i]} ${gs[$i]}
 				(( jobcount=jobcount+1 ))
+				echo Running job $jobcount of $jobs 
 			done
                         for (( i=0; i<simuljobs; i++ ))
                         do
@@ -29,7 +34,8 @@ do
 				unset -v 'dts[$i]'
                         done
 			(( simuljobcount=0 ))
-			wait
+
+			sleep $sleeptime
 		fi
 
 	done
@@ -40,6 +46,7 @@ done
 for (( i=0; i<${#gs[@]}; i++ ))
 do
 	
-	bash ~/singularity/slurm_nevrome_coest.sh short 8 10 --wait R/crossvalidation/interpolation_performance_test.R $jobcount ${dts[$i]} ${gs[$i]}
+	bash ~/singularity/slurm_nevrome_coest.sh short 8 10 R/crossvalidation/interpolation_performance_test.R $jobcount ${dts[$i]} ${gs[$i]}
 	(( jobcount=jobcount+1 ))
+	echo Running job $jobcount of $jobs
 done
