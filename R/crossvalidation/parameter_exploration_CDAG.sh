97c3fc6bf610dad35ba2c4d881ce7aa38e48e48b
Clemens Schmid <schmid@shh.mpg.de>
2020-04-15 14:40:09 +0200

    nested loop approach to start jobs -- should not be done like this

--- R/crossvalidation/parameter_exploration_CDAG.sh
+++ R/crossvalidation/parameter_exploration_CDAG.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-bash ~/singularity/slurm_nevrome_coest.sh short 16 30 R/crossvalidation/interpolation_performance_test.R 1 0.001
-bash ~/singularity/slurm_nevrome_coest.sh short 16 30 R/crossvalidation/interpolation_performance_test.R 2 0.005
-bash ~/singularity/slurm_nevrome_coest.sh short 16 30 R/crossvalidation/interpolation_performance_test.R 3 0.01
-bash ~/singularity/slurm_nevrome_coest.sh short 16 30 R/crossvalidation/interpolation_performance_test.R 4 0.05
-bash ~/singularity/slurm_nevrome_coest.sh short 16 30 R/crossvalidation/interpolation_performance_test.R 5 0.1
+i=0
+for g in 0.001 0.005 0.01 0.05 0.1
+do
+	for dt in {20..300..20}
+	do
+		echo "bash ~/singularity/slurm_nevrome_coest.sh short 8 10 R/crossvalidation/interpolation_performance_test.R $i $dt $g"
+		((i=i+1))
+	done
+done
 
