d61641ca916ab427716ec494be40f621c0e01729
Clemens Schmid <clemens@nevrome.de>
2020-04-18 15:47:03 +0200

    exploring cleaned dataset

--- code/mds/plot_mds.R
+++ code/mds/plot_mds.R
@@ -1,38 +1,28 @@
-#scp schmid@cdag2-new.cdag.shh.mpg.de:/projects1/coest_mobility/coest.interpol.2020/data/mds/plink.mds .
+#scp schmid@cdag2-new.cdag.shh.mpg.de:/projects1/coest_mobility/coest.interpol.2020/data/mds/1240K_HumanOrigins.pruned.mds .
+#scp schmid@cdag2-new.cdag.shh.mpg.de:/projects1/coest_mobility/coest.interpol.2020/data/mds/1240K_HumanOrigins.filtered.pruned.mds .
 
 library(magrittr)
 library(ggplot2)
 
-mds <- readr::read_delim("data/mds/plink.mds", " ", trim_ws = T) %>%
+mds1 <- readr::read_delim("data/mds/1240K_HumanOrigins.pruned.mds", " ", trim_ws = T) %>%
   dplyr::select(-X8)
+
+mds2 <- readr::read_delim("data/mds/1240K_HumanOrigins.filtered.pruned.mds", " ", trim_ws = T) %>%
+  dplyr::select(-X8)
+
 
 load("data/anno_1240K_and_anno_1240K_HumanOrigins_filtered.RData")
 anno <- anno_1240K_and_anno_1240K_HumanOrigins_filtered
 
-anno  <- anno %>% dplyr::filter(
-  sample_id != "I4933"
-)
-
-mds_worked <- anno %>% dplyr::left_join(
-  mds, by = c("sample_id" = "IID")
-) %>% dplyr::filter(
-  C2 < 0.04,
-  C2 > -0.03
+mds_worked <- mds2 %>% dplyr::left_join(
+  anno, by = c("IID" = "sample_id")
 )
 
 mds_worked %>% ggplot() +
-  geom_point(aes(C2, PC4))
+  geom_point(aes(C1, C2))
 
+mds_worked %>% ggplot() +
+  geom_point(aes(C1, PC1))
 
-mds_worked <- mds %>% dplyr::filter(
-  IID != "I4933"
-) %>% dplyr::left_join(
-  anno, by = c("IID" = "sample_id")
-) %>%
-  dplyr::mutate(
-    SG = grepl(".SG", IID)
-  ) 
-
-
-
-
+mds_worked %>% ggplot() +
+  geom_point(aes(C2, PC2))
