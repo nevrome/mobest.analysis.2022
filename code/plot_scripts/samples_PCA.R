b488e9aecbbcb6a6cb4fa48f44733b143b4dcfa8
Clemens Schmid <clemens@nevrome.de>
2020-04-20 16:03:10 +0200

    improved PCA plot

--- code/plot_scripts/samples_PCA.R
+++ code/plot_scripts/samples_PCA.R
@@ -3,41 +3,54 @@ library(ggplot2)
 
 load("data/anno_1240K_and_anno_1240K_HumanOrigins_pca.RData")
 ref_pops <- readLines("data/population_lists/PCA_6.pops")
-ref_pops_grouping <- readr::read_tsv("data/population_lists/contemporary_group_info_180131.txt", col_names = T)
 
 pca_ref <- anno_1240K_and_anno_1240K_HumanOrigins_pca %>%
   dplyr::filter(
     group_label %in% ref_pops
-  ) %>%
-  dplyr::left_join(
-    ref_pops_grouping, by = c("group_label" = "Pop")
   )
 
-load("data/anno_1240K_and_anno_1240K_HumanOrigins_filtered.RData")
-anno <- anno_1240K_and_anno_1240K_HumanOrigins_filtered
+load("data/anno_1240K_and_anno_1240K_HumanOrigins_final.RData")
+anno <- anno_1240K_and_anno_1240K_HumanOrigins_final
 
 p <- ggplot() +
   geom_point(
     data = pca_ref,
-    aes(x = PC1, y = PC2, color = Region)
+    aes(x = PC1, y = PC2),
+    shape = 20,
+    size = 0.2
   ) +
+  ggnewscale::new_scale_color() +
   geom_point(
     data = anno,
-    aes(x = PC1, y = PC2),
-    color = "black",
-    alpha = 0.5,
-    shape = 3,
+    aes(x = PC1, y = PC2, color = region_id, shape = age_group_id),
+    alpha = 0.7,
     size = 2
   ) +
   theme_bw() +
   theme(
-    axis.title = element_text(size = 20),
-    axis.text = element_text(size = 20),
-    legend.text = element_text(size = 20),
-    legend.title = element_text(size = 20),
-    legend.position = "bottom"
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 12),
+    legend.text = element_text(size = 12),
+    legend.title = element_blank(),
+    legend.position = "right"
   ) +
-  guides(color = guide_legend(override.aes = list(size = 10)))
+  guides(color = guide_legend(override.aes = list(size = 2))) +
+  scale_shape_manual(
+    values = c(
+      ">-10000" = 15,
+      "-8000 - -6000" = 15,
+      "-6000 - -4000" = 17,
+      "-4000 - -2000" = 6,
+      "-2000 - 0" = 4
+    )
+  ) +
+  scale_color_manual(
+    values = c(
+      "#999999", "#E69F00", "#56B4E9", "#009E73", "#871200",
+      "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#2fff00"
+    )
+  ) +
+  coord_fixed()
 
 ggsave(
   paste0("plots/samples_PCA.jpeg"),
@@ -45,7 +58,7 @@ ggsave(
   device = "jpeg",
   scale = 0.8,
   dpi = 300,
-  width = 300, height = 250, units = "mm",
+  width = 330, height = 250, units = "mm",
   limitsize = F
 )
 
