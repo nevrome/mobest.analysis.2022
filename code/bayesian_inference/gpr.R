f66e04bff826a4ea7c8d42a05aee1009d08d32ca
Clemens Schmid <clemens@nevrome.de>
2020-04-24 11:19:42 +0200

    add linear model to apply gpr only on residuals

--- code/bayesian_inference/gpr.R
+++ code/bayesian_inference/gpr.R
@@ -1,21 +1,28 @@
 library(magrittr)
 
-load("data/anno_1240K_and_anno_1240K_HumanOrigins_final.RData")
-anno <- anno_1240K_and_anno_1240K_HumanOrigins_final %>%
-  dplyr::filter(
-    region_id == "Central Europe",
-    calage_center > -4000, calage_center < -2300
-  )
+# load("data/anno_1240K_and_anno_1240K_HumanOrigins_final.RData")
+# anno <- anno_1240K_and_anno_1240K_HumanOrigins_final %>%
+#   dplyr::filter(
+#     region_id == "Central Europe",
+#     calage_center > -4000, calage_center < -2300
+#   )
+# 
+# x1 <- anno$x
+# x2 <- anno$y
+# x3 <- anno$calage_center
+# y <- anno$PC1
 
-x1 <- anno$x
-x2 <- anno$y
-x3 <- anno$calage_center
-y <- anno$PC1
+independent <- tibble::tibble(
+  x1 <- seq(10, 200, 10),
+  x2 <- 21:40,
+  x3 <- 41:60
+)
+dependent <- x1 + x2 + x3 + rnorm(length(x1), 0, 5)
 
-x1 <- 1:20
-x2 <- 21:40
-x3 <- 41:60
-y <- x1 + x2 + x3 + rnorm(length(x1), 0, 5)
+# linear fit
+combined <- independent %>% dplyr::mutate(d = dependent)
+model <- stats::lm(d ~ x1 + x2 + x3, data = combined)
+dependent <- model[["residuals"]]
 
 
 fit <- rstan::stan(
@@ -23,8 +30,8 @@ fit <- rstan::stan(
   data = list(
     D = 3,
     x = data.frame(x1, x2, x3),
-    N = length(y),
-    y = y
+    N = length(dependent),
+    y = dependent
   ),
   chains = 1,
   cores = 5
