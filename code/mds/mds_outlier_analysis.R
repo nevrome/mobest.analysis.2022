f1b772ae70260167b0ae332f35c028a002aea9ba
Clemens Schmid <clemens@nevrome.de>
2020-04-30 00:19:02 +0200

    added some plots to explore what makes outliers in the MDS outliers

--- code/mds/mds_outlier_analysis.R
+++ code/mds/mds_outlier_analysis.R
@@ -26,9 +26,68 @@ mds2 <- mds %>% dplyr::left_join(
 
 # SG vs. Capture (data_type)
 
+hu <- mds2 %>%
+  dplyr::filter(data_type %in% c("Shotgun", "1240K")) %>%
+  dplyr::group_by(
+    data_type, in_95
+  ) %>%
+  dplyr::summarise(
+    n = dplyr::n(),
+  ) %>%
+  dplyr::group_by(
+    data_type
+  ) %>% 
+  dplyr::mutate(
+    percent = round(n / sum(n), 2),
+  )
+
+p1 <- hu %>% ggplot(aes(x = data_type, y = n, fill = in_95)) +
+  geom_bar(
+    position = position_stack(),
+    stat = "identity"
+  ) +
+  geom_text(
+    aes(label = percent), position = position_stack(vjust = 0.5), size = 5
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = "bottom"
+  ) +
+  ggtitle("Shotgun vs. Capture")
+
 # coverage
+
+p2 <- mds2 %>%
+  ggplot(aes(x = in_95, y = coverage, fill = in_95)) +
+  geom_jitter(width = 0.3, alpha = 0.2) +
+  geom_boxplot(width = 0.1) +
+  scale_y_log10(labels = scales::comma) +
+  theme_bw() +
+  guides(fill = FALSE) +
+  ggtitle("Coverage")
 
 # snps_hit_on_autosomes
 
+p3 <- mds2 %>%
+  ggplot(aes(x = in_95, y = snps_hit_on_autosomes, fill = in_95)) +
+  geom_jitter(width = 0.3, alpha = 0.2) +
+  geom_boxplot(width = 0.1) +
+  scale_y_log10(labels = scales::comma) +
+  theme_bw() +
+  guides(fill = FALSE) +
+  ggtitle("# SNPs on autosomes")
 
+# merge plots
+
+p <- cowplot::plot_grid(p1, p2, p3, nrow = 1)
+
+ggsave(
+  paste0("plots/mds_outlier_analysis.jpeg"),
+  plot = p,
+  device = "jpeg",
+  scale = 0.7,
+  dpi = 300,
+  width = 400, height = 230, units = "mm",
+  limitsize = F
+)
 
