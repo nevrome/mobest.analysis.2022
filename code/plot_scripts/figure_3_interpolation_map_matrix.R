fb21cb688fd240a52b086c8afc5b77572013d597
Clemens Schmid <clemens@nevrome.de>
2020-05-05 15:58:55 +0200

    split plot to allow for different colour scales

--- code/plot_scripts/figure_3_interpolation_map_matrix.R
+++ code/plot_scripts/figure_3_interpolation_map_matrix.R
@@ -9,10 +9,10 @@ ex <- raster::extent(research_area)
 xlimit <- c(ex[1], ex[2])
 ylimit <- c(ex[3], ex[4])
 
-p <- interpol_grid_spatial %>%
+p_PC1 <- interpol_grid_spatial %>%
   dplyr::filter(
     independent_table_id == "age_center",
-    dependent_var_id %in% c("PC1", "PC2"),
+    dependent_var_id %in% c("PC1"),
     kernel_setting_id == "ds200_dt400_g01",
     pred_grid_id == "scs100_tl100",
     z %in% c(-7000, -5000, -3000, -1000)
@@ -35,7 +35,45 @@ p <- interpol_grid_spatial %>%
     crs = sf::st_crs("+proj=aea +lat_1=43 +lat_2=62 +lat_0=30 +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs")
   ) +
   guides(
-    fill = guide_colorbar(title = "PC prediction", barwidth = 25)
+    fill = guide_colorbar(title = "PC prediction", barwidth = 10)
+  ) +
+  theme(
+    legend.position = "bottom",
+    legend.box = "horizontal",
+    legend.title = element_text(size = 15),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    legend.text = element_text(size = 15),
+    strip.text = element_text(size = 15)
+  )
+
+p_PC2 <- interpol_grid_spatial %>%
+  dplyr::filter(
+    independent_table_id == "age_center",
+    dependent_var_id %in% c("PC2"),
+    kernel_setting_id == "ds200_dt400_g01",
+    pred_grid_id == "scs100_tl100",
+    z %in% c(-7000, -5000, -3000, -1000)
+  ) %>%
+  ggplot() +
+  geom_sf(data = extended_area, fill = "white") +
+  geom_raster(aes(x, y, fill = mean)) +
+  facet_grid(rows = dplyr::vars(z), cols = dplyr::vars(dependent_var_id)) +
+  geom_sf(
+    data = anno_slices_geo %>% dplyr::mutate(z = age) %>% dplyr::filter(z %in% c(-7000, -5000, -3000, -1000)),
+    size = 2,
+    shape = 18,
+    color = "red"
+  ) +
+  geom_sf(data = extended_area, fill = NA) +
+  scale_fill_viridis_c(option = "magma") +
+  theme_bw() +
+  coord_sf(
+    xlim = xlimit, ylim = ylimit,
+    crs = sf::st_crs("+proj=aea +lat_1=43 +lat_2=62 +lat_0=30 +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs")
+  ) +
+  guides(
+    fill = guide_colorbar(title = "PC prediction", barwidth = 10)
   ) +
   theme(
     legend.position = "bottom",
@@ -47,12 +85,16 @@ p <- interpol_grid_spatial %>%
     strip.text = element_text(size = 15)
   )
   
+
+# merge plots
+p <- cowplot::plot_grid(p_PC1, p_PC2, ncol = 2)
+
 ggsave(
   "plots/figure_3_interpolation_map_matrix.jepg",
   plot = p,
   device = "jpeg",
   scale = 0.8,
   dpi = 300,
-  width = 300, height = 400, units = "mm",
+  width = 300, height = 350, units = "mm",
   limitsize = F
 )
