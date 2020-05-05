bd367e3cbe31b94cf2d9c17e4a2d78ac4847291f
Clemens Schmid <clemens@nevrome.de>
2020-05-05 15:11:08 +0200

    schema to describe the general paper workflow

---
+++ code/plot_scripts/figure_2_schema.R
@@ -0,0 +1,7 @@
+DiagrammeR::grViz("code/plot_scripts/figure_2_schema.dot") %>%
+  DiagrammeRsvg::export_svg() %>% 
+  charToRaw %>% 
+  rsvg::rsvg_png(
+    "plots/figure_2_schema.png", 
+    width = 1000
+  )
