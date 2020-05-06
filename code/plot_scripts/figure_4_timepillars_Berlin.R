2917456910407e134aba37307b774a585bea28e7
Clemens Schmid <clemens@nevrome.de>
2020-05-06 11:34:33 +0200

    first complete draft of timepillars plot

--- code/plot_scripts/figure_4_timepillars_Berlin.R
+++ code/plot_scripts/figure_4_timepillars_Berlin.R
@@ -18,7 +18,6 @@ load("data/gpr/interpol_grid_Berlin.RData")
 #### filter ####
 poi_timeseries <- interpol_grid %>%
   dplyr::filter(
-    dependent_var_id %in% c("PC1", "PC2"),
     z %% 500 == 0
   ) %>% 
   tidyr::pivot_wider(
@@ -27,8 +26,19 @@ poi_timeseries <- interpol_grid %>%
     values_from = c("mean", "sd")
   )
 
+poi_timeseries_large_kernel <- poi_timeseries %>% dplyr::filter(
+  kernel_setting_id == "ds1000_dt1000_g001"
+)
+poi_timeseries_medium_kernel <- poi_timeseries %>% dplyr::filter(
+  kernel_setting_id == "ds500_dt500_g001"
+)
+poi_timeseries_small_kernel <- poi_timeseries %>% dplyr::filter(
+ kernel_setting_id == "ds200_dt200_g001"
+)
+
+#### pca ####
+
 # large kernel
-poi_timeseries_large_kernel <- poi_timeseries %>% dplyr::filter(kernel_setting_id == "ds1000_dt1000_g001")
 p_pca_large_kernel <- ggplot() +
   geom_point(
     data = pca_ref,
@@ -113,7 +123,6 @@ p_pca_large_kernel <- ggplot() +
   coord_fixed()
 
 # medium kernel
-poi_timeseries_medium_kernel <- poi_timeseries %>% dplyr::filter(kernel_setting_id == "ds500_dt500_g001")
 p_pca_medium_kernel <- ggplot() +
   geom_point(
     data = pca_ref,
@@ -198,7 +207,6 @@ p_pca_medium_kernel <- ggplot() +
   coord_fixed()
 
 # small kernel
-poi_timeseries_small_kernel <- poi_timeseries %>% dplyr::filter(kernel_setting_id == "ds200_dt200_g001")
 p_pca_small_kernel <- ggplot() +
   geom_point(
     data = pca_ref,
@@ -285,3 +293,264 @@ p_pca_small_kernel <- ggplot() +
 p_pca_bottom <- cowplot::plot_grid(p_pca_medium_kernel, p_pca_small_kernel, nrow = 1)
 p_pca <- cowplot::plot_grid(p_pca_large_kernel, p_pca_bottom, ncol = 1, rel_heights = c(1, 0.7))
 
+#### MDS ####
+
+# large kernel
+p_mds_large_kernel <- ggplot() +
+  geom_point(
+    data = anno,
+    aes(x = C1, y = C2, color = region_id, shape = age_group_id),
+    alpha = 0.7,
+    size = 2
+  )  +
+  scale_color_manual(
+    values = c(
+      "Central Europe" = "#999999", 
+      "Iberia" = "#E69F00", 
+      "Eastern Europe" = "#56B4E9", 
+      "Britain and Ireland" = "#009E73", 
+      "Turkey" = "#871200",
+      "France" = "#F0E442", 
+      "Near East" = "#0072B2", 
+      "Caucasus" = "#D55E00", 
+      "Italy" = "#CC79A7", 
+      "Southeastern Europe" = "#2fff00"
+    )
+  ) +
+  scale_shape_manual(
+    values = c(
+      ">-8000" = 15,
+      "-8000 - -6000" = 15,
+      "-6000 - -4000" = 17,
+      "-4000 - -2000" = 6,
+      "-2000 - 0" = 4
+    )
+  ) +
+  guides(
+    color = guide_legend()
+  ) +
+  ggnewscale::new_scale_color() +
+  geom_path(
+    data = poi_timeseries_large_kernel,
+    aes(x = mean_C1, y = mean_C2)
+  ) +
+  geom_errorbar(
+    data = poi_timeseries_large_kernel,
+    aes(
+      x = mean_C1, 
+      ymin = mean_C2 - sd_C2, ymax = mean_C2 + sd_C2,
+      color = z
+    ),
+    alpha = 0.5
+  ) +
+  geom_errorbarh(
+    data = poi_timeseries_large_kernel,
+    aes(
+      y = mean_C2, 
+      xmin = mean_C1 - sd_C1, xmax = mean_C1 + sd_C1,
+      color = z
+    ),
+    alpha = 0.5
+  ) +
+  geom_point(
+    data = poi_timeseries_large_kernel,
+    aes(
+      x = mean_C1, 
+      y = mean_C2,
+      color = z
+    ),
+    size = 3
+  ) +
+  scale_color_gradient2(
+    limits = c(-7500, -500), low = "black", mid = "red", high = "green", midpoint = -5000,
+    breaks = seq(-7500, -500, 1000)
+  ) +
+  theme_bw() +
+  theme(
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 12),
+    legend.text = element_text(size = 12),
+    legend.title = element_blank(),
+    legend.position = "right"
+  ) +
+  coord_fixed() +
+  scale_y_reverse()
+
+# medium kernel
+p_mds_medium_kernel <- ggplot() +
+  geom_point(
+    data = anno,
+    aes(x = C1, y = C2, color = region_id, shape = age_group_id),
+    alpha = 0.7,
+    size = 2
+  )  +
+  scale_color_manual(
+    values = c(
+      "Central Europe" = "#999999", 
+      "Iberia" = "#E69F00", 
+      "Eastern Europe" = "#56B4E9", 
+      "Britain and Ireland" = "#009E73", 
+      "Turkey" = "#871200",
+      "France" = "#F0E442", 
+      "Near East" = "#0072B2", 
+      "Caucasus" = "#D55E00", 
+      "Italy" = "#CC79A7", 
+      "Southeastern Europe" = "#2fff00"
+    )
+  ) +
+  scale_shape_manual(
+    values = c(
+      ">-8000" = 15,
+      "-8000 - -6000" = 15,
+      "-6000 - -4000" = 17,
+      "-4000 - -2000" = 6,
+      "-2000 - 0" = 4
+    )
+  ) +
+  guides(
+    color = guide_legend()
+  ) +
+  ggnewscale::new_scale_color() +
+  geom_path(
+    data = poi_timeseries_medium_kernel,
+    aes(x = mean_C1, y = mean_C2)
+  ) +
+  geom_errorbar(
+    data = poi_timeseries_medium_kernel,
+    aes(
+      x = mean_C1, 
+      ymin = mean_C2 - sd_C2, ymax = mean_C2 + sd_C2,
+      color = z
+    ),
+    alpha = 0.5
+  ) +
+  geom_errorbarh(
+    data = poi_timeseries_medium_kernel,
+    aes(
+      y = mean_C2, 
+      xmin = mean_C1 - sd_C1, xmax = mean_C1 + sd_C1,
+      color = z
+    ),
+    alpha = 0.5
+  ) +
+  geom_point(
+    data = poi_timeseries_medium_kernel,
+    aes(
+      x = mean_C1, 
+      y = mean_C2,
+      color = z
+    ),
+    size = 3
+  ) +
+  scale_color_gradient2(
+    limits = c(-7500, -500), low = "black", mid = "red", high = "green", midpoint = -5000,
+    breaks = seq(-7500, -500, 1000)
+  ) +
+  theme_bw() +
+  theme(
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 12),
+    legend.text = element_text(size = 12),
+    legend.title = element_blank(),
+    legend.position = "none"
+  ) +
+  coord_fixed() +
+  scale_y_reverse()
+
+# small kernel
+p_mds_small_kernel <- ggplot() +
+  geom_point(
+    data = anno,
+    aes(x = C1, y = C2, color = region_id, shape = age_group_id),
+    alpha = 0.7,
+    size = 2
+  )  +
+  scale_color_manual(
+    values = c(
+      "Central Europe" = "#999999", 
+      "Iberia" = "#E69F00", 
+      "Eastern Europe" = "#56B4E9", 
+      "Britain and Ireland" = "#009E73", 
+      "Turkey" = "#871200",
+      "France" = "#F0E442", 
+      "Near East" = "#0072B2", 
+      "Caucasus" = "#D55E00", 
+      "Italy" = "#CC79A7", 
+      "Southeastern Europe" = "#2fff00"
+    )
+  ) +
+  scale_shape_manual(
+    values = c(
+      ">-8000" = 15,
+      "-8000 - -6000" = 15,
+      "-6000 - -4000" = 17,
+      "-4000 - -2000" = 6,
+      "-2000 - 0" = 4
+    )
+  ) +
+  guides(
+    color = guide_legend()
+  ) +
+  ggnewscale::new_scale_color() +
+  geom_path(
+    data = poi_timeseries_small_kernel,
+    aes(x = mean_C1, y = mean_C2)
+  ) +
+  geom_errorbar(
+    data = poi_timeseries_small_kernel,
+    aes(
+      x = mean_C1, 
+      ymin = mean_C2 - sd_C2, ymax = mean_C2 + sd_C2,
+      color = z
+    ),
+    alpha = 0.5
+  ) +
+  geom_errorbarh(
+    data = poi_timeseries_small_kernel,
+    aes(
+      y = mean_C2, 
+      xmin = mean_C1 - sd_C1, xmax = mean_C1 + sd_C1,
+      color = z
+    ),
+    alpha = 0.5
+  ) +
+  geom_point(
+    data = poi_timeseries_small_kernel,
+    aes(
+      x = mean_C1, 
+      y = mean_C2,
+      color = z
+    ),
+    size = 3
+  ) +
+  scale_color_gradient2(
+    limits = c(-7500, -500), low = "black", mid = "red", high = "green", midpoint = -5000,
+    breaks = seq(-7500, -500, 1000)
+  ) +
+  theme_bw() +
+  theme(
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 12),
+    legend.text = element_text(size = 12),
+    legend.title = element_blank(),
+    legend.position = "none"
+  ) +
+  coord_fixed() +
+  scale_y_reverse()
+
+p_mds_bottom <- cowplot::plot_grid(p_mds_medium_kernel, p_mds_small_kernel, nrow = 1)
+p_mds <- cowplot::plot_grid(p_mds_large_kernel, p_mds_bottom, ncol = 1, rel_heights = c(1, 0.7))
+
+#### merge plots ####
+
+p <- cowplot::plot_grid(p_pca, p_mds, ncol = 1)
+
+ggsave(
+  paste0("plots/figure_4_timepillars_Berlin.jpeg"),
+  plot = p,
+  device = "jpeg",
+  scale = 1,
+  dpi = 300,
+  width = 300, height = 600, units = "mm",
+  limitsize = F
+)
