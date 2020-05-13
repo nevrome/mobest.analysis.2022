ce6a0d3a6099b77ecbb8db259275b373379c5b0a
Clemens Schmid <clemens@nevrome.de>
2020-05-13 10:23:00 +0200

    further work on angle functions

--- code/experiments_with_angles.R
+++ code/experiments_with_angles.R
@@ -10,27 +10,26 @@ deg2vec <- function(x) {
   c(sin(deg2rad(x)), cos(deg2rad(x)))
 }
 
-a <- deg2vec(360)
-b <- deg2vec(270)
-d <- deg2vec(280)
-
-mean_vec_2 <- function(x) {
-  Reduce(`+`, x)/length(x)
-}
-
-x <- list(a, b, d)
-vec <- mean_vec_2(x)
-
 vec2deg <- function(x) {
   rad2deg(atan2(x[1], x[2]))
 }
 
-res <- vec2deg(vec)
-
-if (res < 0) {
-  schu <- 360+res
-} else {
-  schu <- res
+mean_vec <- function(x) {
+  y <- lapply(x, deg2vec)
+  Reduce(`+`, y)/length(y)
 }
+
+mean_deg <- function(x) {
+  res <- vec2deg(mean_vec(x))
+  if (res < 0) {
+    360 + res
+  } else {
+    res
+  }
+}
+
+mean_deg(c(10, 15, 355, 270, 180, 90, 90, 95, 115))
+
+mean_deg(c(360, 180))
 
 # mobest:::angle_between_along_360(hu)
