c761d10bc583b8ba00adaf9ac3e0ee8c66966cba
Clemens Schmid <clemens@nevrome.de>
2020-05-13 10:53:32 +0200

    some experiments with mean angle code - also submitted to Rosetta code

--- code/experiments_with_angles.R
+++ code/experiments_with_angles.R
@@ -28,8 +28,27 @@ mean_deg <- function(x) {
   }
 }
 
-mean_deg(c(10, 15, 355, 270, 180, 90, 90, 95, 115))
+mean_deg(c(350, 10))
+mean_deg(c(90, 180, 270, 360))
+mean_deg(c(10, 20, 30))
 
-mean_deg(c(360, 180))
+library(ggplot2)
 
-# mobest:::angle_between_along_360(hu)
+testplot <- function(a, b) {
+  ggplot() +
+    geom_point(data = a, mapping = aes(x = angle, y = r)) +
+    geom_point(data = b, mapping = aes(x = angle, y = r), color = "red", size = 5) +
+    coord_polar() +
+    xlim(0,360)
+}
+
+a <- tibble::tibble(angle = c(270, 10, 20, 200), r = 1)
+b <- tibble::tibble(angle = mean_deg(a$angle), r = 1)
+
+testplot(a, b)
+
+a <- tibble::tibble(angle = c(10, 15, 355, 270, 180, 90, 90, 95, 115), r = 1)
+b <- tibble::tibble(angle = mean_deg(a$angle), r = 1)
+
+testplot(a, b)
+
