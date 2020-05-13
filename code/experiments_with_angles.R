fa6b5c57770d1a1f32973501a020b2a06c8bda76
Clemens Schmid <clemens@nevrome.de>
2020-05-13 11:21:00 +0200

    outsourced angle functions to mobest

--- code/experiments_with_angles.R
+++ code/experiments_with_angles.R
@@ -1,37 +1,3 @@
-deg2rad <- function(x) {
-  x * pi/180
-}
-
-rad2deg <- function(x) {
-  x * 180/pi
-}
-
-deg2vec <- function(x) {
-  c(sin(deg2rad(x)), cos(deg2rad(x)))
-}
-
-vec2deg <- function(x) {
-  rad2deg(atan2(x[1], x[2]))
-}
-
-mean_vec <- function(x) {
-  y <- lapply(x, deg2vec)
-  Reduce(`+`, y)/length(y)
-}
-
-mean_deg <- function(x) {
-  res <- vec2deg(mean_vec(x))
-  if (res < 0) {
-    360 + res
-  } else {
-    res
-  }
-}
-
-mean_deg(c(350, 10))
-mean_deg(c(90, 180, 270, 360))
-mean_deg(c(10, 20, 30))
-
 library(ggplot2)
 
 testplot <- function(a, b) {
@@ -43,12 +9,12 @@ testplot <- function(a, b) {
 }
 
 a <- tibble::tibble(angle = c(270, 10, 20, 200), r = 1)
-b <- tibble::tibble(angle = mean_deg(a$angle), r = 1)
+b <- tibble::tibble(angle = mobest::mean_deg(a$angle), r = 1)
 
 testplot(a, b)
 
 a <- tibble::tibble(angle = c(10, 15, 355, 270, 180, 90, 90, 95, 115), r = 1)
-b <- tibble::tibble(angle = mean_deg(a$angle), r = 1)
+b <- tibble::tibble(angle = mobest::mean_deg(a$angle), r = 1)
 
 testplot(a, b)
 
