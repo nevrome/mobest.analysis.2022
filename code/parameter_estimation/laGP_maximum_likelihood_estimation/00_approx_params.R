be6d706b9248cac5bbace385982865ae6f630dda
Clemens Schmid <clemens@nevrome.de>
2020-06-16 12:09:21 +0200

    further work on mle parameter estimation

--- code/parameter_estimation/laGP_maximum_likelihood_estimation/00_approx_params.R
+++ code/parameter_estimation/laGP_maximum_likelihood_estimation/00_approx_params.R
@@ -8,15 +8,16 @@ anno <- anno_1240K_and_anno_1240K_HumanOrigins_final
 
 #### approximation with mleGPsep ####
 
+# data prep
 independent <- anno %>%
   dplyr::transmute(
     x = x/1000000,
     y = y/1000000,
     z = calage_center/1000
   )
-
 dependent <- anno$PC1
 
+# parameter estimation
 mleGPsep_out <- lapply(1:10, function(i) {
   da <- laGP::darg(list(mle = TRUE), independent)
   gp <- laGP::newGPsep(
@@ -36,6 +37,7 @@ mleGPsep_out <- lapply(1:10, function(i) {
   return(param_estimation)
 })
 
+# look at result parameters
 d <- lapply(mleGPsep_out, function(x) { x$d }) %>% do.call(rbind, .) %>% 
   tidyr::as_tibble() %>% 
   magrittr::set_colnames(c("dx", "dy", "dt")) %>%
@@ -57,20 +59,50 @@ its <- sapply(mleGPsep_out, function(x) { x$it })
 mean(its)
 2*sd(its)
 
-# gp
+#### approximation with mleGP ####
 
-gp <- laGP::newGP(
-  X = independent, 
-  Z = dependent, 
-  d = da$start,
-  g = 0.01,
-  dK = TRUE
-)
+scaling_factor_sequence <- c(seq(0.1, 0.9, 0.1), 1, seq(2, 10, 1))
 
-res <- laGP::mleGP(
-  gpi = gp,
-  param = "d",
-  tmin = da$min, tmax = da$max
-)
+# parameter estimation
+mleGP_out <- lapply(scaling_factor_sequence, function(scaling_factor) {
+  # data prep inside of parameter estimation, because different scaling factors are tested
+  independent <- anno %>%
+    dplyr::transmute(
+      x = x/1000000,
+      y = y/1000000,
+      z = calage_center/1000 * scaling_factor
+    )
+  dependent <- anno$PC1
+  da <- laGP::darg(list(mle = TRUE), independent)
+  gp <- laGP::newGP(
+    X = independent, 
+    Z = dependent, 
+    d = da$start,
+    g = 0.01,
+    dK = TRUE
+  )
+  param_estimation <- laGP::mleGP(
+    gpi = gp,
+    param = "d",
+    tmin = da$min, tmax = da$max, ab = da$ab
+  )
+  laGP::deleteGP(gp)
+  return(param_estimation)
+})
 
-res$d * 1000
+d <- sapply(mleGP_out, function(x) { x$d }) * 1000
+its <- sapply(mleGP_out, function(x) { x$it })
+
+library(ggplot2)
+tibble::tibble(
+  scaling_factor = scaling_factor_sequence,
+  scaling_factor_fractional = fractional::fractional(scaling_factor_sequence),
+  scaling_factor_label = factor(
+    as.character(as.character(scaling_factor_fractional)), 
+    levels = as.character(as.character(scaling_factor_fractional))
+  ),
+  d = d
+) %>% ggplot() +
+  geom_point(
+    aes(scaling_factor_label, d)
+  )
