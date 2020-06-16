5d1ad3377697793728c0b01d386027cf193ea935
Clemens Schmid <clemens@nevrome.de>
2020-06-16 15:26:23 +0200

    extra mle sep plot script

--- code/parameter_estimation/laGP_maximum_likelihood_estimation/00_approx_params.R
+++ code/parameter_estimation/laGP_maximum_likelihood_estimation/00_approx_params.R
@@ -128,32 +128,6 @@ mle_out <- rbind(mleGPsep_out, jmleGPsep_out) %>%
 
 save(mle_out, file = "data/parameter_exploration/mle_out.RData")
 
-#scp schmid@cdag2-new.cdag.shh.mpg.de:/projects1/coest_mobility/coest.interpol.2020/data/parameter_exploration/mle_out.RData ../parameter_exploration/mle_out.RData
-
-# library(ggplot2)
-
-# load("data/parameter_exploration/mle_out.RData")
-
-# mle_out %>%
-#   ggplot() +
-#   facet_wrap(
-#     mle_method~parameter,
-#     nrow = 2,
-#     ncol = 4,
-#     # rows = dplyr::vars(mle_method),
-#     # cols = dplyr::vars(parameter),
-#     scales = "free_y"
-#   ) +
-#   geom_boxplot(
-#     aes(x = ancestry_component, y = value, color = ancestry_component)
-#   ) +
-#   guides(color = F) +
-#   theme_bw() +
-#   xlab("ancestry_component") +
-#   ylab("estimated value")
-
-
-
 
 
 
---
+++ code/plot_scripts/figure_sup_4_mle_GPsep.R
@@ -0,0 +1,23 @@
+#scp schmid@cdag2-new.cdag.shh.mpg.de:/projects1/coest_mobility/coest.interpol.2020/data/parameter_exploration/mle_out.RData ../parameter_exploration/mle_out.RData
+
+# library(ggplot2)
+
+# load("data/parameter_exploration/mle_out.RData")
+
+mle_out %>%
+  ggplot() +
+  facet_wrap(
+    mle_method~parameter,
+    nrow = 2,
+    ncol = 4,
+    # rows = dplyr::vars(mle_method),
+    # cols = dplyr::vars(parameter),
+    scales = "free_y"
+  ) +
+  geom_jitter(
+    aes(x = ancestry_component, y = value, color = ancestry_component)
+  ) +
+  guides(color = F) +
+  theme_bw() +
+  xlab("ancestry_component") +
+  ylab("estimated value")
