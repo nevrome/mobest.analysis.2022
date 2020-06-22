4b63f26ab2df22f5d21d4f0ed37adb6a388a8c3a
Clemens Schmid <clemens@nevrome.de>
2020-06-22 17:28:43 +0200

    some improvements for the crossvalidation distance distribution plot

--- code/plot_scripts/figure_sup_6_crossvalidation_prediction_accuracy.R
+++ code/plot_scripts/figure_sup_6_crossvalidation_prediction_accuracy.R
@@ -1,10 +1,17 @@
 library(magrittr)
 library(ggplot2)
 
+load("data/anno_1240K_and_anno_1240K_HumanOrigins_final.RData")
+anno <- anno_1240K_and_anno_1240K_HumanOrigins_final
 load("data/parameter_exploration/crossvalidation/interpol_comparison.RData")
 
 sample_interpol_comparison <- interpol_comparison %>%
   dplyr::sample_n(10000)
+
+sample_interpol_comparison$difference[sample_interpol_comparison$dependent_var == "PC1_dist"] %<>% `/`(dist(range(anno$PC1)))
+sample_interpol_comparison$difference[sample_interpol_comparison$dependent_var == "PC2_dist"] %<>% `/`(dist(range(anno$PC2)))
+sample_interpol_comparison$difference[sample_interpol_comparison$dependent_var == "C1_dist"] %<>% `/`(dist(range(anno$C1, na.rm = T)))
+sample_interpol_comparison$difference[sample_interpol_comparison$dependent_var == "C2_dist"] %<>% `/`(dist(range(anno$C2, na.rm = T)))
 
 p <- sample_interpol_comparison %>%
   ggplot(aes(y = dependent_var, x = difference, fill = 0.5 - abs(0.5 - stat(ecdf)))) +
@@ -19,8 +26,8 @@ p <- sample_interpol_comparison %>%
   ) +
   theme_bw() +
   scale_y_discrete(expand = expansion(mult = c(0.1, 0.5))) +
-  scale_x_continuous(limits = c(-0.05, 0.05)) +
-  xlab("Difference between prediction and measured ancestry component") +
+  scale_x_continuous(limits = c(-0.5, 0.5), breaks = seq(-0.5, 0.5, 0.1)) +
+  xlab("Normalitzed difference between prediction and measured ancestry component") +
   ylab("Density curve for each ancestry component")
 
 ggsave(
