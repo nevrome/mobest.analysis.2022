7f33dd83b6a1270f0a6e75ffa98c615fea9baf3b
Clemens Schmid <clemens@nevrome.de>
2020-06-23 15:45:27 +0200

    and a first draft of the final mleGP figure

--- code/plot_scripts/figure_sup_9_mle_GP.R
+++ code/plot_scripts/figure_sup_9_mle_GP.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 load("data/parameter_exploration/mle/mle_out.RData")
 
-p1 <- mleGP_out_df %>%
+p1 <- mle_out %>%
   tidyr::pivot_longer(cols = c(ds, dt), names_to = "d_type", values_to = "d_value") %>%
   ggplot() +
   geom_point(
@@ -12,9 +12,12 @@ p1 <- mleGP_out_df %>%
   theme_bw() +
   scale_y_continuous(sec.axis = sec_axis(~sqrt(.), name = latex2exp::TeX("$\\sqrt{\\theta}$"))) +
   ylab(latex2exp::TeX("$\\theta$")) +
-  xlab("Scaling factor")
+  xlab("") +
+  scale_color_discrete(
+    name = latex2exp::TeX("$\\theta$ scaling"), labels = list(latex2exp::TeX("$\\theta_s$"), latex2exp::TeX("$\\theta_t$"))
+  )
 
-p2 <- mleGP_out_df %>% ggplot() +
+p2 <- mle_out %>% ggplot() +
   geom_point(
     aes(x = scaling_factor_label, y = l)
   ) +
@@ -22,4 +25,14 @@ p2 <- mleGP_out_df %>% ggplot() +
   ylab("Gaussian process log likelihood") +
   xlab("Scaling factor")
 
-cowplot::plot_grid(p1, p2, nrow = 2, align = "v", axis = "lr")
+p <- cowplot::plot_grid(p1, p2, nrow = 2, align = "v", axis = "lr")
+
+ggsave(
+  "plots/figure_sup_9_mle_GP.jpeg",
+  plot = p,
+  device = "jpeg",
+  scale = 0.6,
+  dpi = 300,
+  width = 300, height = 150, units = "mm",
+  limitsize = F
+)
