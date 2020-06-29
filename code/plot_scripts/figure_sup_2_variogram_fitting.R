7189a88f88bd191f542446ab2423bd2dea45a45b
Clemens Schmid <clemens@nevrome.de>
2020-06-29 17:54:40 +0200

    reworked fig sup 2 to work with residuals

--- code/plot_scripts/figure_sup_2_variogram_fitting.R
+++ code/plot_scripts/figure_sup_2_variogram_fitting.R
@@ -6,12 +6,12 @@ load("data/parameter_exploration/variogram/all_distances.RData")
 d_binned <- d_all %>%
   dplyr::mutate(
     geo_dist_cut = cut(geo_dist, breaks = c(seq(0, max(geo_dist), 100), max(geo_dist)), include.lowest	= T, labels = F) * 100,
-    time_dist_cut = cut(time_dist, breaks = c(seq(0, max(time_dist), 500), max(time_dist)), include.lowest	= T, labels = F) * 500
+    time_dist_cut = cut(time_dist, breaks = c(seq(0, max(time_dist), 100), max(time_dist)), include.lowest	= T, labels = F) * 100
   ) %>%
   dplyr::group_by(geo_dist_cut, time_dist_cut) %>%
   dplyr::summarise(
     n = dplyr::n(),
-    mean_sq_pca_dist = mean(pca_dist^2)
+    PC1_resid = mean(PC1_dist_resid^2, na.rm = T),
   ) %>%
   dplyr::ungroup()
 
@@ -19,29 +19,29 @@ var_model <- function(cov0, rho, g, r) {
   2 * (cov0 - cov0*exp(-(r / rho)^2)) + g
 }
 
-p <- ggplot(dplyr::filter(d_binned, time_dist_cut == 500)) +
+p <- ggplot(dplyr::filter(d_binned, geo_dist_cut == 100)) +
   geom_line(
-    mapping = aes(x = geo_dist_cut, y = mean_sq_pca_dist)
+    mapping = aes(x = time_dist_cut, y = PC1_resid)
   ) +
   stat_function(
-    fun = function(r) {var_model(0.25, 35000, 0.001, r)}, 
+    fun = function(r) {var_model(0.25, 70000, 0.0001, r)}, 
     mapping = aes(color = "A"),
     linetype = "dashed", 
     size = 0.9
   ) + 
   stat_function(
-    fun = function(r) {var_model(25, 350000, 0.001, r)}, 
+    fun = function(r) {var_model(25, 700000, 0.0001, r)}, 
     mapping = aes(color = "B"),
     linetype = "dashed", 
     size = 1.2
   ) +
   scale_colour_manual("Models", values = c("red", "blue"), labels = list(
-    latex2exp::TeX("$Cov_0 = 0.25, \\rho = 35000$"),
-    latex2exp::TeX("$Cov_0 = 25, \\rho = 350000$")
+    latex2exp::TeX("$Cov_0 = 0.25, \\rho = 70000$"),
+    latex2exp::TeX("$Cov_0 = 25, \\rho = 700000$")
   )) +
   theme_bw() +
-  xlab("spatial distance: 100km bins") +
-  ylab("mean squared euclidean distance in PC1 & PC2 PCA space")
+  xlab("temporal distance: 100y bins") +
+  ylab("mean squared euclidean distance along PC1")
 
 ggsave(
   "plots/figure_sup_2_variogram_fitting.jpeg",
