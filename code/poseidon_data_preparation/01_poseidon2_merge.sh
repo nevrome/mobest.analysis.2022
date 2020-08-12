eb5570838d49b7ff29e9070723b5018d30ffcfed
Clemens Schmid <clemens@nevrome.de>
2020-08-12 14:57:44 +0200

    merge script

---
+++ code/poseidon_data_preparation/01_poseidon2_merge.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+poseidon2 merge /projects1/coest_mobility/coest.interpol.2020/code/poseidon_data_preparation/dataset_list.txt /projects1/coest_mobility/coest.interpol.2020/data/poseidon_data/poseidon_merged_dataset
+
