01097bcef77daffbd5549e2293d5a8acc7c82e0b
Clemens Schmid <clemens@nevrome.de>
2020-08-14 11:42:34 +0200

    updated basic plotting code

--- code/plot_scripts/3D_plot_basic.R
+++ code/plot_scripts/3D_plot_basic.R
@@ -1,18 +1,17 @@
 library(magrittr)
 
-load("data/anno_1240K_and_anno_1240K_HumanOrigins_filtered.RData")
-anno <- anno_1240K_and_anno_1240K_HumanOrigins_filtered
+load("data/poseidon_data/janno_final.RData")
 
-threed <- anno %>%
+threed <- janno_final %>%
   dplyr::transmute(
     x = x/1000, 
     y = y/1000,
-    z = calage_center,
-    color = viridis::viridis(50)[as.numeric(cut(PC1, breaks = 50))]
+    z = Date_BC_AD_Median_Derived,
+    color = viridis::viridis(50)[as.numeric(cut(C1, breaks = 50))]
   )
 
 # plot
-png(filename = "plots/3D_plot_basic_PC1.png", width = 22, height = 18, units = "cm", res = 300)
+png(filename = "plots/3D_plot_basic_C1.png", width = 22, height = 18, units = "cm", res = 300)
 
 scatterplot3d::scatterplot3d(
   threed$x, threed$y, threed$z, color = threed$color,
@@ -24,6 +23,21 @@ scatterplot3d::scatterplot3d(
   col.axis = "grey",
   zlim = c(-7500, -500),
   mar = c(3,3,0,2)
+)
+
+dev.off()
+
+
+png(filename = "plots/3D_plot_basic_C1.png", width = 22, height = 14, units = "cm", res = 300)
+
+s <- scatterplot3d::scatterplot3d(
+  threed$x, threed$y, threed$z, color = threed$color,
+  pch = 18, cex.symbols = 1.5,
+  angle = 70,
+  xlab = "x", ylab = "y", zlab = "time calBC",
+  col.axis = "grey",
+  zlim = c(-8000, 2000),
+  mar = c(2.7, 2.7, 0, 2)
 )
 
 dev.off()
