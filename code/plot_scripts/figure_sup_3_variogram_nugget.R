8fd5604c8b0721e188f86a82167d7a32a0c319b8
Clemens Schmid <clemens@nevrome.de>
2020-08-16 13:51:30 +0200

    update sup fig 3

--- code/plot_scripts/figure_sup_3_variogram_nugget.R
+++ code/plot_scripts/figure_sup_3_variogram_nugget.R
@@ -4,7 +4,7 @@ library(ggplot2)
 load("data/parameter_exploration/variogram/all_distances.RData")
 
 d_all_long <- d_all %>% tidyr::pivot_longer(
-  cols = c(PC1_dist_resid, PC2_dist_resid, C1_dist_resid, C2_dist_resid),
+  cols = c(C1_dist_resid, C2_dist_resid),
   names_to = "dist_type", values_to = "dist_val"
 )
 
@@ -12,11 +12,9 @@ lower_left <- d_all_long %>%
   dplyr::filter(time_dist < 50 & geo_dist < 50) %>%
   dplyr::filter(geo_dist != 0 & time_dist != 0) %>%
   dplyr::mutate(
-    dist_type = replace(dist_type, dist_type == "PC1_dist_resid", "PC1"),
-    dist_type = replace(dist_type, dist_type == "PC2_dist_resid", "PC2"),
     dist_type = replace(dist_type, dist_type == "C1_dist_resid", "C1"),
     dist_type = replace(dist_type, dist_type == "C2_dist_resid", "C2"),
-    dist_type = factor(dist_type, levels = c("PC1", "PC2", "C1", "C2"))
+    dist_type = factor(dist_type, levels = c("C1", "C2"))
   )
 
 lower_left_median <- lower_left %>%
@@ -62,6 +60,6 @@ ggsave(
   device = "jpeg",
   scale = 0.6,
   dpi = 300,
-  width = 300, height = 200, units = "mm",
+  width = 300, height = 120, units = "mm",
   limitsize = F
 )
