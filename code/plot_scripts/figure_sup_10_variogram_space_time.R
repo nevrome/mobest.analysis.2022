b70ecfbd7d1664f1bbc70ab3d9787aa620314684
Clemens Schmid <clemens@nevrome.de>
2020-08-17 12:09:57 +0200

    update sup figure 10

--- code/plot_scripts/figure_sup_10_variogram_space_time.R
+++ code/plot_scripts/figure_sup_10_variogram_space_time.R
@@ -4,7 +4,7 @@ library(magrittr)
 load("data/parameter_exploration/variogram/all_distances.RData")
 
 d_all_long <- d_all %>% tidyr::pivot_longer(
-  cols = c(PC1_dist_resid, PC2_dist_resid, C1_dist_resid, C2_dist_resid),
+  cols = c(C1_dist_resid, C2_dist_resid),
   names_to = "dist_type", values_to = "dist_val"
 ) #%>%
   # dplyr::mutate(
@@ -15,11 +15,9 @@ bottom_space <- d_all_long %>%
   dplyr::filter(time_dist < 50) %>%
   dplyr::filter(geo_dist != 0 & time_dist != 0) %>%
   dplyr::mutate(
-    dist_type = replace(dist_type, dist_type == "PC1_dist_resid", "PC1"),
-    dist_type = replace(dist_type, dist_type == "PC2_dist_resid", "PC2"),
     dist_type = replace(dist_type, dist_type == "C1_dist_resid", "C1"),
     dist_type = replace(dist_type, dist_type == "C2_dist_resid", "C2"),
-    dist_type = factor(dist_type, levels = c("PC1", "PC2", "C1", "C2")),
+    dist_type = factor(dist_type, levels = c("C1", "C2")),
     geo_dist_cut = (cut(
       geo_dist, 
       breaks = seq(0, max(geo_dist), 100), 
@@ -38,11 +36,9 @@ left_time <- d_all_long %>%
   dplyr::filter(geo_dist < 50) %>%
   dplyr::filter(geo_dist != 0 & time_dist != 0) %>%
   dplyr::mutate(
-    dist_type = replace(dist_type, dist_type == "PC1_dist_resid", "PC1"),
-    dist_type = replace(dist_type, dist_type == "PC2_dist_resid", "PC2"),
     dist_type = replace(dist_type, dist_type == "C1_dist_resid", "C1"),
     dist_type = replace(dist_type, dist_type == "C2_dist_resid", "C2"),
-    dist_type = factor(dist_type, levels = c("PC1", "PC2", "C1", "C2")),
+    dist_type = factor(dist_type, levels = c("C1", "C2")),
     time_dist_cut = (cut(
       time_dist, 
       breaks = seq(0, max(time_dist), 100), 
@@ -84,7 +80,7 @@ p_space <- ggplot() +
     color = FALSE
   ) + 
   xlab("spatial distance in kilometres") +
-  ylab("distance along a certain ancestry component")
+  ylab("ancestry component distance      ")
   
 
 p_time <- ggplot(left_time) + 
@@ -111,7 +107,7 @@ p_time <- ggplot(left_time) +
     color = FALSE
   ) +
   xlab("temporal distance in years") +
-  ylab("distance along a certain ancestry component")
+  ylab("ancestry component distance      ")
 
 
 p <- cowplot::plot_grid(p_space, p_time, nrow = 1, labels = c("A", "B"))
@@ -122,6 +118,6 @@ ggsave(
   device = "jpeg",
   scale = 0.6,
   dpi = 300,
-  width = 400, height = 200, units = "mm",
+  width = 400, height = 100, units = "mm",
   limitsize = F
 )
