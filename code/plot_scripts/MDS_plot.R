61fc68261b56c2363692aeea539a5c4e9ba56129
Clemens Schmid <clemens@nevrome.de>
2020-08-19 17:20:09 +0200

    a simple mds plot

---
+++ code/plot_scripts/MDS_plot.R
@@ -0,0 +1,47 @@
+library(magrittr)
+library(ggplot2)
+
+load("data/poseidon_data/janno_final.RData")
+load("data/plot_reference_data/region_id_colors.RData")
+load("data/plot_reference_data/age_group_id_shapes.RData")
+
+p_mds <- ggplot() +
+  geom_point(
+    data = janno_final,
+    aes(x = C1, y = C2, color = region_id, shape = age_group_id),
+    alpha = 0.7,
+    size = 2
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+    legend.background = element_blank(),
+    legend.title = element_text(size = 13),
+    legend.spacing.x = unit(0.2, 'cm'),
+    legend.key.height = unit(0.4, 'cm'),
+    legend.text = element_text(size = 9),
+  ) +
+  guides(color = guide_legend(override.aes = list(size = 2))) +
+  scale_shape_manual(
+    values = age_group_id_shapes
+  ) +
+  scale_color_manual(
+    values = region_id_colors
+  ) +
+  guides(
+    color = guide_legend(title = "Region", nrow = 3, ncol = 4),
+    shape = guide_legend(title = "Time", nrow = 3, ncol = 4)
+  ) +
+  coord_fixed(xlim = c(-0.05, 0.09), ylim = c(-0.07, 0.065)) +
+  scale_y_continuous(breaks = seq(-0.08, 0.08, 0.02)) +
+  scale_x_continuous(breaks = seq(-0.06, 0.08, 0.02))
+
+ggsave(
+  paste0("plots/MDS_plot.jpeg"),
+  plot = p_mds,
+  device = "jpeg",
+  scale = 0.5,
+  dpi = 300,
+  width = 570, height = 600, units = "mm",
+  limitsize = F
+)
