3c8f86c49f0c4e61bb9fab08f1a5f9e52346cf93
Clemens Schmid <clemens@nevrome.de>
2020-08-24 17:30:19 +0200

    work on the C1 postdiction map matrix plot

--- code/plot_scripts/presentations/interpolation_map_matrix.R
+++ code/plot_scripts/presentations/interpolation_map_matrix.R
@@ -1,7 +1,7 @@
 library(magrittr)
 library(ggplot2)
 
-load("data/gpr/interpol_grid.RData")
+load("data/gpr/interpol_grid_median.RData")
 load("data/spatial/research_area.RData")
 load("data/spatial/extended_area.RData")
 load("data/plot_reference_data/age_group_id_shapes.RData")
@@ -12,7 +12,7 @@ janno_final <- janno_final %>%
   dplyr::mutate(
     z = as.numeric(as.character(cut(
       Date_BC_AD_Median_Derived, 
-      breaks = seq(-7500, 1500, 1000), 
+      breaks = c(8000, seq(-6500, 500, 1000), 2000), 
       labels = seq(-7000, 1000, 1000),
       include.lowest = T
     )))
@@ -21,26 +21,26 @@ janno_final <- janno_final %>%
     z %in% seq(-7000, 0, 1000)
   )
 
+igprep <- interpol_grid %>%
+  dplyr::filter(
+    dependent_var_id %in% "C1",
+    kernel_setting_id == "ds600_dt300_g001",
+    z %in% seq(-7000, 0, 1000)
+  ) 
+
 ex <- raster::extent(research_area)
 xlimit <- c(ex[1], ex[2])
 ylimit <- c(ex[3], ex[4])
 
-p <- interpol_grid %>%
-  dplyr::filter(
-    independent_table_id == "age_sample_2",
-    dependent_var_id %in% "C1",
-    kernel_setting_id == "ds800_dt1400_g001",
-    pred_grid_id == "scs100_tl100",
-    z %in% seq(-7000, 0, 1000)
-  ) %>%
+p <- igprep %>%
   ggplot() +
   geom_sf(data = extended_area, fill = "white") +
   geom_raster(aes(x, y, fill = mean)) +
   facet_wrap(dplyr::vars(z), nrow = 2) +
   geom_sf(data = extended_area, fill = NA, colour = "black") +
   geom_point(
-    data = . %>% dplyr::filter(sd > (0.2 * diff(range(mean)))),
-    aes(x, y), alpha = 0.8, color = "red", shape = 4
+    data = . %>% dplyr::filter(sd > (0.5 * diff(c(min(janno_final$C1), max(janno_final$C1))))),
+    aes(x, y), alpha = 1, color = "red", shape = 4
   ) +
   geom_point(
     data = janno_final,
@@ -52,7 +52,10 @@ p <- interpol_grid %>%
     values = age_group_id_shapes,
     guide = FALSE
   ) +
-  scale_fill_viridis_c() +
+  scale_fill_viridis_c(
+    limits = c(min(janno_final$C1), max(janno_final$C1)), 
+    oob = scales::squish
+  ) +
   theme_bw() +
   coord_sf(
     xlim = xlimit, ylim = ylimit,
