d8fdde9999061cc3b294c7b1ac3bd1c6d514bf4e
Clemens Schmid <clemens@nevrome.de>
2020-09-08 17:41:13 +0200

    experiments with very specific wiggle exploration

--- code/mobility_estimation/03_mobility_estimation_pipeline_median.R
+++ code/mobility_estimation/03_mobility_estimation_pipeline_median.R
@@ -77,6 +77,29 @@ interpol_grid <- mobest::unnest_model_grid(model_grid_result)
 
 interpol_grid_origin <- mobest::search_spatial_origin(interpol_grid, steps = 4)
 
+gb <- interpol_grid_origin %>%
+  sf::st_as_sf(
+    coords = c("x", "y"),
+    crs = sf::st_crs(mobility_regions),
+    remove = FALSE
+  ) %>%
+  sf::st_intersection(mobility_regions) %>%
+  sf::st_drop_geometry() %>%
+  dplyr::filter(
+    region_id == "Britain and Ireland"
+  )
+
+load("data/spatial/extended_area.RData")
+
+gb %>% dplyr::filter(
+  z > -4000 & z < -3500
+) %>%
+  ggplot() +
+  geom_sf(data = extended_area) +
+  geom_raster(aes(x, y, fill = mean_C1)) +
+  geom_segment(aes(x, y, xend = x_origin, yend = y_origin)) +
+  facet_wrap(~z)
+
 # library(ggplot2)
 # interpol_grid_origin %>%
 #   dplyr::filter(
@@ -95,52 +118,40 @@ mobility <- mobility_proxy
  
 save(mobility_proxy, file = paste0("data/mobility_estimation/mobility_proxy_median.RData"))
 
-# mobility_proxy$region_id = factor(mobility_proxy$region_id, levels = c(
-#   "Britain and Ireland",
-#   "France",
-#   "Iberia",
-#   "Italy",
-#   "Central Europe",
-#   "Eastern Europe",
-#   "Southeastern Europe",
-#   "Turkey",
-#   "Caucasus",
-#   "Near East"
-# ))
-# 
-# mobility_proxy %>%
-#   dplyr::group_by(kernel_setting_id, region_id) %>%
-#   dplyr::arrange(z, .by_group = T) %>%
-#   dplyr::mutate(
-#     movavg = slider::slide_dbl(mean_km_per_decade, mean, .before = 4, .after = 4)
-#   ) %>%
-#   ggplot() +
-#   geom_line(
-#     aes(
-#       x = z, y = mean_km_per_decade,
-#       group = interaction(independent_table_id, kernel_setting_id),
-#       color = angle_deg
-#     ),
-#     alpha = 0.5
-#   ) +
-#   geom_line(
-#     aes(
-#       x = z, y = movavg,
-#       group = interaction(independent_table_id, kernel_setting_id),
-#     ),
-#     color = "blue"
-#   ) +
-#   facet_grid(cols = dplyr::vars(region_id), rows = dplyr::vars(kernel_setting_id)) +
-#   theme_bw() +
-#   theme(
-#     legend.position = "bottom",
-#     axis.text.x = element_text(angle = 40, hjust = 1),
-#     strip.background = element_rect(fill = NA)
-#   ) +
-#   xlab("time calBC/calAD [y]") +
-#   ylab("\"Speed\" [km/decade]") +
-#   scale_color_gradientn(
-#     colours = c("#F5793A", "#85C0F9", "#85C0F9", "#A95AA1", "#A95AA1", "#33a02c", "#33a02c", "#F5793A"),
-#     guide = F
-#   ) +
-#   scale_x_continuous(breaks = c(-7000, -5000, -3000, -1000, 1000))
+mobility_proxy %>%
+  dplyr::group_by(kernel_setting_id, region_id) %>%
+  dplyr::arrange(z, .by_group = T) %>%
+  dplyr::mutate(
+    movavg = slider::slide_dbl(mean_km_per_decade, mean, .before = 4, .after = 4)
+  ) %>%
+  ggplot() +
+  geom_line(
+    aes(
+      x = z, y = mean_km_per_decade,
+      group = interaction(independent_table_id, kernel_setting_id),
+      color = angle_deg
+    ),
+    alpha = 0.5
+  ) +
+  geom_line(
+    aes(
+      x = z, y = movavg,
+      group = interaction(independent_table_id, kernel_setting_id),
+    ),
+    color = "blue"
+  ) +
+  #facet_grid(cols = dplyr::vars(region_id), rows = dplyr::vars(kernel_setting_id)) +
+  facet_wrap(dplyr::vars(region_id)) +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+    axis.text.x = element_text(angle = 40, hjust = 1),
+    strip.background = element_rect(fill = NA)
+  ) +
+  xlab("time calBC/calAD [y]") +
+  ylab("\"Speed\" [km/decade]") +
+  scale_color_gradientn(
+    colours = c("#F5793A", "#85C0F9", "#85C0F9", "#A95AA1", "#A95AA1", "#33a02c", "#33a02c", "#F5793A"),
+    guide = F
+  ) +
+  scale_x_continuous(breaks = c(-7000, -5000, -3000, -1000, 1000))
