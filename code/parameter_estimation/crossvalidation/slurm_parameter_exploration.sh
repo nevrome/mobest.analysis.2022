b02082fd75f624f71e37b48c3b7b42be79e313be
Clemens Schmid <clemens@nevrome.de>
2020-09-10 13:33:56 +0200

    new crossvalidation experiment

--- code/parameter_estimation/crossvalidation/slurm_parameter_exploration.sh
+++ code/parameter_estimation/crossvalidation/slurm_parameter_exploration.sh
@@ -5,12 +5,12 @@
 #SBATCH --mem=10G                                         # memory pool for all cores
 #SBATCH -o /projects1/coest_mobility/log/%j.out           # STDOUT (the standard output stream) into file <JOB_NUMBER>.out
 #SBATCH -e /projects1/coest_mobility/log/%j.err           # STDERR (the output stream for errors) into file <JOB_NUMBER>.err
-#SBATCH --array 0-99%5
+#SBATCH --array 0-9%5
 #SBATCH -J "cross"
 
 # parameters
-g_to_explore=(0.01)
-dt_to_explore=($(seq 100 100 10000))
+g_to_explore=(0.1)
+dt_to_explore=($(seq 100 100 1000))
 
 jobs=$((${#g_to_explore[@]}*${#dt_to_explore[@]}))
 
