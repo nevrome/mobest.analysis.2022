e183f51a3b264b64f71487dadbccdffe65551dce
Clemens Schmid <clemens@nevrome.de>
2020-09-29 12:42:54 +0200

    updated isotropic estimation plot

--- code/plot_scripts/paper/figure_sup_9_mle_GP.R
+++ code/plot_scripts/paper/figure_sup_9_mle_GP.R
@@ -4,26 +4,33 @@ library(ggplot2)
 load("data/parameter_exploration/mle/mle_out.RData")
 
 p1 <- mle_out %>%
-  #tidyr::pivot_longer(cols = c(ds, dt), names_to = "d_type", values_to = "d_value") %>%
   ggplot() +
   geom_point(
     aes(x = scaling_factor_label, y = d),
   ) +
+  geom_text(
+    aes(x = scaling_factor_label, y = d, label = round(d)),
+    angle = 90, hjust = 1.5, size = 3
+  ) +
+  facet_wrap(~ancestry_component, scales = "free_y") +
   theme_bw() +
-  #scale_y_continuous(sec.axis = sec_axis(~sqrt(.), name = latex2exp::TeX("$\\sqrt{\\theta}$"))) +
   ylab(latex2exp::TeX("$\\sqrt{\\theta}$")) +
-  xlab("")# +
-  #scale_color_discrete(
-  #  name = latex2exp::TeX("$\\theta$ scaling"), labels = list(latex2exp::TeX("$\\theta_s$"), latex2exp::TeX("$\\theta_t$"))
-  #)
+  xlab("Scaling factor") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 p2 <- mle_out %>% ggplot() +
   geom_point(
     aes(x = scaling_factor_label, y = l)
   ) +
+  geom_text(
+    aes(x = scaling_factor_label, y = l, label = round(l)),
+    angle = 90, hjust = 1.5, size = 3
+  ) +
+  facet_wrap(~ancestry_component, scales = "free_y") +
   theme_bw() +
   ylab("Gaussian process log likelihood") +
-  xlab("Scaling factor")
+  xlab("Scaling factor") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 p <- cowplot::plot_grid(p1, p2, nrow = 2, align = "v", axis = "lr")
 
@@ -31,8 +38,9 @@ ggsave(
   "plots/figure_sup_9_mle_GP.jpeg",
   plot = p,
   device = "jpeg",
-  scale = 0.6,
+  scale = 0.8,
   dpi = 300,
   width = 300, height = 150, units = "mm",
   limitsize = F
 )
+
