64a57b9d76d210acb10524851b06a6b8e974e34c
Clemens Schmid <clemens@nevrome.de>
2020-09-29 15:21:48 +0200

    update of covariance illustration plot

--- code/plot_scripts/paper/figure_sup_5_kernel_size_meaning.R
+++ code/plot_scripts/paper/figure_sup_5_kernel_size_meaning.R
@@ -4,8 +4,8 @@ library(ggplot2)
 kernel_theta <- function(distance, d) { exp(-(distance^2) / d) }
 
 kernel_theta_data <- expand.grid(
-  dist_p1_p2 = seq(0, 300, 1),
-  d = c(100, 2000, 5000, 20000)
+  dist_p1_p2 = seq(0, 2000, 1),
+  d = c(100, 500, 1000, 2000)^2
 )
 
 kernel_theta_data$k <- sapply(
@@ -17,10 +17,10 @@ kernel_theta_data$k <- sapply(
 
 kernel_theta_data$d_label <- as.factor(kernel_theta_data$d)
 levels(kernel_theta_data$d_label) <- c(
-  latex2exp::TeX("$\\theta = 100,\\; \\sqrt{\\theta} = 10$"),
-  latex2exp::TeX("$\\theta = 2000,\\; \\sqrt{\\theta} \\approx 44.7$"),
-  latex2exp::TeX("$\\theta = 5000,\\; \\sqrt{\\theta} \\approx 70.7$"),
-  latex2exp::TeX("$\\theta = 20000,\\; \\sqrt{\\theta} \\approx 141.4$")
+  latex2exp::TeX("$\\theta = 10000,\\; \\sqrt{\\theta} = 100$"),
+  latex2exp::TeX("$\\theta = 250000,\\; \\sqrt{\\theta} \\approx 500$"),
+  latex2exp::TeX("$\\theta = 1000000,\\; \\sqrt{\\theta} \\approx 1000$"),
+  latex2exp::TeX("$\\theta = 4000000,\\; \\sqrt{\\theta} \\approx 2000$")
 )
 
 p <- kernel_theta_data %>% 
