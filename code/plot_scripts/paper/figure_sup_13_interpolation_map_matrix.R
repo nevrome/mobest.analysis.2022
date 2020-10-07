833ab2b4b5b739258284c2af52385a6afe82f967
Clemens Schmid <clemens@nevrome.de>
2020-10-07 16:59:10 +0200

    attempt to plot both measures of speed next to each other

--- code/plot_scripts/paper/figure_sup_13_interpolation_map_matrix.R
+++ code/plot_scripts/paper/figure_sup_13_interpolation_map_matrix.R
@@ -9,7 +9,7 @@ load("data/plot_reference_data/age_group_id_shapes.RData")
 load("data/spatial/epsg102013.RData")
 load("data/poseidon_data/janno_final.RData")
 load("data/mobility_estimation/mobility_proxy_median.RData")
-
+load("data/mobility_estimation/alternative_mobility_proxy_median.RData")
 
 janno_final <- janno_final %>% 
   dplyr::mutate(
@@ -203,7 +203,43 @@ lapply(
     #   data = . %>% dplyr::filter(sd > (0.2 * diff(range(mean)))),
     #   aes(x, y), alpha = 0.8, color = "grey", shape = 4
     # ) +
-    scale_fill_viridis_c(option = "inferno", direction = -1) +
+    scale_fill_viridis_c(option = "inferno", direction = -1, limits = c(0, 500)) +
+    theme_bw() +
+    coord_sf(
+      xlim = xlimit, ylim = ylimit,
+      crs = epsg102013
+    ) +
+    guides(
+      fill = guide_colorbar(title = "Speed", barwidth = 10)
+    ) +
+    theme(
+      legend.position = "bottom",
+      legend.box = "horizontal",
+      legend.title = element_text(size = 17),
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      legend.text = element_text(size = 12),
+      strip.text = element_text(size = 12),
+      strip.background = element_rect(fill = NA),
+      axis.ticks = element_blank(),
+      panel.background = element_rect(fill = "#BFD5E3")
+    )
+  
+  p_alternative_speed <- alternative_mobility_proxy %>%
+    dplyr::filter(
+      kernel_setting_id == kernel_setting,
+      z %in% seq(-7500, 1500, 500)
+    ) %>%
+    ggplot() +
+    geom_sf(data = extended_area, fill = "white") +
+    geom_raster(aes(x, y, fill = speed_km_per_decade)) +
+    facet_wrap(dplyr::vars(z), ncol = 2) +
+    geom_sf(data = extended_area, fill = NA, colour = "black") +
+    # geom_point(
+    #   data = . %>% dplyr::filter(sd > (0.2 * diff(range(mean)))),
+    #   aes(x, y), alpha = 0.8, color = "grey", shape = 4
+    # ) +
+    scale_fill_viridis_c(option = "inferno", direction = -1, limits = c(0, 500)) +
     theme_bw() +
     coord_sf(
       xlim = xlimit, ylim = ylimit,
@@ -226,7 +262,7 @@ lapply(
     )
   
   # merge plots
-  p <- cowplot::plot_grid(p_Meas, p_C1, p_C2, p_Change, p_absolute_speed, p_absolute_speed, ncol = 6)
+  p <- cowplot::plot_grid(p_Meas, p_C1, p_C2, p_Change, p_absolute_speed, p_alternative_speed, ncol = 6)
   
   ggsave(
     paste0("plots/figure_sup_13_interpolation_map_matrix_", kernel_setting, ".jpeg"),
