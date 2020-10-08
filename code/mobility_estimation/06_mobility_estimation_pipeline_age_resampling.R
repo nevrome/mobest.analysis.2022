0d9753ebd6329df4a21ad522a6a290bdad0dc10d
Clemens Schmid <clemens@nevrome.de>
2020-10-08 17:44:49 +0200

    removing superfluous code

--- code/mobility_estimation/06_mobility_estimation_pipeline_age_resampling.R
+++ code/mobility_estimation/06_mobility_estimation_pipeline_age_resampling.R
@@ -5,7 +5,7 @@ library(magrittr)
 #### read parameters ####
 
 args <- unlist(strsplit(commandArgs(trailingOnly = TRUE), " "))
-#age_resampling_run <- 4
+age_resampling_run <- 3
 age_resampling_run <- as.numeric(args[1]) + 1
 
 #### data ####
@@ -28,46 +28,31 @@ model_grid <- mobest::create_model_grid(
     C2 = janno_final$C2
   ),
   kernel = list(
-    ds400_dt200_g001 = list(d = c(400000, 400000, 200), g = 0.01, on_residuals = T, auto = F),
-    ds600_dt300_g001 = list(d = c(600000, 600000, 300), g = 0.01, on_residuals = T, auto = F),
-    ds800_dt400_g001 = list(d = c(800000, 800000, 400), g = 0.01, on_residuals = T, auto = F)
+    ds550_dt1050_g006 = list(d = c(550000, 550000, 1050), g = 0.06, on_residuals = T, auto = F),
+    ds550_dt550_g006 = list(d = c(550000, 550000, 550), g = 0.06, on_residuals = T, auto = F),
+    ds1050_dt550_g006 = list(d = c(1050000, 1050000, 550), g = 0.06, on_residuals = T, auto = F)
   ),
   prediction_grid = list(
     scs100_tl100 = mobest::create_prediction_grid(
       area, 
+      mobility_regions,
       spatial_cell_size = 100000, 
-      time_layers = seq(-7500, 1500, 50)
+      time_layers = seq(-7600, 1500, 100)
     )
   )
 )
 
 #### run interpolation on model grid ####
 
-model_grid_result <- mobest::run_model_grid(model_grid)
-
-#### unnest prediction to get a point-wise prediction table ####
-
-interpol_grid <- mobest::unnest_model_grid(model_grid_result)
-
-#### remove points with too high SD ####
-
-# interpol_grid <- interpol_grid %>% dplyr::filter(
-#   ifelse(
-#     dependent_var_id == "C1",
-#     sd/diff(c(min(janno_final$C1), max(janno_final$C1))) < 0.5,
-#     sd/diff(c(min(janno_final$C2), max(janno_final$C2))) < 0.5
-#   )
-# )
+interpol_grid <- mobest::run_model_grid(model_grid)
 
 #### spatial origin ####
 
-interpol_grid_origin <- mobest::search_spatial_origin(interpol_grid, steps = 4)
-
-
+interpol_grid_origin <- mobest::search_spatial_origin(interpol_grid, steps = 1)
 
 #### mobility proxy ####
 
-mobility_proxy <- mobest::estimate_mobility(interpol_grid_origin, mobility_regions)
+mobility_proxy <- mobest::estimate_mobility(interpol_grid_origin)
 
 save(mobility_proxy, file = paste0("data/mobility_estimation/age_resampling/mobility_proxy_", age_resampling_run, ".RData"))
 
