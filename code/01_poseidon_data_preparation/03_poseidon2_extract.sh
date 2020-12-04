cd701c6057a543d75695837cd1b36a64af457aac
Clemens Schmid <clemens@nevrome.de>
2020-12-04 12:43:32 +0100

    update of extraction script

--- code/01_poseidon_data_preparation/03_poseidon2_extract.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -r /projects1/coest_mobility/coest.interpol.2020/data/poseidon_data/poseidon_extracted
-
-poseidon2 extract /projects1/coest_mobility/coest.interpol.2020/code/poseidon_data_preparation/ind_list.txt /projects1/coest_mobility/coest.interpol.2020/data/poseidon_data/poseidon_merged /projects1/coest_mobility/coest.interpol.2020/data/poseidon_data/poseidon_extracted
-
