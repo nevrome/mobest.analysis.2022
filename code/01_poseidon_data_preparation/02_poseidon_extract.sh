3a678254914dd7885deded540daf57551f91702f
Clemens Schmid <clemens@nevrome.de>
2020-12-06 12:56:28 +0100

    added convertf command as long as poseidon only returns eigenstrat data

--- code/01_poseidon_data_preparation/02_poseidon_extract.sh
+++ code/01_poseidon_data_preparation/02_poseidon_extract.sh
@@ -8,4 +8,7 @@ trident forge \
   -n poseidon_extracted \
   -o /projects1/coest_mobility/mobest.analysis.2020/data/poseidon_data/poseidon_extracted
 
+# so far poseidon does not support PLINK-Binary output
+convertf -p ../../../code/01_poseidon_data_preparation/par.extracted_eigenstrat_to_plinkbinary
+
 # sbatch -p long -c 1 --mem=20G -J "trident" --wrap="/projects1/coest_mobility/mobest.analysis.2020/code/01_poseidon_data_preparation/02_poseidon_extract.sh"
