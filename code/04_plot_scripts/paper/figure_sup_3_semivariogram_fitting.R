f0b9022c9166a355d0625c0f5807dbfba3719722
Clemens Schmid <clemens@nevrome.de>
2020-12-09 14:25:58 +0100

    removing duplicated code

--- code/04_plot_scripts/paper/figure_sup_3_semivariogram_fitting.R
+++ code/04_plot_scripts/paper/figure_sup_3_semivariogram_fitting.R
@@ -1,33 +1,14 @@
 library(ggplot2)
 
-load("data/parameter_exploration/variogram/all_distances.RData")
-
-# binning
-d_binned <- d_all %>%
-  dplyr::mutate(
-    geo_dist_cut = cut(
-      geo_dist, breaks = c(seq(0, max(geo_dist), 100), max(geo_dist)), 
-      include.lowest	= T, labels = F
-    ) * 100,
-    time_dist_cut = cut(
-      time_dist, breaks = c(seq(0, max(time_dist), 100), max(time_dist)), 
-      include.lowest	= T, labels = F
-    ) * 100
-  ) %>%
-  dplyr::group_by(geo_dist_cut, time_dist_cut) %>%
-  dplyr::summarise(
-    n = dplyr::n(),
-    C1_resid = 0.5*mean(C1_dist_resid^2, na.rm = T),
-  ) %>%
-  dplyr::ungroup()
+load("data/parameter_exploration/variogram/binned_distances.RData")
 
 var_model <- function(cov0, rho, g, r) {
   2 * (cov0 - cov0*exp(-(r / rho)^2)) + g
 }
 
-p <- ggplot(dplyr::filter(d_binned, geo_dist_cut == 500)) +
+p <- ggplot(dplyr::filter(d_binned, geo_dist_cut == 450)) +
   geom_line(
-    mapping = aes(x = time_dist_cut, y = C1_resid)
+    mapping = aes(x = time_dist_cut, y = C1_dist_resid)
   ) +
   stat_function(
     fun = function(r) {var_model(0.25, 100000, 0.0003, r)}, 
