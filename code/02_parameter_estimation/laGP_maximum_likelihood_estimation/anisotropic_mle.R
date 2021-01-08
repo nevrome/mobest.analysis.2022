f3fd9fae2d8d6f411b2c9a920a5015c36a3d2bc6
Clemens Schmid <clemens@nevrome.de>
2021-01-08 16:40:03 +0100

    outsourced mle code

--- code/02_parameter_estimation/laGP_maximum_likelihood_estimation/anisotropic_mle.R
+++ code/02_parameter_estimation/laGP_maximum_likelihood_estimation/anisotropic_mle.R
@@ -7,119 +7,39 @@ library(laGP)
 
 load("data/poseidon_data/janno_final.RData")
 
-iterations <- 50
-
 #### approximation with mleGPsep (anisotropic) ####
 
-mleGPsep_out <- lapply(c("C1", "C2"), function(ancestry_component) {
-  
-  # code fragment if not every every ancestry component is known for each observation
-  # .ancestry_component <- rlang::ensym(ancestry_component)
-  # janno_final_filtered <- janno_final %>% 
-  #   dplyr::select(x, y, Date_BC_AD_Median_Derived, !!.ancestry_component) %>% 
-  #   dplyr::filter(!is.na(!!.ancestry_component))
-  
-  independent <- janno_final %>%
-    dplyr::transmute(
-      x = x/1000000,
-      y = y/1000000,
-      z = Date_BC_AD_Median_Derived/1000
-    )
-  
-  dependent <- janno_final[[ancestry_component]]
-  
-  # parameter estimation
-  mleGPsep_params <- lapply(1:iterations, function(i) {
-    da <- laGP::darg(list(mle = TRUE), independent)
-    ga <- laGP::garg(list(mle = TRUE), dependent)
-    gp <- laGP::newGPsep(
-      X = independent, 
-      Z = dependent, 
-      d = da$start,
-      g = ga$start,
-      dK = TRUE
-    )
-    param_estimation <- laGP::mleGPsep(
-      gpsepi = gp,
-      param = "both",
-      tmin = c(da$min, ga$min), tmax = c(da$max, ga$max), ab = c(da$ab, ga$ab),
-      maxit = 200
-    )
-    laGP::deleteGPsep(gp)
-    return(param_estimation)
-  })
-  
-  # look at result parameters
-  lapply(mleGPsep_params, function(x) { 
-    tibble::tibble(
-      mle_method = "mleGPsep",
-      ancestry_component = ancestry_component,
-      dx = sqrt(x$theta[1]) * 1000, 
-      dy = sqrt(x$theta[2]) * 1000, 
-      dt = sqrt(x$theta[3]) * 1000, 
-      g = x$theta[4], 
-      its = x$its,
-      msg = x$msg,
-      conv = x$conv
-    )
-  }) %>% dplyr::bind_rows()
-  
-}) %>% dplyr::bind_rows()
-
+mleGPsep_out <- mobest::laGP_mle_anisotropic(
+  independent = mobest::create_spatpos(
+    id = janno_final$Individual_ID,
+    x = janno_final$x / 1000,
+    y = janno_final$y / 1000,
+    z = janno_final$Date_BC_AD_Median_Derived
+  ),
+  dependent = list(
+    C1 = janno_final$C1,
+    C2 = janno_final$C2
+  ),
+  iterations = 50
+)
 
 #### approximation with jmleGPsep (anisotropic) ####
 
-jmleGPsep_out <- lapply(c("C1", "C2"), function(ancestry_component) {
-  
-  independent <- janno_final %>%
-    dplyr::transmute(
-      x = x/1000000,
-      y = y/1000000,
-      z = Date_BC_AD_Median_Derived/1000
-    )
-  
-  dependent <- janno_final[[ancestry_component]]
-  
-  # parameter estimation
-  jmleGPsep_params <- lapply(1:iterations, function(i) {
-    da <- laGP::darg(list(mle = TRUE), independent)
-    ga <- laGP::garg(list(mle = TRUE), dependent)
-    gp <- laGP::newGPsep(
-      X = independent, 
-      Z = dependent, 
-      d = da$start,
-      g = ga$start,
-      dK = TRUE
-    )
-    param_estimation <- laGP::jmleGPsep(
-      gpsepi = gp,
-      drange = c(da$min, da$max),
-      grange = c(ga$min, ga$max),
-      dab = da$ab, 
-      gab = ga$ab,
-      maxit = 200
-    )
-    laGP::deleteGPsep(gp)
-    return(param_estimation)
-  })
-  
-  # look at result parameters
-  jmleGPsep_params %>% 
-    dplyr::bind_rows() %>%
-    dplyr::transmute(
-      mle_method = "jmleGPsep",
-      ancestry_component = ancestry_component,
-      dx = sqrt(d.1) * 1000, 
-      dy = sqrt(d.2) * 1000, 
-      dt = sqrt(d.3) * 1000, 
-      g = g, 
-      its = tot.its,
-      msg = NA,
-      conv = dconv
-    ) %>%
-    tibble::as_tibble()
-  
-}) %>% dplyr::bind_rows()
+jmleGPsep_out <- mobest::laGP_jmle_anisotropic(
+  independent = mobest::create_spatpos(
+    id = janno_final$Individual_ID,
+    x = janno_final$x / 1000,
+    y = janno_final$y / 1000,
+    z = janno_final$Date_BC_AD_Median_Derived
+  ),
+  dependent = list(
+    C1 = janno_final$C1,
+    C2 = janno_final$C2
+  ),
+  iterations = 50
+)
+
+#### merge result ####
 
 mlesep_out <- rbind(mleGPsep_out, jmleGPsep_out) %>%
   tidyr::pivot_longer(cols = c("dx", "dy", "dt", "g"), names_to = "parameter", values_to = "value") %>% 
