9006bbea29d7df440b805738dcc452ce495c6511
Clemens Schmid <clemens@nevrome.de>
2021-02-17 13:08:31 +0100

    new data collection

--- code/01_poseidon_data_preparation/01_janno_filter_for_relevant_individuals.R
+++ code/01_poseidon_data_preparation/01_janno_filter_for_relevant_individuals.R
@@ -3,20 +3,20 @@ library(magrittr)
 load("data/spatial/epsg102013.RData")
 load("data/spatial/research_area.RData")
 
-janno_raw <- poseidon2::read_janno("/projects1/poseidon/repo/ancient")
+janno_raw <- poseidonR::read_janno("/projects1/poseidon/repo/ancient")
 
-janno_age <- janno_raw %>% poseidon2::process_age()
-
-# lacking info filter
-janno_with_sufficient_info <- janno_age %>%
+# lacking spatial info filter
+janno_raw_spatial_positions <- janno_raw %>%
   dplyr::filter(
-    !is.na(Latitude) & !is.na(Longitude),
-    !is.na(Date_BC_AD_Median_Derived)
+    !is.na(Latitude) & !is.na(Longitude)
   )
 
-# temoral filter
-janno_age_filtered <- janno_with_sufficient_info %>% dplyr::filter(
-  Date_BC_AD_Median_Derived > -8000
+# process age information
+janno_age <- janno_raw_spatial_positions %>% poseidon2::process_age()
+
+# temporal filter
+janno_age_filtered <- janno_age %>% dplyr::filter(
+  !is.na(Date_BC_AD_Median_Derived) & Date_BC_AD_Median_Derived > -8000
 )
 
 # spatial filter 
@@ -28,6 +28,7 @@ janno_spatial <- janno_age_filtered %>%
   ) %>%
   sf::st_transform(epsg102013)
 
+# export for QGIS: Check if the research area is still adequate
 janno_spatial %>% 
   sf::write_sf(dsn = "data/poseidon_data/janno_spatial_pre_filter.gpkg", driver = "GPKG")
 
@@ -48,7 +49,9 @@ janno_spatial_filtered_non_sf <- janno_spatial_filtered %>%
 # Nr_autosomal_SNPs? Coverage_1240K? Endogenous?, Damage?, Xcontam?, mtContam?
 
 # Nr_autosomal_SNPs: should be >= 20000 SNPs
-janno_QC <- janno_spatial_filtered_non_sf %>% dplyr::filter(Nr_autosomal_SNPs >= 20000)
+janno_QC <- janno_spatial_filtered_non_sf %>% dplyr::filter(
+  Nr_autosomal_SNPs >= 20000
+)
 # Xcontam: if male, then should not be higher then 10%
 janno_QC <- janno_QC %>% dplyr::filter(
   is.na(Xcontam) | Genetic_Sex == "F" | (Genetic_Sex == "M" & Xcontam < 0.1)
@@ -61,14 +64,13 @@ janno_QC <- janno_QC %>% dplyr::filter(
   !grepl("cont|excluded", x = Individual_ID, ignore.case = T)
 )
 
-# Mehrfachmessungen an einem Individuum und Verwandte Individuen bleiben drin - schwer systematisch zu entfernen und theoretisch kein Problem
-# Coverage ist hier unwichtig
-# Nach damage sollte nicht gefiltert werden
+# Multiple samples from the same individual as well as biologically related individuals are not removed: This should not be an issue for this analysis
+# Coverage and damage are not too relevant here as filter criteria
 
 # prepare extract list for poseidon2 extract
 janno_filtered_final <- janno_QC
 
-# export for QGIS: check every now and then if the new data justifies a bigger/different research area
+# export for QGIS: check every now and then if the new data justifies different region definition
 janno_filtered_final %>% 
   sf::st_as_sf(
     coords = c("Longitude", "Latitude"), 
