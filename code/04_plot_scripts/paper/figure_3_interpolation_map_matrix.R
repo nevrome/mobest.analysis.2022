4b658a7ce4fc319b96ab34c7335079430c7256d4
Clemens Schmid <clemens@nevrome.de>
2021-03-04 16:00:49 +0100

    reworked plot grid for figure 3

--- code/04_plot_scripts/paper/figure_3_interpolation_map_matrix.R
+++ code/04_plot_scripts/paper/figure_3_interpolation_map_matrix.R
@@ -4,7 +4,6 @@ library(ggplot2)
 load("data/gpr/interpol_grid_median.RData")
 load("data/spatial/research_area.RData")
 load("data/spatial/extended_area.RData")
-load("data/plot_reference_data/age_group_id_shapes.RData")
 load("data/spatial/epsg102013.RData")
 load("data/poseidon_data/janno_final.RData")
 
@@ -13,7 +12,7 @@ janno_final <- janno_final %>%
     z = as.numeric(as.character(cut(
       Date_BC_AD_Median_Derived, 
       breaks = seq(-8000, 2000, 2000), 
-      labels = seq(-7000, 2000, 2000),
+      labels = seq(-7000, 1000, 2000),
       include.lowest = T
     )))
   )
@@ -25,14 +24,12 @@ ylimit <- c(ex[3], ex[4])
 p_C1 <- interpol_grid %>%
   dplyr::filter(
     dependent_var_id %in% "C1",
-    kernel_setting_id == .$kernel_setting_id[1],
-    pred_grid_id == .$pred_grid_id[1],
     z %in% seq(-7000, 1000, 2000)
   ) %>%
   ggplot() +
-  geom_sf(data = extended_area, fill = "white") +
+  geom_sf(data = extended_area, fill = "black") +
   geom_raster(aes(x, y, fill = mean)) +
-  facet_grid(rows = dplyr::vars(z), cols = dplyr::vars(dependent_var_id)) +
+  facet_grid(cols = dplyr::vars(z), rows = dplyr::vars(dependent_var_id)) +
   geom_sf(data = extended_area, fill = NA, colour = "black") +
   # geom_point(
   #   data = . %>% dplyr::filter(sd > (0.15 * diff(range(mean)))),
@@ -40,16 +37,12 @@ p_C1 <- interpol_grid %>%
   # ) +
   geom_point(
     data = janno_final,
-    aes(x, y, shape = age_group_id),
-    size = 1.5,
+    aes(x, y),
+    size = 0.5,
     color = "white"
   ) +
-  scale_shape_manual(
-    values = age_group_id_shapes,
-    guide = FALSE
-  ) +
   scale_fill_viridis_c(
-    breaks = c(-0.08, -0.04, 0, 0.04)
+    breaks = seq(-0.1, 0.1, 0.02)
   ) +
   theme_bw() +
   coord_sf(
@@ -57,16 +50,16 @@ p_C1 <- interpol_grid %>%
     crs = epsg102013
   ) +
   guides(
-    fill = guide_colorbar(title = "Prediction", barwidth = 10)
+    fill = guide_colorbar(title = "Prediction C1  ", barwidth = 25)
   ) +
   theme(
     legend.position = "bottom",
     legend.box = "horizontal",
-    legend.title = element_text(size = 17),
+    legend.title = element_text(size = 12),
     axis.title = element_blank(),
     axis.text = element_blank(),
     legend.text = element_text(size = 12),
-    strip.text = element_text(size = 17),
+    strip.text = element_text(size = 15),
     axis.ticks = element_blank(),
     panel.background = element_rect(fill = "#BFD5E3")
   )
@@ -74,14 +67,12 @@ p_C1 <- interpol_grid %>%
 p_C2 <- interpol_grid %>%
   dplyr::filter(
     dependent_var_id %in% "C2",
-    kernel_setting_id == .$kernel_setting_id[1],
-    pred_grid_id == .$pred_grid_id[1],
     z %in% seq(-7000, 1000, 2000)
   ) %>%
   ggplot() +
-  geom_sf(data = extended_area, fill = "white") +
+  geom_sf(data = extended_area, fill = "black") +
   geom_raster(aes(x, y, fill = mean)) +
-  facet_grid(rows = dplyr::vars(z), cols = dplyr::vars(dependent_var_id)) +
+  facet_grid(cols = dplyr::vars(z), rows = dplyr::vars(dependent_var_id)) +
   geom_sf(data = extended_area, fill = NA, colour = "black") +
   # geom_point(
   #   data = . %>% dplyr::filter(sd > (0.15 * diff(range(mean)))),
@@ -89,16 +80,12 @@ p_C2 <- interpol_grid %>%
   # ) +
   geom_point(
     data = janno_final,
-    aes(x, y, shape = age_group_id),
-    size = 1.5,
+    aes(x, y),
+    size = 0.5,
     color = "white"
   ) +
-  scale_shape_manual(
-    values = age_group_id_shapes,
-    guide = FALSE
-  ) +
   scale_fill_viridis_c(
-    breaks = c(-0.08, -0.04, 0, 0.04),
+    breaks = seq(-0.1, 0.1, 0.02),
     option = "magma"
   ) +
   theme_bw() +
@@ -107,31 +94,39 @@ p_C2 <- interpol_grid %>%
     crs = epsg102013
   ) +
   guides(
-    fill = guide_colorbar(title = "Prediction", barwidth = 10)
+    fill = guide_colorbar(title = "Prediction C2  ", barwidth = 25)
   ) +
   theme(
     legend.position = "bottom",
     legend.box = "horizontal",
-    legend.title = element_text(size = 17),
+    legend.title = element_text(size = 12),
     axis.title = element_blank(),
     axis.text = element_blank(),
     legend.text = element_text(size = 12),
-    strip.text = element_text(size = 17),
+    strip.text = element_text(size = 15),
     axis.ticks = element_blank(),
     panel.background = element_rect(fill = "#BFD5E3")
   )
   
 
 # merge plots
-p <- cowplot::plot_grid(p_C1, p_C2, ncol = 2)
+C1_legend <- cowplot::get_legend(p_C1)
+p_C1 <- p_C1 + theme(legend.position = "none")
+C2_legend <- cowplot::get_legend(p_C2)
+p_C2 <- p_C2 + theme(legend.position = "none")
+
+plots <- cowplot::plot_grid(p_C1, p_C2, nrow = 2)
+legends <- cowplot::plot_grid(C1_legend, C2_legend, nrow = 1)
+
+p <- cowplot::plot_grid(plots, legends, nrow = 2, rel_heights = c(1, 0.15))
 
 ggsave(
   "plots/figure_3_interpolation_map_matrix.jpeg",
   plot = p,
   device = "jpeg",
-  scale = 0.8,
+  scale = 0.5,
   dpi = 300,
-  width = 300, height = 420, units = "mm",
+  width = 800, height = 260, units = "mm",
   limitsize = F
 )
 
