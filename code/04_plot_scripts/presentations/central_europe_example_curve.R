5e0f51fb47a22f1bce3159073c829dd6c5a52abc
Clemens Schmid <clemens@nevrome.de>
2021-03-04 15:18:19 +0100

    xlim

--- code/04_plot_scripts/presentations/central_europe_example_curve.R
+++ code/04_plot_scripts/presentations/central_europe_example_curve.R
@@ -103,8 +103,9 @@ p_estimator <- ggplot() +
   ) +
   scale_x_continuous(breaks = seq(-7000, 1000, 1000)) +
   coord_cartesian(
-    ylim = c(0, 2300)
-    #ylim = c(0, max(origin_grid$spatial_distance, na.rm = T))
+    ylim = c(0, 2300),
+    #ylim = c(0, max(origin_grid$spatial_distance, na.rm = T)),
+    xlim = c(-7500, 1500)
   )
 
 #### direction legend ####
