1942ce3625f076cab4b9bd58c9f9c0f801760591
Clemens Schmid <clemens@nevrome.de>
2021-04-16 16:25:20 +0200

    started to create scheme for better cluster-client interaction

---
+++ code/technical_helpers.R
@@ -0,0 +1,34 @@
+basepath <- "/mnt/archgen/users/schmid/mobest.analysis.2020"
+
+cluster_up <- function(path) {
+  if (dir.exists(path)) { 
+    scp <- "scp -r"
+    to_copy <- file.path(basepath, dirname(path))
+  } else { 
+    scp <- "scp"
+    to_copy <- file.path(basepath, path)
+  }
+  rstudioapi::terminalExecute(paste0(
+    scp, " -oProxyJump=clemens_schmid@sshgw.eva.mpg.de ", 
+    path, 
+    " clemens_schmid@daghead1.eva.mpg.de:", 
+    to_copy
+  ))
+}
+
+cluster_down <- function(path) {
+  if (dir.exists(path)) {
+    scp <- "scp -r"
+    to_copy <- dirname(path)
+  } else { 
+    scp <- "scp"
+    to_copy <- path
+  }
+  rstudioapi::terminalExecute(paste0(
+    scp, " -oProxyJump=clemens_schmid@sshgw.eva.mpg.de ", 
+    "clemens_schmid@daghead1.eva.mpg.de:",
+    file.path(basepath, path), " ",
+    to_copy
+  ))
+}
+
