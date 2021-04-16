674e03b364c4d4e658ae49b3cd62ba3e99f29147
Clemens Schmid <clemens@nevrome.de>
2021-04-16 17:37:30 +0200

    and now for the origin search

---
+++ code/03_origin_search.R
@@ -0,0 +1,7 @@
+source("code/03_origin_search/01_interpolation_at specific_places_median_age+one_kernel_setting.R")
+
+source("code/03_origin_search/02_origin_search_pipeline-median_age+one_kernel_setting.R")
+
+cluster_qsub_script("code/03_origin_search/sge_origin_search.sh")
+source("code/03_origin_search/03b_merge_runs.R")
+source("code/03_origin_search/04_moving_window.R")
