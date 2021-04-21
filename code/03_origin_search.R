0a9caf112e20c0d5e0b1a46fa7ca45e57e7e9dec
Clemens Schmid <clemens@nevrome.de>
2021-04-21 17:23:22 +0200

    forgot result download in pipeline management script

--- code/03_origin_search.R
+++ code/03_origin_search.R
@@ -5,5 +5,7 @@ source("code/03_origin_search/01_interpolation_at_specific_places_median_age+one
 source("code/03_origin_search/02_origin_search_pipeline-median_age+one_kernel_setting.R")
 
 cluster_qsub_script("code/03_origin_search/sge_origin_search.sh")
+cluster_down("data/origin_search/age_resampling+one_kernel_setting")
 source("code/03_origin_search/03b_merge_runs.R")
+
 source("code/03_origin_search/04_moving_window.R")
