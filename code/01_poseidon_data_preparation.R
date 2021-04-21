b9572d828e1fc3a3e477c7d7b6ddb2bbfbf263ce
Clemens Schmid <clemens@nevrome.de>
2021-04-22 01:30:48 +0200

    forgot to upload spatial reference data

--- code/01_poseidon_data_preparation.R
+++ code/01_poseidon_data_preparation.R
@@ -3,6 +3,7 @@ source("code/technical_helpers.R")
 local_script("./code/01_poseidon_data_preparation/00_fetch_poseidon.sh")
 source("./code/01_poseidon_data_preparation/00_prepare_plot_reference_data.R")
 source("./code/01_poseidon_data_preparation/00_prepare_spatial_data.R")
+cluster_up("data/spatial")
 
 source("./code/01_poseidon_data_preparation/01_janno_filter_for_relevant_individuals.R")
 local_script("./code/01_poseidon_data_preparation/02_poseidon_extract.sh")
