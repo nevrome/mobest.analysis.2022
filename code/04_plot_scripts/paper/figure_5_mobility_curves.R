5f80c23ff734a047f1853e659ab605bfa002ef90
Clemens Schmid <clemens@nevrome.de>
2021-04-29 12:29:18 +0200

    simplified mobility curve plot for the paper

---
+++ code/04_plot_scripts/paper/figure_5_mobility_curves.R
@@ -0,0 +1,144 @@
+library(magrittr)
+library(ggplot2)
+
+#### data ####
+
+# curves
+load("data/poseidon_data/janno_final.RData")
+load("data/origin_search/origin_grid_median_modified.RData")
+load("data/origin_search/origin_grid_modified.RData")
+load("data/origin_search/moving_origin_grid.RData")
+load("data/origin_search/no_data_windows.RData")
+
+# maps
+load("data/spatial/mobility_regions.RData")
+load("data/spatial/research_area.RData")
+load("data/spatial/extended_area.RData")
+load("data/spatial/epsg3035.RData")
+load("data/plot_reference_data/region_id_shapes.RData")
+
+#### mobility estimator curves ####
+p_estimator <- ggplot() +
+  lemon::facet_rep_wrap(~region_id, ncol = 1, repeat.tick.labels = T) +
+  geom_rect(
+    data = no_data_windows,
+    mapping = aes(
+      ymax = Inf,
+      ymin = -Inf,
+      xmin = min_date_not_covered,
+      xmax = max_date_not_covered
+    ),
+    fill = "lightgrey"
+  ) +
+  geom_ribbon(
+    data = moving_origin_grid,
+    mapping = aes(
+      x = z,
+      ymin = undirected_mean_spatial_distance - 2*sd_spatial_distance,
+      ymax = undirected_mean_spatial_distance + 2*sd_spatial_distance
+    ),
+    fill = "lightgrey",
+    alpha = 0.3
+  ) +
+  geom_ribbon(
+    data = moving_origin_grid,
+    mapping = aes(
+      x = z,
+      ymin = undirected_mean_spatial_distance - 2*std_spatial_distance,
+      ymax = undirected_mean_spatial_distance + 2*std_spatial_distance
+    ),
+    fill = "lightgrey",
+  ) +
+  geom_line(
+    data = moving_origin_grid,
+    mapping = aes(x = z, y = undirected_mean_spatial_distance),
+    size = 0.4
+  ) +
+  geom_rect(
+    data = tibble::tibble(xmin = -Inf, ymin = -Inf, ymax = 0, xmax = Inf),
+    mapping = aes(
+      xmin = xmin, xmax = xmax,
+      ymin = ymin, ymax = ymax
+    ),
+    fill = "white"
+  ) +
+  geom_point(
+    data = origin_grid_median_modified,
+    mapping = aes(
+      x = search_z, y = spatial_distance, color = angle_deg
+    ),
+    alpha = 1,
+    size = 1.5,
+    shape = 4
+  ) +
+  geom_point(
+    data = janno_final %>% dplyr::filter(!is.na(region_id)),
+    aes(x = Date_BC_AD_Median_Derived, y = -100),
+    shape = "|"
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+  ) +
+  xlab("time in years calBC/calAD") +
+  ylab("spatial distance to \"link point\" (undirected mean) [km]") +
+  scale_color_gradientn(
+    colours = c("#F5793A", "#85C0F9", "#85C0F9", "#A95AA1", "#A95AA1", "#33a02c", "#33a02c", "#F5793A"),
+    na.value = NA,
+    guide = F
+  ) +
+  scale_x_continuous(breaks = seq(-7000, 1000, 1000)) +
+  coord_cartesian(
+    xlim = c(-7400, 1400),
+    ylim = c(-100, max(origin_grid_median_modified$spatial_distance, na.rm = T))
+  )
+
+#### direction legend ####
+
+p_legend <- tibble::tibble(
+  ID = letters[1:8],
+  angle_start = seq(0, 325, 45),
+  angle_stop = seq(45, 360, 45)
+) %>%
+  ggplot() + 
+  geom_rect(
+    aes(xmin = 3, xmax = 4, ymin = angle_start, ymax = angle_stop, fill = ID)
+  ) +
+  scale_fill_manual(
+    values = c("#F5793A", "#85C0F9", "#85C0F9", "#A95AA1", "#A95AA1", "#33a02c", "#33a02c", "#F5793A"), 
+    guide = FALSE
+  ) +
+  coord_polar(theta = "y") +
+  xlim(2, 4.5) +
+  scale_y_continuous(
+    breaks = c(0, 45, 90, 135, 180, 225, 270, 315),
+    labels = c("N", "NE", "E", "SE", "S", "SW", "W", "NW")
+  ) +
+  theme_minimal() +
+  theme(
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size = 10, colour = "black")
+  )
+
+
+#### compile plots ####
+
+p <- cowplot::ggdraw(p_estimator) +
+  cowplot::draw_plot(
+    p_legend,
+    x = 0.2, y = 0.83, 
+    width = 0.2, height = 0.2
+  )
+
+ggsave(
+  paste0("plots/figure_5_mobility_curves.png"),
+  plot = p,
+  device = "png",
+  scale = 0.5,
+  dpi = 300,
+  width = 300, height = 720, units = "mm",
+  limitsize = F
+)
+
