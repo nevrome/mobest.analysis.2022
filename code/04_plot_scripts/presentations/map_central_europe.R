d35971dadf60bbc432a7b6643d91be93ccb13bb6
Clemens Schmid <clemens@nevrome.de>
2021-04-30 13:30:21 +0200

    regional perspective map

--- code/04_plot_scripts/presentations/map_central_europe.R
+++ code/04_plot_scripts/presentations/map_central_europe.R
@@ -8,6 +8,17 @@ load("data/spatial/epsg3035.RData")
 load("data/plot_reference_data/region_id_shapes.RData")
 load("data/plot_reference_data/age_colors_gradient.RData")
 
+`%=NA=%` <- function(a, b) {
+  c <- a == b
+  c <- ifelse(is.na(c), FALSE, c)
+}
+
+janno_mod <- janno_final %>% 
+  dplyr::arrange(Date_BC_AD_Median_Derived) %>%
+  dplyr::mutate(
+    central_europe = region_id %=NA=% "Central Europe"
+  )
+
 # map
 p_map <- ggplot() +
   geom_sf(
@@ -19,12 +30,22 @@ p_map <- ggplot() +
     fill = NA, colour = "black", size = 0.8, linetype = "dashed"
   ) +
   geom_jitter(
-    data = janno_final %>% dplyr::arrange(Date_BC_AD_Median_Derived),
-    aes(x = x, y = y, color = Date_BC_AD_Median_Derived),
+    data = janno_mod %>% dplyr::filter(!central_europe),
+    aes(x = x, y = y),
+    color = "darkgrey",
     alpha = 1,
     width = 60000,
     height = 60000,
     shape = 3
+  ) +
+  geom_jitter(
+    data = janno_mod %>% dplyr::filter(central_europe),
+    aes(x = x, y = y, color = Date_BC_AD_Median_Derived),
+    size = 2,
+    alpha = 1,
+    width = 60000,
+    height = 60000,
+    shape = 16
   ) +
   theme_bw() +
   coord_sf(
@@ -49,14 +70,24 @@ p_map <- ggplot() +
   )
 
 # space time plot
-p_space_time <- ggplot(
-  data = janno_final,
-  aes(
-    x = Longitude, y = Date_BC_AD_Median_Derived, 
-    color = Date_BC_AD_Median_Derived
-  )
-) +
-  geom_point(shape = 3) +
+p_space_time <- ggplot() +
+  geom_point(
+    data = janno_mod %>% dplyr::filter(!central_europe),
+    mapping = aes(
+      x = Longitude, y = Date_BC_AD_Median_Derived
+    ),
+    color = "darkgrey",
+    shape = 3
+  ) +
+  geom_point(
+    data = janno_mod %>% dplyr::filter(central_europe),
+    mapping = aes(
+      x = Longitude, y = Date_BC_AD_Median_Derived, 
+      color = Date_BC_AD_Median_Derived
+    ),
+    shape = 16,
+    size = 3
+  ) +
   age_colors_gradient +
   theme_bw() +
   xlab("Longitude") +
@@ -72,11 +103,10 @@ p_space_time <- ggplot(
 
 # temporal distribution
 
-p_tempdist <- janno_final %>%
+p_tempdist <- janno_mod %>%
   ggplot() +
   geom_histogram(
-    aes(x = Date_BC_AD_Median_Derived),
-    fill = "grey",
+    aes(x = Date_BC_AD_Median_Derived, fill = central_europe),
     breaks = seq(-8000, 2000, 200)
   ) +
   theme_bw() +
@@ -92,9 +122,12 @@ p_tempdist <- janno_final %>%
     plot.margin = unit(c(5.5, 5.5, 5.5, 0), "points")
   ) +
   coord_flip(xlim = c(-7800, 1800)) +
-  # scale_fill_manual(
-  #   values = region_id_colors
-  # ) +
+  scale_fill_manual(
+    values = c(
+      "TRUE" = "black",
+      "FALSE" = "darkgrey"
+    )
+  ) +
   scale_x_continuous(breaks = seq(-8000, 2000, 1000)) +
   scale_y_continuous(breaks = c(0, 100, 200))
 
@@ -107,7 +140,7 @@ right <- cowplot::plot_grid(
 p <- cowplot::plot_grid(p_map, right, nrow = 1, ncol = 2, rel_widths = c(1, 0.3), scale = 0.97)
 
 ggsave(
-  paste0("plots/presentation/map_without_regions.jpeg"),
+  paste0("plots/presentation/map_central_europe.jpeg"),
   plot = p,
   device = "jpeg",
   scale = 0.6,
