0d37b1eefe9622db6de1c907fc75d441f7f927ea
Clemens Schmid <clemens@nevrome.de>
2021-05-02 16:45:51 +0200

    nicer distance maps for presentation

---
+++ code/04_plot_scripts/presentations/distance_example_maps.R
@@ -0,0 +1,62 @@
+library(magrittr)
+library(ggplot2)
+
+load("data/spatial/research_area.RData")
+load("data/spatial/extended_area.RData")
+load("data/spatial/epsg3035.RData")
+load("data/origin_search/janno_search.RData")
+load("data/origin_search/closest_points_examples.RData")
+load("data/origin_search/distance_grid_examples.RData")
+
+p <- ggplot() +
+  geom_sf(data = extended_area, fill = "black") +
+  geom_raster(
+    data = distance_grid_examples %>% dplyr::filter(Individual_ID == "3DT26.SG"),
+    mapping = aes(x = x, y = y, fill = gen_dist),
+  ) +
+  scale_fill_viridis_c(option = "mako", direction = -1) +
+  geom_sf(data = extended_area, fill = NA, colour = "black") +
+  geom_point(
+    data = janno_search %>% dplyr::filter(Individual_ID == "3DT26.SG"),
+    mapping = aes(x = x, y = y),
+    colour = "red",
+    size = 5
+  ) +
+  geom_point(
+    data = closest_points_examples %>% dplyr::filter(Individual_ID == "3DT26.SG"),
+    mapping = aes(x = x, y = y),
+    colour = "orange",
+    shape = "✖",
+    size = 7,
+    alpha = 0.7
+  ) +
+  theme_bw() +
+  coord_sf(
+    expand = FALSE,
+    crs = epsg3035
+  ) +
+  guides(
+    fill = guide_colorbar(title = "Genetic distance  ", barwidth = 25)
+  ) +
+  theme(
+    legend.position = "bottom",
+    legend.box = "horizontal",
+    legend.title = element_text(size = 15),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    legend.text = element_text(size = 15),
+    strip.text = element_text(size = 15),
+    axis.ticks = element_blank(),
+    panel.background = element_rect(fill = "#BFD5E3")
+  )
+
+ggsave(
+  "plots/presentation/distance_example_map_3DT.png",
+  plot = p,
+  device = "jpeg",
+  scale = 0.7,
+  dpi = 300,
+  width = 400, height = 300, units = "mm",
+  limitsize = F
+)
+
