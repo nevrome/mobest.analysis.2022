5032b67da16af2076f9afbcf701193fa2eee2df6
Clemens Schmid <clemens@nevrome.de>
2021-05-31 22:23:16 +0200

    some more love for workflow 	automatization

--- code/technical_helpers.R
+++ code/technical_helpers.R
@@ -1,10 +1,26 @@
 basepath <- "/mnt/archgen/users/schmid/mobest.analysis.2020"
 
+pw <- readLines("pw.txt")
+
+submit_pw_once <- function(termId) { rstudioapi::terminalSend(termId, paste0(pw, "\n")) }
+
+login <- function(termId) {
+  Sys.sleep(1)
+  submit_pw_once(termId)
+  Sys.sleep(1)
+  submit_pw_once(termId)
+}
+
 local_script <- function(path) {
-  rstudioapi::terminalExecute(paste0(
-    "cd ~/agora/mobest.analysis.2020 && ./",
-    path
-  ))
+  termId <- rstudioapi::terminalCreate()
+  rstudioapi::terminalSend(
+    termId,
+    paste0(
+      "cd ~/agora/mobest.analysis.2020 && ./",
+      path,
+      "\n"
+    )
+  )
 }
 
 cluster_script <- function(path) {
@@ -25,11 +41,17 @@ cluster_singularity_script <- function(path, cores = 16, memory = 50) {
 }
 
 cluster_run <- function(command) {
-  rstudioapi::terminalExecute(paste0(
-    "ssh -oProxyJump=clemens_schmid@sshgw.eva.mpg.de ", 
-    "clemens_schmid@daghead1.eva.mpg.de -t ",
-    "\'bash -l -c \"", command, "\"\'"
-  ))
+  termId <- rstudioapi::terminalCreate()
+  rstudioapi::terminalSend(
+    termId,
+    paste0(
+      "ssh -oProxyJump=clemens_schmid@sshgw.eva.mpg.de ", 
+      "clemens_schmid@daghead1.eva.mpg.de -t ",
+      "\'bash -l -c \"", command, "\"\'",
+      "\n"
+    )
+  )
+  login(termId)
 }
 
 cluster_up_one <- function(path) {
@@ -40,12 +62,18 @@ cluster_up_one <- function(path) {
     scp <- "scp"
     to_copy <- file.path(basepath, path)
   }
-  rstudioapi::terminalExecute(paste0(
-    scp, " -oProxyJump=clemens_schmid@sshgw.eva.mpg.de ", 
-    path, 
-    " clemens_schmid@daghead1.eva.mpg.de:", 
-    to_copy
-  ))
+  termId <- rstudioapi::terminalCreate()
+  rstudioapi::terminalSend(
+    termId,
+    paste0(
+      scp, " -oProxyJump=clemens_schmid@sshgw.eva.mpg.de ", 
+      path, 
+      " clemens_schmid@daghead1.eva.mpg.de:", 
+      to_copy,
+      "\n"
+    )
+  )
+  login(termId)
 }
 
 cluster_up <- function(...) {
@@ -61,12 +89,18 @@ cluster_down_one <- function(path) {
     scp <- "scp"
     to_copy <- path
   }
-  rstudioapi::terminalExecute(paste0(
-    scp, " -oProxyJump=clemens_schmid@sshgw.eva.mpg.de ", 
-    "clemens_schmid@daghead1.eva.mpg.de:",
-    file.path(basepath, path), " ",
-    to_copy
-  ))
+  termId <- rstudioapi::terminalCreate()
+  rstudioapi::terminalSend(
+    termId,
+    paste0(
+      scp, " -oProxyJump=clemens_schmid@sshgw.eva.mpg.de ", 
+      "clemens_schmid@daghead1.eva.mpg.de:",
+      file.path(basepath, path), " ",
+      to_copy,
+      "\n"
+    )
+  )
+  login(termId)
 }
 
 cluster_down <- function(...) {
