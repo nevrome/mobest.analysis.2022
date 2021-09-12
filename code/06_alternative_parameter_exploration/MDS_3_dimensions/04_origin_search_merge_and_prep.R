5e66e36f822c637bb787aec3d47a5e8fab7d6f67
Clemens Schmid <clemens@nevrome.de>
2021-09-12 16:13:43 +0200

    merged the two significantly smaller origin search data modification scripts

--- code/06_alternative_parameter_exploration/MDS_3_dimensions/04_origin_search_merge_and_prep.R
+++ code/06_alternative_parameter_exploration/MDS_3_dimensions/04_origin_search_merge_and_prep.R
@@ -1,5 +1,9 @@
 library(magrittr)
 
+#### data ####
+
+load("data/poseidon_data/janno_final.RData")
+
 #### load, merge and modify data ####
 
 # load resampling runs
@@ -32,6 +36,24 @@ origin_grid_modified <- origin_grid_modified %>%
     search_z_cut = age_groups_limits[cut(search_z, age_groups_limits, labels = F)] + 250,
   )
 
+# means for search points across age resampling
+origin_grid_mean <- mobest::average_origin_searchid(origin_grid_modified)
+
+# moving window
+moving_origin_grid <- mobest::average_origin_moving_window(
+  origin_grid_modified,
+  window_start = -8000,
+  window_stop = 2000,
+  window_width = 400,
+  window_step = 50
+)
+
+# no data windows
+no_data_windows <- mobest::no_data_windows(moving_origin_grid, window_step = 50)
+
 #### save output ####
 
 save(origin_grid_modified, file = "data/origin_search/origin_grid_modified_mds3.RData")
+save(origin_grid_mean, file = "data/origin_search/origin_grid_mean_mds3.RData")
+save(moving_origin_grid, file = "data/origin_search/moving_origin_grid_mds3.RData")
+save(no_data_windows, file = "data/origin_search/no_data_windows_mds3.RData")
