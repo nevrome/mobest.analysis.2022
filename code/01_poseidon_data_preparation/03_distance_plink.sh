795f81b73191e486e435de53131c8624996bb5bd
Clemens Schmid <clemens@nevrome.de>
2021-09-20 14:09:00 +0200

    update plink sge scripts

--- code/01_poseidon_data_preparation/03_distance_plink.sh
+++ code/01_poseidon_data_preparation/03_distance_plink.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #$ -S /bin/bash #defines bash as the shell for execution
-#$ -N mobest #Name of the command that will be listed in the queue
+#$ -N dist #Name of the command that will be listed in the queue
 #$ -cwd #change to current directory
 #$ -j y #join error and standard output in one file, no error file will be written
-#$ -o mobest_qsub_output #standard output file or directory (joined with error because of -j y)
+#$ -o ~/log #standard output file or directory (joined with error because of -j y)
 #$ -q archgen.q #queue
 #$ -pe smp 10 #needs 8 CPU cores
 #$ -l h_vmem=40G #request 4Gb of memory
