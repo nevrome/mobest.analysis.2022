4dd8dd925b5b19925b2cc397df857ce221ad167c
Clemens Schmid <clemens@nevrome.de>
2021-10-22 23:02:32 +0200

    singularity doesn't build in my normal tempdir

--- singularity_build_sif.sh
+++ singularity_build_sif.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo singularity build singularity_mobest.sif singularity_mobest.def
+sudo SINGULARITY_TMPDIR=$HOME/agora/mobest.analysis.2020/stempdir singularity build singularity_mobest.sif singularity_mobest.def
