ea05fbf9b19ca349b7d35b68dbbf0bf97087532d
Clemens Schmid <clemens@nevrome.de>
2021-10-23 14:12:23 +0200

    pipeline

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,6 +1,5 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.0 script --package shake
---{-# LANGUAGE BlockArguments,OverloadedStrings #-}
 
 import Development.Shake
 import Development.Shake.Command
@@ -13,6 +12,12 @@ code04Paper x = code "04_plot_scripts" </> "paper" </> x
 _data x = "data" </> x
 dataSpatial x = _data "spatial" </> x
 dataPlotReferenceData x = _data "plot_reference_data" </> x
+dataPoseidonData x = _data "poseidon_data" </> x
+dataPoseidonDataPoseidonExtracted x = dataPoseidonData "poseidon_extracted" </> x
+dataPoseidonDataMDS x = dataPoseidonData "mds" </> x
+
+rscript = cmd_ "./singularity_mobest.sif Rscript"
+bashscript = cmd_ "./singularity_mobest.sif"
 
 main :: IO ()
 main = shakeArgs shakeOptions {
@@ -26,8 +31,8 @@ main = shakeArgs shakeOptions {
     "plots" </> "figure_1_temporal_and_spatial_distribution_of_input_data.jpeg" %> \out -> do
         let script = code04Paper "figure_1_temporal_and_spatial_distribution_of_input_data.R"
             dataFiles = [
-                --   "poseidon_data" </> "janno_final.RData"
-                  dataSpatial "research_area.RData"
+                  dataPoseidonData "janno_final.RData"
+                , dataSpatial "research_area.RData"
                 , dataSpatial "extended_area.RData"
                 , dataSpatial "epsg3035.RData"
                 , dataSpatial "mobility_regions.RData"
@@ -35,7 +40,18 @@ main = shakeArgs shakeOptions {
                 , dataPlotReferenceData "age_colors_gradient.RData"
               ]
         need $ script : dataFiles
-        cmd_ "Rscript" script
+        rscript script
+
+    dataPoseidonData "janno_final.RData" %> \out -> do
+        let script = code04Paper "figure_1_temporal_and_spatial_distribution_of_input_data.R"
+            dataFiles = [
+                  dataSpatial "epsg3035.RData"
+                , dataSpatial "mobility_regions.RData"
+                , dataPoseidonDataPoseidonExtracted "poseidon_extracted.janno"
+                , dataPoseidonDataMDS "mds.mds"
+              ]
+        need $ script : dataFiles
+        rscript script
 
     map dataSpatial [
           "research_area.RData"
@@ -53,7 +69,18 @@ main = shakeArgs shakeOptions {
                 , "mobility_regions/mobility_regions.gpkg"
                 ]
         need $ script : dataFiles
-        cmd_ "Rscript" script
+        rscript script
+
+    dataPoseidonData "poseidon_extracted.janno" %> \out -> do
+        let script = code04Paper "figure_1_temporal_and_spatial_distribution_of_input_data.R"
+            dataFiles = [
+                  dataSpatial "epsg3035.RData"
+                , dataSpatial "mobility_regions.RData"
+                , dataPoseidonDataPoseidonExtracted "poseidon_extracted.janno"
+                , dataPoseidonDataMDS "mds.mds"
+              ]
+        need $ script : dataFiles
+        rscript script
 
     map dataPlotReferenceData [
           "region_id_shapes.RData"
@@ -61,7 +88,7 @@ main = shakeArgs shakeOptions {
         ] &%> \out -> do
         let script = code01 "00_prepare_plot_reference_data.R"
         need [script]
-        cmd_ "Rscript" script
+        rscript script
 
         
 
