0357937bf388da8b8e8ad1d7d409d13f94911946
Clemens Schmid <clemens@nevrome.de>
2021-10-24 13:44:09 +0200

    some scripts run in fact so fast, that submitting them to a hpc is not necessary

--- code/01_poseidon_data_preparation/03_distance_plink.sh
+++ code/01_poseidon_data_preparation/03_distance_plink.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#$ -S /bin/bash #defines bash as the shell for execution
-#$ -N dist #Name of the command that will be listed in the queue
-#$ -cwd #change to current directory
-#$ -j y #join error and standard output in one file, no error file will be written
-#$ -o ~/log #standard output file or directory (joined with error because of -j y)
-#$ -q archgen.q #queue
-#$ -pe smp 10 #needs 8 CPU cores
-#$ -l h_vmem=40G #request 4Gb of memory
-#$ -V # load personal profile
-
-date
-
 cd data/poseidon_data/identical_filter
 
 # pruning
@@ -19,7 +7,3 @@ plink1.9 --bfile ../poseidon_extracted_pre_identicals_filter/poseidon_extracted_
 
 # pairwise distances
 plink1.9 --bfile poseidon_extracted_pre_identicals_filter.pruned --distance square 1-ibs
-
-date
-
-exit 0
--- code/01_poseidon_data_preparation/06_mds_plink.sh
+++ code/01_poseidon_data_preparation/06_mds_plink.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#$ -S /bin/bash #defines bash as the shell for execution
-#$ -N dist #Name of the command that will be listed in the queue
-#$ -cwd #change to current directory
-#$ -j y #join error and standard output in one file, no error file will be written
-#$ -o ~/log #standard output file or directory (joined with error because of -j y)
-#$ -q archgen.q #queue
-#$ -pe smp 10 #needs 8 CPU cores
-#$ -l h_vmem=40G #request 4Gb of memory
-#$ -V # load personal profile
-
-date
-
 cd data/poseidon_data/mds
 
 # pruning
@@ -25,8 +13,3 @@ plink1.9 --bfile poseidon_extracted.pruned --genome --out poseidon_extracted.pru
 # C2 with --mds-plot 2 or --mds-plot 3. So it's sufficient to call --mds-plot 3 once
 # and then select the desired dimensions later)
 plink1.9 --bfile poseidon_extracted.pruned --cluster --mds-plot 3 --read-genome poseidon_extracted.pruned.genome --out mds
-
-date
-
-exit 0
-
