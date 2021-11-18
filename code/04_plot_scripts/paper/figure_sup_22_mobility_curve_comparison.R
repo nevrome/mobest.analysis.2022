bfe09fbc96d563ff2be217dcafed1001d3e5f6ef
Clemens Schmid <clemens@nevrome.de>
2021-11-18 18:18:12 +0100

    added a plot to compare different runs

---
+++ code/04_plot_scripts/paper/figure_sup_22_mobility_curve_comparison.R
@@ -0,0 +1,65 @@
+library(magrittr)
+
+get_mean_points <- function(x, run) {
+  x %>%
+    dplyr::select(mean_search_z, search_id, region_id, directed_mean_spatial_distance) %>%
+    dplyr::mutate(run = run)
+}
+
+load("data/origin_search/origin_grid_mean.RData")
+mds2 <- origin_grid_mean %>% get_mean_points("Default: 2D MDS")
+load("data/origin_search/origin_grid_mean_mds3.RData")
+mds3 <- origin_grid_mean %>% get_mean_points("3D MDS")
+load("data/origin_search/origin_grid_derived_data_retro_low.RData")
+low_retro <- origin_grid_derived_data$origin_grid_mean %>% get_mean_points("Lower rearview distance")
+load("data/origin_search/origin_grid_derived_data_retro_high.RData")
+high_retro <- origin_grid_derived_data$origin_grid_mean %>% get_mean_points("Higher rearview distance")
+
+comb <- dplyr::bind_rows(mds3, low_retro, high_retro) %>%
+  dplyr::left_join(mds2 %>% dplyr::select(search_id, spacedist = directed_mean_spatial_distance)) %>%
+  dplyr::mutate(diff_to_mds2 = directed_mean_spatial_distance - spacedist)
+
+library(ggplot2)
+
+p <- ggplot() +
+  facet_grid(rows = dplyr::vars(region_id), cols = dplyr::vars(run)) +
+  geom_point(
+    data = comb,
+    mapping = aes(
+      x = mean_search_z, y = directed_mean_spatial_distance, fill = diff_to_mds2
+    ),
+    alpha = 1,
+    size = 1.7,
+    shape = 21,
+    colour = "black",
+    stroke = 0.1
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = "bottom"
+  ) +   
+  coord_cartesian(
+    xlim = c(-7400, 1400),
+    ylim = c(-100, 3000)
+  ) +
+  scale_x_continuous(breaks = seq(-7000, 1000, 2000)) +
+  scale_fill_gradient2(
+    low = "#4dac26",
+    mid = "white",
+    high = "#d01c8b"
+  ) +
+  guides(
+    fill = guide_colorbar(title = "Divergence from default [km]    ", barwidth = 20, barheight = 1.5),
+  ) +
+  xlab("time [years calBC/calAD]") +
+  ylab("spatial distance to \"origin point\" (directed mean) [km]") 
+
+ggsave(
+  paste0("plots/figure_sup_22_mobility_curve_comparison.jpeg"),
+  plot = p,
+  device = "jpeg",
+  scale = 0.6,
+  dpi = 300,
+  width = 430, height = 390, units = "mm",
+  limitsize = F
+)
