5ccd483c49c1dce8fd207b537541d528b33b2059
Clemens Schmid <clemens@nevrome.de>
2021-12-05 23:44:37 +0100

    16Gb are not enough for modify_crossvalidation_results.R (should be refactored)

--- Shakefile.hs
+++ Shakefile.hs
@@ -41,7 +41,7 @@ mpiEVAClusterSettings = Settings {
   , singularityContainer = "singularity_mobest.sif"
   , bindPath = "--bind=/mnt/archgen/users/schmid"
   --, qrsh = "qrsh -b y -cwd -q archgen.q -pe smp 4 -l h_vmem=16G -now n -V -N hedgehog"
-  , qsubSmallCommand =  "qsub -sync y -b y -cwd -q archgen.q -pe smp 8 -l h_vmem=16G -now n -V -j y -o ~/log -N small"
+  , qsubSmallCommand =  "qsub -sync y -b y -cwd -q archgen.q -pe smp 8 -l h_vmem=20G -now n -V -j y -o ~/log -N small"
   , qsubMediumCommand = "qsub -sync y -b y -cwd -q archgen.q -pe smp 16 -l h_vmem=32G -now n -V -j y -o ~/log -N medium"
   , qsubScript = "qsub -sync y -N large " -- trailing space is meaningful!
 }
