135857ec63c8eaa13598be166650f6118ba1cbf6
Clemens Schmid <clemens@nevrome.de>
2021-12-13 15:05:17 +0100

    cleaned up workflow.R

--- code/workflow.R
+++ code/workflow.R
@@ -3,35 +3,24 @@ u <- "clemens_schmid"
 h <- "daghead1.eva.mpg.de"
 lb <- "~/agora/mobest.analysis.2020"
 cb <- "/mnt/archgen/users/schmid/mobest.analysis.2020"
-ssp <- "/mnt/archgen/users/schmid/mobest.analysis.2020/singularity_qsub.sh"
 
-bash <- function(x) { eva.cluster::run_local_bash_command(
-  paste("./singularity_mobest.sif", x), where_run_path = lb
-)}
 up <- function(...) { eva.cluster::cluster_up(
     ..., user = u, host = h, pw = pw, local_base = lb, cluster_base = cb, equal_path = T
 )}
 down <- function(...) { eva.cluster::cluster_down(
     ..., user = u, host = h, pw = pw, local_base = lb, cluster_base = cb, equal_path = T
 )}
-qsub <- function(x) { eva.cluster::run_cluster_qsub_script(
-    x, where_run_path = cb, host = h, user = u, pw = pw
-)}
-singularity_qsub <- function(x) { eva.cluster::run_cluster_singularity_script(
-  x, where_run_path = cb, singularity_script_path = ssp, cores = 16, memory = 50, 
-  host = h, user = u, pw = pw
-)}
 
-#### singularity container ####
+#### build and upload singularity container ####
 
 system("./singularity_build_sif.sh")
 up("singularity_mobest.sif")
 
-#### download output of the pipeline ####
+#### download output of the shake pipeline ####
 
+down("ShakeReport.html")
 down("plots/")
 down("tables/")
-down("ShakeReport.html")
 down("data/")
 
 #### run all plotting scripts ####
