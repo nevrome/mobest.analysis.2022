09faecac0f3c55ee789ca6c85905d9212627e074
Clemens Schmid <clemens@nevrome.de>
2022-03-08 17:37:56 +0100

    run command

--- post_review_experiments/emu.sh
+++ post_review_experiments/emu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# qsub -b y -cwd -q archgen.q -pe smp 8  -l h_vmem=100G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif ./post_review_experiments/emu.sh
+
 emu \
   --plink data/poseidon_data/poseidon_extracted/poseidon_extracted \
   --n_eig 10 \
