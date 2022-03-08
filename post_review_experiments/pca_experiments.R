abce5bec50638af45f8735a48d039675c77dc0e2
Clemens Schmid <clemens@nevrome.de>
2022-03-08 14:31:06 +0100

    more pca experiments

--- post_review_experiments/pca_experiments.R
+++ post_review_experiments/pca_experiments.R
@@ -9,25 +9,35 @@ fam <- readr::read_tsv(
   col_names = FALSE
   )
 
-to_project <- fam %>%
+fam_capture <- fam %>%
   dplyr::mutate(
     Capture_Type = dplyr::case_when(
       grepl(".SG", X2) ~ "Shotgun",
       TRUE ~ "Capture"
     )
-  ) %$%
-  which(Capture_Type == "Shotgun")
+  ) 
 
 pca_out <- smartsnp::smart_pca(
   "data/poseidon_data/poseidon_extracted/poseidon_extracted.geno",
   sample_group = seq_len(nrow(fam)),
   missing_impute = "mean",
   pc_axes = 3,
-  sample_project = to_project,
+  sample_project = which(fam_capture$Capture_Type == "Capture"),
   pc_project = c(1,2,3)
 )
 
-save(pca_out, file = "pca_experiment_project_shotgun_on_capture.RData")
+save(pca_out, file = "pca_experiment_project_capture_on_shotgun.RData")
+
+# pca_out <- smartsnp::smart_pca(
+#   "data/poseidon_data/poseidon_extracted/poseidon_extracted.geno",
+#   sample_group = seq_len(nrow(fam)),
+#   missing_impute = "mean",
+#   pc_axes = 3,
+#   sample_project = which(fam_capture$Capture_Type == "Shotgun"),
+#   pc_project = c(1,2,3)
+# )
+# 
+# save(pca_out, file = "pca_experiment_project_shotgun_on_capture.RData")
 
 #### normal pca ####
 
