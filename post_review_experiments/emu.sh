43c57e0d020f75cb1b1d437d5e895976b89f157f
Clemens Schmid <clemens@nevrome.de>
2022-03-08 21:05:35 +0100

    for the future: try less memory

--- post_review_experiments/emu.sh
+++ post_review_experiments/emu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# qsub -b y -cwd -q archgen.q -pe smp 36 -l h_vmem=300G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif ./post_review_experiments/emu.sh
+# qsub -b y -cwd -q archgen.q -pe smp 36 -l h_vmem=50G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif ./post_review_experiments/emu.sh
+# low memory: untested
 
 emu \
   --plink data/poseidon_data/poseidon_extracted/poseidon_extracted \
