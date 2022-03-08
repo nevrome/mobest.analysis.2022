95f498c9ca9567d447b826d0564b4cd13434e75a
Clemens Schmid <clemens@nevrome.de>
2022-03-08 12:41:02 +0100

    experiments with PCA

---
+++ post_review_experiments/pca_experiments.R
@@ -0,0 +1,9 @@
+# qsub -sync y -b y -cwd -q archgen.q -pe smp 8  -l h_vmem=20G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif Rscript pca_experiment.R
+
+pca_out <- smartsnp::smart_pca(
+  "../data/poseidon_data/poseidon_extracted/poseidon_extracted.geno",
+  sample_group = 1:3191,
+  missing_impute = "mean"
+)
+
+save(pca_out, file = "pca_experiment.RData")
