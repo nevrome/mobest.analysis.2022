61c1abf12a6adc17ea48c33d9eada6d9b7e93556
Clemens Schmid <clemens@nevrome.de>
2022-03-08 12:47:12 +0100

    adjusting the script

--- post_review_experiments/pca_experiments.R
+++ post_review_experiments/pca_experiments.R
@@ -1,7 +1,7 @@
-# qsub -sync y -b y -cwd -q archgen.q -pe smp 8  -l h_vmem=20G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif Rscript pca_experiment.R
+# qsub -sync y -b y -cwd -q archgen.q -pe smp 8  -l h_vmem=40G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif Rscript post_review_experiments/pca_experiment.R
 
 pca_out <- smartsnp::smart_pca(
-  "../data/poseidon_data/poseidon_extracted/poseidon_extracted.geno",
+  "data/poseidon_data/poseidon_extracted/poseidon_extracted.geno",
   sample_group = 1:3191,
   missing_impute = "mean"
 )
