4e1ff6787c2815ce2c5560372acb63f691602475
Clemens Schmid <clemens@nevrome.de>
2022-03-16 18:28:41 +0100

    and more structural name adjustments

--- code/01_data_preparation/03_remove_related_individuals/03_poseidon_extract.sh
+++ code/01_data_preparation/03_remove_related_individuals/03_poseidon_extract.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -r data/genotype_data/poseidon_extracted
+rm -r data/genotype_data/remove_related_individuals/remove_related_selection
 
 trident forge \
-  --forgeFile code/01_data_preparation/ind_list.txt \
+  --forgeFile code/01_data_preparation/ind_list_remove_related_selection.txt \
   -d data/genotype_data/aadrv50 \
-  -o data/genotype_data/poseidon_extracted
+  -o data/genotype_data/remove_related_individuals/remove_related_selection
