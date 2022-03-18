82d1f91b02f3bea7632f71f356390563ed714746
Clemens Schmid <clemens@nevrome.de>
2022-03-18 15:05:10 +0100

    I will need both PLINK and EIGENSTRAT data

--- code/01_data_preparation/04_prepare_snp_selections/6_extract_individuals_with_enough_snps.sh
+++ code/01_data_preparation/04_prepare_snp_selections/6_extract_individuals_with_enough_snps.sh
@@ -5,7 +5,15 @@ trident forge \
   -d unfiltered_snp_selection_pre_ind_correction \
   -o unfiltered_snp_selection
   
+trident genoconvert \
+  -d unfiltered_snp_selection \
+  --outFormat EIGENSTRAT
+  
 trident forge \
   --forgeFile ../../../code/01_data_preparation/ind_list_post_snp_selection.txt \
   -d filtered_snp_selection_pre_ind_correction \
   -o filtered_snp_selection
+
+trident genoconvert \
+  -d filtered_snp_selection \
+  --outFormat EIGENSTRAT
