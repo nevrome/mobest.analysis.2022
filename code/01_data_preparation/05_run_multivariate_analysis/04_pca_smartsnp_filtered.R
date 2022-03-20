5b57c20b5833a0c4c43650c5fc9ec70b4098dc8d
Clemens Schmid <clemens@nevrome.de>
2022-03-20 15:53:47 +0100

    scripts to run pca and emu

---
+++ code/01_data_preparation/05_run_multivariate_analysis/04_pca_smartsnp_filtered.R
@@ -0,0 +1,18 @@
+# qsub -b y -cwd -q archgen.q -pe smp 8 -l h_vmem=200G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif Rscript code/01_data_preparation/05_run_multivariate_analysis/03_pca_smartsnp_filtered.R
+
+fam <- readr::read_tsv(
+  "data/genotype_data/snp_subsets/filtered_snp_selection/filtered_snp_selection.fam",
+  col_names = FALSE
+)
+
+pca_out <- smartsnp::smart_pca(
+  "data/genotype_data/snp_subsets/filtered_snp_selection/filtered_snp_selection.geno",
+  sample_group = seq_len(nrow(fam)),
+  missing_impute = "mean",
+  pc_axes = 10
+)
+
+save(
+  pca_out,
+  file = "data/genotype_data/multivariate_analysis/PCA_filtered_snp_selection/pca_out.RData"
+)
---
+++ code/01_data_preparation/05_run_multivariate_analysis/05_emu_unfiltered.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd data/genotype_data
+
+# qsub -b y -cwd -q archgen.q -pe smp 48 -l h_vmem=100G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif ./code/01_data_preparation/05_run_multivariate_analysis/05_emu_unfiltered.sh
+
+emu \
+  --plink snp_subsets/unfiltered_snp_selection/unfiltered_snp_selection \
+  --n_eig 10 \
+  --n_out 10 \
+  --maf 0.05 \
+  --threads 48 \
+  --out multivariate_analysis/EMU_unfiltered_snp_selection/emu_out.txt \
+  --loadings
