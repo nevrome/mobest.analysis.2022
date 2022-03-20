fdde708103ba047097c6dbd61f17593bd99b006c
Clemens Schmid <clemens@nevrome.de>
2022-03-20 17:37:16 +0100

    fixed call commands

--- code/01_data_preparation/05_run_multivariate_analysis/04_pca_smartsnp_filtered.R
+++ code/01_data_preparation/05_run_multivariate_analysis/04_pca_smartsnp_filtered.R
@@ -1,4 +1,4 @@
-# qsub -b y -cwd -q archgen.q -pe smp 8 -l h_vmem=200G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif Rscript code/01_data_preparation/05_run_multivariate_analysis/03_pca_smartsnp_filtered.R
+# qsub -b y -cwd -q archgen.q -pe smp 8 -l h_vmem=200G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif Rscript code/01_data_preparation/05_run_multivariate_analysis/04_pca_smartsnp_filtered.R
 
 fam <- readr::read_tsv(
   "data/genotype_data/snp_subsets/filtered_snp_selection/filtered_snp_selection.fam",
--- code/01_data_preparation/05_run_multivariate_analysis/06_emu_filtered.sh
+++ code/01_data_preparation/05_run_multivariate_analysis/06_emu_filtered.sh
@@ -2,7 +2,7 @@
 
 cd data/genotype_data
 
-# qsub -b y -cwd -q archgen.q -pe smp 48 -l h_vmem=100G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif ./code/01_data_preparation/05_run_multivariate_analysis/05_emu_filtered.sh
+# qsub -b y -cwd -q archgen.q -pe smp 48 -l h_vmem=100G -now n -V -j y -o ~/log -N experiment singularity exec --bind=/mnt/archgen/users/schmid singularity_mobest.sif ./code/01_data_preparation/05_run_multivariate_analysis/06_emu_filtered.sh
 
 emu \
   --plink snp_subsets/filtered_snp_selection/filtered_snp_selection \
