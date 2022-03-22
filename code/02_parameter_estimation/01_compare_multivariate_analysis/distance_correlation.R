c190be30955328b55ffa8a005e82179c7f70e731
Clemens Schmid <clemens@nevrome.de>
2022-03-22 16:24:53 +0100

    more work on the distance correlation table

--- code/02_parameter_estimation/01_compare_multivariate_analysis/distance_correlation.R
+++ code/02_parameter_estimation/01_compare_multivariate_analysis/distance_correlation.R
@@ -1,69 +1,59 @@
 load("data/genotype_data/janno_final.RData")
 
-large_distance_table <- mobest::calculate_pairwise_distances(
-  independent = mobest::create_spatpos(
-    id = janno_final$Poseidon_ID,
-    x = janno_final$x,
-    y = janno_final$y,
-    z = janno_final$Date_BC_AD_Median_Derived
-  ),
-  dependent = mobest::create_obs(
-    C1_mds_u = janno_final$C1_mds_f
-    # C2_mds_u = janno_final$C2_mds_f,
-    # C3_mds_u = janno_final$C3_mds_f,
-    # C4_mds_u = janno_final$C4_mds_f,
-    # C5_mds_u = janno_final$C5_mds_f,
-    # C6_mds_u = janno_final$C6_mds_f,
-    # C7_mds_u = janno_final$C7_mds_f,
-    # C8_mds_u = janno_final$C8_mds_f
-    # C1_mds_f = janno_final$C1_mds_f,
-    # C2_mds_f = janno_final$C2_mds_f,
-    # C3_mds_f = janno_final$C3_mds_f,
-    # C4_mds_f = janno_final$C4_mds_f,
-    # C1_pca_u = janno_final$C1_pca_u,
-    # C2_pca_u = janno_final$C2_pca_u,
-    # C3_pca_u = janno_final$C3_pca_u,
-    # C4_pca_u = janno_final$C4_pca_u,
-    # C1_pca_f = janno_final$C1_pca_f,
-    # C2_pca_f = janno_final$C2_pca_f,
-    # C3_pca_f = janno_final$C3_pca_f,
-    # C4_pca_f = janno_final$C4_pca_f,
-    # C1_emu_u = janno_final$C1_emu_u,
-    # C2_emu_u = janno_final$C2_emu_u,
-    # C3_emu_u = janno_final$C3_emu_u,
-    # C4_emu_u = janno_final$C4_emu_u,
-    # C1_emu_f = janno_final$C1_emu_f,
-    # C2_emu_f = janno_final$C2_emu_f,
-    # C3_emu_f = janno_final$C3_emu_f,
-    # C4_emu_f = janno_final$C4_emu_f
-  )
-)
-
-#save(large_distance_table, "data/parameter_exploration/multivariate_comparison/large_distance_table.RData")
-
 d <- function(...) {
   in_vecs <- list(...)
   checkmate::assert_true(length(unique(purrr::map_int(in_vecs, length))) == 1)
-  purrr::map(in_vecs, function(x) { x^2 }) %>%
-    purrr::reduce(`+`) %>%
-    sqrt
+  purrr::map(in_vecs, function(x) { x^2 }) %>% purrr::reduce(`+`) %>% sqrt
 }
 
 d_cum_df <- function(x) {
   purrr::map_dfc(1:ncol(x), function(i) {
-    dist_vec <- do.call(d, as.list(x[,1:i]))
-    setNames(
-      list(dist_vec),
-      paste0("C1to", substr(colnames(x)[i], start = 1, stop = 2))
-    )
-  })
+      d_in_list <- if (i < 2) { list(x[,1]) } else { as.list(x[,1:i]) }
+      dist_vec <- do.call(d, d_in_list)
+      setNames(
+        list(dist_vec),
+        paste0("C1to", substr(colnames(x)[i], start = 1, stop = 2))
+      )
+    }
+  )
 }
 
-spatiotemp_dist <- d(large_distance_table$geo_dist, large_distance_table$time_dist)
+spatiotemp_dist <- d(
+  mobest::calculate_geo_pairwise_distances(
+    ids = janno_final$Poseidon_ID,
+    independent = mobest::create_spatpos(
+      id = janno_final$Poseidon_ID,
+      x = janno_final$x,
+      y = janno_final$y,
+      z = janno_final$Date_BC_AD_Median_Derived
+    )
+  )[,3],
+  mobest::calculate_time_pairwise_distances(
+    ids = janno_final$Poseidon_ID,
+    independent = mobest::create_spatpos(
+      id = janno_final$Poseidon_ID,
+      x = janno_final$x,
+      y = janno_final$y,
+      z = janno_final$Date_BC_AD_Median_Derived
+    )
+  )[,3]
+)
 
-large_distance_table %>%
-  dplyr::select(tidyselect::matches("C[0-9]_(mds|pca|emu)_(u|f)_dist$")) %>%
-  d_cum_df() %>%
+mobest::calculate_dependent_pairwise_distances(
+  ids = janno_final$Poseidon_ID,
+  dependent = mobest::create_obs(
+    C1_mds_u = janno_final$C1_mds_u,
+    C2_mds_u = janno_final$C2_mds_u,
+    C3_mds_u = janno_final$C3_mds_u,
+    C4_mds_u = janno_final$C4_mds_u,
+    C5_mds_u = janno_final$C5_mds_u
+  )
+) %>% 
+  purrr::reduce(
+    function(x, y) { dplyr::bind_cols(x, y[3]) }
+  ) %>%
+  dplyr::select(-Var1, -Var2) %>%
+  d_cum_df %>%
   dplyr::summarise(
     dplyr::across(
       .fns = function(x) {
@@ -71,6 +61,11 @@ large_distance_table %>%
       }
     )
   )
+
+#save(large_distance_table, "data/parameter_exploration/multivariate_comparison/large_distance_table.RData")
+
+
+
 
 
 
