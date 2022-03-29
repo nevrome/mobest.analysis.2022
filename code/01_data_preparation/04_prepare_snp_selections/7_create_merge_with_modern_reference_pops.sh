3b3678df4f39b1a604d8cfea764e13de3557b678
Clemens Schmid <clemens@nevrome.de>
2022-03-29 14:22:03 +0200

    prepare datasets with modern reference pops for projection

--- code/01_data_preparation/04_prepare_snp_selections/7_create_merge_with_modern_reference_pops.sh
+++ code/01_data_preparation/04_prepare_snp_selections/7_create_merge_with_modern_reference_pops.sh
@@ -1,22 +1,28 @@
 cd data/genotype_data
 
+# prepare modern pops poseidon package
+
 trident forge \
   --forgeFile ../../code/01_data_preparation/modern_western_eurasian_populations.txt \
   -d aadrv50_1240K_HO \
   -o aadrv50_1240K_HO_Western_Eurasia_modern
   --minimal
 
-
-#### TODO ####
+# merge snp selection packages with modern pops
 
 trident forge \
   -d aadrv50_1240K_HO_Western_Eurasia_modern \
-  -d snp_subsets/... \
-  -f "**" \
-  -o unfiltered_snp_selection_with_modern_reference_pops
+  -d snp_subsets/unfiltered_snp_selection \
+  -f "*aadrv50_1240K_HO_Western_Eurasia_modern*,*unfiltered_snp_selection*" \
+  --intersect \
+  -o snp_subsets/unfiltered_snp_selection_with_modern_reference_pops \
+  --minimal
   
 trident forge \
-  --forgeFile ../../../code/01_data_preparation/modern_western_eurasian_populations.txt \
-  -d filtered_snp_selection \
-  -o filtered_snp_selection_with_modern_reference_pops
+  -d aadrv50_1240K_HO_Western_Eurasia_modern \
+  -d snp_subsets/filtered_snp_selection \
+  -f "*aadrv50_1240K_HO_Western_Eurasia_modern*,*filtered_snp_selection*" \
+  --intersect \
+  -o snp_subsets/filtered_snp_selection_with_modern_reference_pops \
+  --minimal
   
