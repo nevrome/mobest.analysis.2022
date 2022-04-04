ec42cbfc83b7981df7149a23cc4dd98ad3feef47
Clemens Schmid <clemens@nevrome.de>
2022-04-04 12:55:07 +0200

    and more refactoring to determine the median pair-wise distances

--- code/02_parameter_estimation/01_compare_multivariate_analysis/distance_correlation.R
+++ code/02_parameter_estimation/01_compare_multivariate_analysis/distance_correlation.R
@@ -61,30 +61,38 @@ d_cum_df <- function(x) {
   )
 }
 
-purrr::pmap_df(
+distance_products <- purrr::pmap_df(
   list(permutations$method, permutations$fstate, observation_bundles_list),
   function(method, fstate, observation_bundle) {
-    mobest::calculate_dependent_pairwise_distances(
+    cum_across_dim_pairwise_distances <- mobest::calculate_dependent_pairwise_distances(
       ids = janno_final$Poseidon_ID,
       dependent = observation_bundle
     ) %>%
       purrr::reduce( function(x, y) { dplyr::bind_cols(x, y[3]) } ) %>%
       dplyr::select(-Var1, -Var2) %>%
-      d_cum_df %>%
+      d_cum_df
+    distance_median <- cum_across_dim_pairwise_distances %>%
       dplyr::summarise(
-        dplyr::across(
-          .fns = function(x) {
-            cor(spatiotemp_dist, x)^2
-          }
-        )
-      ) %>%
+        dplyr::across(.fns = function(x) { median(x) } )
+      )
+    distance_correlation <- cum_across_dim_pairwise_distances %>%
+      dplyr::summarise(
+        dplyr::across(.fns = function(x) { cor(spatiotemp_dist, x)^2 } )
+      )
+    dplyr::bind_rows(distance_median, distance_correlation) %>%
       dplyr::mutate(
+        measure = c("median", "distance correlation"),
         method = method,
         snp_selection = fstate
       )
   }
 )
 
-#save(large_distance_table, "data/parameter_exploration/multivariate_comparison/large_distance_table.RData")
+distance_median <- distance_products %>% dplyr::filter(measure == "median")
+distance_correlation <- distance_products %>% dplyr::filter(measure == "distance correlation")
+
+save(distance_median, file = "data/parameter_exploration/multivariate_analysis_comparison/distance_median.RData")
+
+#save(large_distance_table, file = "data/parameter_exploration/multivariate_comparison/large_distance_table.RData")
 
 # dplyr::mutate(dplyr::across(tidyselect:::where(is.numeric), round, 3)) %>% knitr::kable()
