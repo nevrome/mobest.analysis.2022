869f1483692b6dd676831c6f5c23891699a68d68
Clemens Schmid <clemens@nevrome.de>
2022-04-21 15:47:42 +0200

    more work on the 3D plot sequence

--- code/04_plot_scripts/presentations/3D_narrative.R
+++ code/04_plot_scripts/presentations/3D_narrative.R
@@ -1,7 +1,8 @@
 library(magrittr)
 
 load("data/genotype_data/janno_final.RData")
-load("data/spatial/area.RData")
+# load("data/spatial/area.RData")
+load("data/spatial/search_area.RData")
 
 #### basic plot of observations ####
 
@@ -29,6 +30,7 @@ scatterplot3d::scatterplot3d(
   mar = c(2.7, 2.7, 0, 2)
 )
 
+s$box3d()
 dev.off()
 
 #### plot with interpolation ####
@@ -52,7 +54,7 @@ model_grid <- mobest::create_model_grid(
   ),
   prediction_grid = mobest::create_spatpos_multi(
     timeslice = mobest::create_prediction_grid(
-      area,
+      search_area,
       spatial_cell_size = 50000
     ) %>% mobest::geopos_to_spatpos(seq(-8000, 2000, 200))
   )
@@ -102,6 +104,7 @@ points(
   col = ggplot2::alpha(threedinter$color, threedinter$alpha * 0.2)
 )
 
+s$box3d()
 dev.off()
 
 #### plot with one interpolation timeslice ####
@@ -124,6 +127,7 @@ s <- scatterplot3d::scatterplot3d(
   mar = c(2.7, 2.7, 0, 2)
 )
 
+s$box3d()
 dev.off()
 
 #### plot with one timeslice, z-axis C1 ####
@@ -142,6 +146,7 @@ s <- scatterplot3d::scatterplot3d(
   mar = c(2.7, 2.7, 0, 2)
 )
 
+s$box3d()
 dev.off()
 
 png(filename = "plots/presentation/3D_plot_gpr_C1_timeslice_zC1b.png", width = 22, height = 14, units = "cm", res = 300)
@@ -158,9 +163,19 @@ s <- scatterplot3d::scatterplot3d(
   mar = c(2.7, 2.7, 0, 2)
 )
 
+s$box3d()
 dev.off()
 
 #### plot with one timeslice, z-axis C1 and error ####
+
+cut_segments <- function(x, y, z_top, z_bottom, color ) {
+  top <- s$xyz.convert(x, y, z_top)
+  bottom <- s$xyz.convert(x, y, z_bottom)
+  segments(
+    bottom$x, bottom$y, top$x, top$y,
+    col = color, lwd = 0.5
+  )
+}
 
 png(filename = "plots/presentation/3D_plot_gpr_C1_timeslice_zC1b_sd.png", width = 22, height = 14, units = "cm", res = 300)
 
@@ -176,22 +191,200 @@ s <- scatterplot3d::scatterplot3d(
   mar = c(2.7, 2.7, 0, 2)
 )
 
-top <- s$xyz.convert(
-  threedinter_timeslice$x + threedinter_timeslice$sd,
-  threedinter_timeslice$y + threedinter_timeslice$sd,
-  threedinter_timeslice$mean + threedinter_timeslice$sd
-)
-bottom <- s$xyz.convert(
-  threedinter_timeslice$x - threedinter_timeslice$sd,
-  threedinter_timeslice$y - threedinter_timeslice$sd,
-  threedinter_timeslice$mean - threedinter_timeslice$sd
-)
-segments(
-  top$x, top$y, bottom$x, bottom$y,
-  col = threedinter_timeslice$color,#ggplot2::alpha(threedinter_timeslice$color, threedinter_timeslice$alpha * 1.5),
-  lwd = 0.5
+cut_segments(
+  threedinter_timeslice$x,
+  threedinter_timeslice$y,
+  threedinter_timeslice$mean + threedinter_timeslice$sd,
+  threedinter_timeslice$mean - threedinter_timeslice$sd,
+  threedinter_timeslice$color
 )
 
+s$box3d()
 dev.off()
 
-#### 
+#### plot with one timeslice, z-axis C1, observation plane ####
+
+obsval <- janno_search$C1_mds_u
+
+threedinter_timeslice_below <- threedinter_timeslice %>%
+  dplyr::filter(mean < obsval)
+threedinter_timeslice_above <- threedinter_timeslice %>%
+  dplyr::filter(mean >= obsval)
+
+png(filename = "plots/presentation/3D_plot_gpr_C1_timeslice_zC1b_plane.png", width = 22, height = 14, units = "cm", res = 300)
+
+s <- scatterplot3d::scatterplot3d(
+  threedinter_timeslice_below$x, threedinter_timeslice_below$y, threedinter_timeslice_below$mean, 
+  color = threedinter_timeslice_below$color,#ggplot2::alpha(threedinter_timeslice$color, threedinter_timeslice$alpha),
+  xlim = range(threed$x), ylim = range(threed$y),
+  lwd = 0.1, pch = 18, cex.symbols = 0.8,
+  angle = 30,
+  xlab = "x", ylab = "y", zlab = "MDS C1",
+  col.axis = "grey",
+  zlim = c(-0.08, 0.11),
+  mar = c(2.7, 2.7, 0, 2)
+)
+
+below <- s$xyz.convert(
+  threedinter_timeslice_below$x,
+  threedinter_timeslice_below$y,
+  threedinter_timeslice_below$mean
+)
+points(
+  below$x, below$y,
+  col = threedinter_timeslice_below$color,
+  pch = 18, cex = 0.8
+)
+
+s$plane3d(
+  Intercept = obsval,#janno_search$C1_mds_u,
+  x.coef = 0, y.coef = 0,
+  lty = "dotted", draw_polygon = TRUE, draw_lines = TRUE, 
+  polygon_args = list(col = rgb(0.8, 0.8, 0.8, 0.8))
+)
+
+above <- s$xyz.convert(
+  threedinter_timeslice_above$x,
+  threedinter_timeslice_above$y,
+  threedinter_timeslice_above$mean
+)
+points(
+  above$x, above$y,
+  col = threedinter_timeslice_above$color,
+  pch = 18, cex = 0.8
+)
+
+s$box3d()
+dev.off()
+
+#### plot with one timeslice, z-axis C1, observation plane, error ####
+
+seglist <- threedinter_timeslice %>%
+  dplyr::mutate(
+    segment_position = dplyr::case_when(
+      mean-sd >= obsval ~ "01_fully_above",
+      mean+sd < obsval ~ "02_fully_below",
+      TRUE ~ "03_cut"
+    )
+  ) %>% dplyr::group_split(segment_position)
+
+png(filename = "plots/presentation/3D_plot_gpr_C1_timeslice_zC1b_sd_plane.png", width = 22, height = 14, units = "cm", res = 300)
+
+s <- scatterplot3d::scatterplot3d(
+  threedinter_timeslice_below$x, threedinter_timeslice_below$y, threedinter_timeslice_below$mean, 
+  color = threedinter_timeslice_below$color,#ggplot2::alpha(threedinter_timeslice$color, threedinter_timeslice$alpha),
+  xlim = range(threed$x), ylim = range(threed$y),
+  lwd = 0.1, pch = 18, cex.symbols = 0.8,
+  angle = 30,
+  xlab = "x", ylab = "y", zlab = "MDS C1",
+  col.axis = "grey",
+  zlim = c(-0.08, 0.11),
+  mar = c(2.7, 2.7, 0, 2)
+)
+
+below <- s$xyz.convert(
+  threedinter_timeslice_below$x,
+  threedinter_timeslice_below$y,
+  threedinter_timeslice_below$mean
+)
+points(
+  below$x, below$y,
+  col = threedinter_timeslice_below$color,
+  pch = 18, cex = 0.8
+)
+cut_segments(
+  seglist[[2]]$x, seglist[[2]]$y,
+  seglist[[2]]$mean + seglist[[2]]$sd,
+  seglist[[2]]$mean - seglist[[2]]$sd,
+  seglist[[2]]$color
+)
+cut_segments(
+  seglist[[3]]$x, seglist[[3]]$y,
+  rep(obsval, nrow(seglist[[3]])),
+  seglist[[3]]$mean - seglist[[3]]$sd,
+  seglist[[3]]$color
+)
+
+s$plane3d(
+  Intercept = obsval,#janno_search$C1_mds_u,
+  x.coef = 0, y.coef = 0,
+  lty = "dotted", draw_polygon = TRUE, draw_lines = TRUE, 
+  polygon_args = list(col = rgb(0.8, 0.8, 0.8, 0.8))
+)
+
+above <- s$xyz.convert(
+  threedinter_timeslice_above$x,
+  threedinter_timeslice_above$y,
+  threedinter_timeslice_above$mean
+)
+points(
+  above$x, above$y,
+  col = threedinter_timeslice_above$color,
+  pch = 18, cex = 0.8
+)
+cut_segments(
+  seglist[[1]]$x, seglist[[1]]$y,
+  seglist[[1]]$mean + seglist[[1]]$sd,
+  seglist[[1]]$mean - seglist[[1]]$sd,
+  seglist[[1]]$color
+)
+cut_segments(
+  seglist[[3]]$x, seglist[[3]]$y,
+  seglist[[3]]$mean + seglist[[3]]$sd,
+  rep(obsval, nrow(seglist[[3]])),
+  seglist[[3]]$color
+)
+
+s$box3d()
+dev.off()
+
+#### origin search ####
+
+# janno_search <- janno_final %>%
+#   dplyr::mutate(
+#     search_z = sapply(janno_final$Date_BC_AD_Sample, function(x){ x[1] })
+#   ) %>% dplyr::filter(
+#     Poseidon_ID %in% c(
+#       "Stuttgart_published.DG" 
+#       #, "RISE434.SG" 
+#       #, "3DT26.SG"
+#       #, "SI-40.SG"
+#     )
+#   )
+# 
+# search <- mobest::locate(
+#   independent = mobest::create_spatpos(
+#     id = janno_final$Poseidon_ID,
+#     x = janno_final$x,
+#     y = janno_final$y,
+#     z = janno_final$Date_BC_AD_Median_Derived
+#   ),
+#   dependent = mobest::create_obs(
+#     C1_mds_u = janno_final$C1_mds_u,
+#     C2_mds_u = janno_final$C2_mds_u
+#   ),
+#   kernel = mobest::create_kernset(
+#     C1_mds_u = mobest::create_kernel(900000, 900000, 800, 0.07),
+#     C2_mds_u = mobest::create_kernel(900000, 900000, 800, 0.07)
+#   ),
+#   search_independent = mobest::create_spatpos(
+#     id = janno_search$Poseidon_ID,
+#     x = janno_search$x,
+#     y = janno_search$y,
+#     z = janno_search$Date_BC_AD_Median_Derived
+#   ),
+#   search_dependent = mobest::create_obs(
+#     C1_mds_u = janno_search$C1_mds_u,
+#     C2_mds_u = janno_search$C2_mds_u
+#   ),
+#   search_space_grid = mobest::create_prediction_grid(
+#     search_area,
+#     spatial_cell_size = 50000
+#   ),
+#   search_time = -700,#seq(-4000, -1800, 50),
+#   search_time_mode = "absolute"
+# )
+# 
+# search_prod <- mobest::multiply_dependent_probabilities(search)
+
+
