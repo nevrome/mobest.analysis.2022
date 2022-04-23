61745c6203955009e142de1dbca3d3d67663555f
Clemens Schmid <clemens@nevrome.de>
2022-04-23 22:22:26 +0200

    prepared a script to construct to movies

--- code/04_plot_scripts/presentations/origin_search_narrative_movie.R
+++ code/04_plot_scripts/presentations/origin_search_narrative_movie.R
@@ -18,6 +18,14 @@ janno_search <- janno_final %>%
     )
   )
 
+toBCAD <- function(x) {
+  dplyr::case_when(
+    x < 0 ~ paste(abs(x), "calBC"),
+    x == 0 ~ paste(x, "calBC/AD"),
+    x > 0 ~ paste(x, "calAD")
+  )
+}
+
 plot_prob <- function(search_prod, path) {
   
   p <- ggplot() +
@@ -25,6 +33,11 @@ plot_prob <- function(search_prod, path) {
     geom_raster(
       data = search_prod,
       mapping = aes(x = field_x, y = field_y, fill = probability_product),
+    ) +
+    annotate(
+      "text",
+      x = 6700000, y = 4400000, size = 11,
+      label = toBCAD(unique(search_prod$search_z))
     ) +
     scale_fill_viridis_c(option = "mako", direction = -1) +
     geom_sf(data = extended_area, fill = NA, colour = "black") +
@@ -52,16 +65,15 @@ plot_prob <- function(search_prod, path) {
       strip.text = element_text(size = 15),
       axis.ticks = element_blank(),
       panel.background = element_rect(fill = "#BFD5E3")
-    ) +
-    ggtitle(paste("time calBC/AD:", unique(search_prod$search_z)))
+    )
   
   ggsave(
     path,
     plot = p,
-    device = "png",
+    device = "jpeg",
     scale = 0.6,
     dpi = 300,
-    width = 350, height = 300, units = "mm",
+    width = 370, height = 300, units = "mm",
     limitsize = F
   )
   
@@ -72,7 +84,9 @@ spatial_pred_grid <- mobest::create_prediction_grid(
   spatial_cell_size = 20000
 )
 
-search_movie <- mobest::locate(
+#### movie1 ####
+
+search_movie1 <- mobest::locate(
   independent = mobest::create_spatpos(
     id = janno_final$Poseidon_ID,
     x = janno_final$x,
@@ -98,16 +112,99 @@ search_movie <- mobest::locate(
     C2_mds_u = janno_search$C2_mds_u
   ),
   search_space_grid = spatial_pred_grid,
-  search_time = seq(-8000, -5000, 20),
+  search_time = seq(-7500, -5000, 10),
   search_time_mode = "absolute"
 )
 
-search_movie_prod <- mobest::multiply_dependent_probabilities(search_movie)
+search_movie1_prod <- mobest::multiply_dependent_probabilities(search_movie1)
 
-search_movie_prod %>%
+search_movie1_prod %>%
   dplyr::group_split(search_z) %>%
   purrr::walk2(
-    ., paste0("plots/presentation/movie/", sprintf("frame_%03d", 1:length(.))),
+    ., paste0("plots/presentation/movie/", sprintf("frame_movie1_%03d", 1:length(.)), ".jpeg"),
+    plot_prob
+  )
+
+#### movie2 ####
+
+poi <- tibble::tibble(x = 25.90848, y = 42.51272) %>% ggplot2::sf_transform_xy(epsg3035, 4326)
+
+model_grid <- mobest::create_model_grid(
+  independent = mobest::create_spatpos_multi(
+    age_median = mobest::create_spatpos(
+      id = janno_final$Poseidon_ID,
+      x = janno_final$x,
+      y = janno_final$y,
+      z = janno_final$Date_BC_AD_Median_Derived
+    )
+  ),
+  dependent = mobest::create_obs(
+    C1_mds_u = janno_final$C1_mds_u,
+    C2_mds_u = janno_final$C2_mds_u
+  ),
+  kernel = mobest::create_kernset_multi(
+    kernel1 = mobest::create_kernset(
+      C1_mds_u = mobest::create_kernel(800000, 800000, 800, 0.07),
+      C2_mds_u = mobest::create_kernel(800000, 800000, 800, 0.07)
+    )
+  ),
+  prediction_grid = mobest::create_spatpos_multi(
+    poi = mobest::create_geopos(
+      id = "Karanovo",
+      x = poi$x,
+      y = poi$y
+    ) %>% mobest::geopos_to_spatpos(seq(-7500, 1000, 10))
+  )
+)
+
+interpol_grid <- mobest::run_model_grid(model_grid)
+
+interpol_grid_wide <- interpol_grid %>%
+  tidyr::pivot_wider(
+    names_from = "dependent_var_id",
+    values_from = c("mean", "sd")
+  )
+
+search_movie2 <- mobest::locate(
+  independent = mobest::create_spatpos(
+    id = janno_final$Poseidon_ID,
+    x = janno_final$x,
+    y = janno_final$y,
+    z = janno_final$Date_BC_AD_Median_Derived
+  ),
+  dependent = mobest::create_obs(
+    C1_mds_u = janno_final$C1_mds_u,
+    C2_mds_u = janno_final$C2_mds_u
+  ),
+  kernel = mobest::create_kernset(
+    C1_mds_u = mobest::create_kernel(800000, 800000, 800, 0.07),
+    C2_mds_u = mobest::create_kernel(800000, 800000, 800, 0.07)
+  ),
+  search_independent = mobest::create_spatpos(
+    id = interpol_grid_wide$id,
+    x = interpol_grid_wide$x,
+    y = interpol_grid_wide$y,
+    z = interpol_grid_wide$z
+  ),
+  search_dependent = mobest::create_obs(
+    C1_mds_u = interpol_grid_wide$mean_C1_mds_u,
+    C2_mds_u = interpol_grid_wide$mean_C2_mds_u
+  ),
+  search_dependent_error = mobest::create_obs_error(
+    C1_mds_u_sd = interpol_grid_wide$sd_C1_mds_u,
+    C2_mds_u_sd = interpol_grid_wide$sd_C2_mds_u
+  ),
+  search_space_grid = spatial_pred_grid,
+  search_time = 0,
+  search_time_mode = "relative"
+)
+
+search_movie2_prod <- mobest::multiply_dependent_probabilities(search_movie2)
+
+search_movie2_prod %>%
+  dplyr::group_split(search_z) %>%
+  purrr::walk2(
+    ., paste0("plots/presentation/movie/", sprintf("frame_movie2_%03d", 1:length(.))),
     plot_prob
   )
 
