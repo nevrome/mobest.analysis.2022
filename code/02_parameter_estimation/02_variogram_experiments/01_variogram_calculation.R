5596dc7a59d136444201d003b65a0ce94e3ad601
Clemens Schmid <clemens@nevrome.de>
2022-05-03 16:28:46 +0200

    update of nugget estimation code

--- code/02_parameter_estimation/02_variogram_experiments/01_variogram_calculation.R
+++ code/02_parameter_estimation/02_variogram_experiments/01_variogram_calculation.R
@@ -11,14 +11,14 @@ d_all <- mobest::calculate_pairwise_distances(
     z = janno_final$Date_BC_AD_Median_Derived
   ),
   dependent = mobest::create_obs(
-    C1 = janno_final$C1,
-    C2 = janno_final$C2,
-    C3 = janno_final$C3
+    C1_mds_u = janno_final$C1_mds_u,
+    C2_mds_u = janno_final$C2_mds_u,
+    C3_mds_u = janno_final$C3_mds_u
   )
 )
 
 save(d_all, file = "data/parameter_exploration/variogram/all_distances.RData")
 
-d_binned <- mobest::bin_pairwise_distances(d_all)
+d_binned <- mobest::bin_pairwise_distances(d_all, geo_bin = 100, time_bin = 100)
 
 save(d_binned, file = "data/parameter_exploration/variogram/binned_distances.RData")
--- code/02_parameter_estimation/02_variogram_experiments/02_nugget_estimation.R
+++ code/02_parameter_estimation/02_variogram_experiments/02_nugget_estimation.R
@@ -4,35 +4,29 @@ load("data/genotype_data/janno_final.RData")
 load("data/parameter_exploration/variogram/all_distances.RData")
 
 d_all_long <- d_all %>% tidyr::pivot_longer(
-  cols = c(C1_dist_resid, C2_dist_resid, C3_dist_resid),
+  cols = c(C1_mds_u_dist, C2_mds_u_dist, C3_mds_u_dist),
   names_to = "dist_type", values_to = "dist_val"
 )
 
 lower_left_variogram <- d_all_long %>%
+  # filter for small temporal and spatial pairwise distances
   dplyr::filter(time_dist < 50 & geo_dist < 50) %>%
-  dplyr::filter(Var1 != Var2) %>%
+  dplyr::filter(id1 != id2) %>%
   dplyr::mutate(
-    dist_type = replace(dist_type, dist_type == "C1_dist_resid", "C1"),
-    dist_type = replace(dist_type, dist_type == "C2_dist_resid", "C2"),
-    dist_type = replace(dist_type, dist_type == "C3_dist_resid", "C3"),
-    dist_type = factor(dist_type, levels = c("C1", "C2", "C3")),
     # rescaling of the dist val to a relative proportion
     dist_val_adjusted = dplyr::case_when(
-      dist_type == "C1" ~ 0.5*(dist_val^2/var(janno_final$C1)),
-      dist_type == "C2" ~ 0.5*(dist_val^2/var(janno_final$C2)),
-      dist_type == "C3" ~ 0.5*(dist_val^2/var(janno_final$C3)),
+      dist_type == "C1_mds_u_dist" ~ 0.5*(dist_val^2/stats::var(janno_final$C1_mds_u)),
+      dist_type == "C2_mds_u_dist" ~ 0.5*(dist_val^2/stats::var(janno_final$C2_mds_u)),
+      dist_type == "C3_mds_u_dist" ~ 0.5*(dist_val^2/stats::var(janno_final$C3_mds_u)),
     )
   )
 
 estimated_nuggets <- lower_left_variogram %>%
   dplyr::group_by(dist_type) %>%
-  dplyr::summarise(mean = mean(dist_val_adjusted, na.rm = T))
-
-nuggets_to_use <- as.character(c(
-  estimated_nuggets$mean[1:2] %>% max %>% round(., 2),
-  estimated_nuggets$mean[1:3] %>% max %>% round(., 2)
-))
+  dplyr::summarise(nugget = mean(dist_val_adjusted, na.rm = T)) %>%
+  dplyr::mutate(
+    dependent_var_id = gsub("_dist", "", dist_type)
+  )
 
 save(lower_left_variogram, file = "data/parameter_exploration/variogram/lower_left_variogram.RData")
 save(estimated_nuggets, file = "data/parameter_exploration/variogram/estimated_nuggets.RData")
-writeLines(nuggets_to_use, "data/parameter_exploration/variogram/nuggets.txt")
