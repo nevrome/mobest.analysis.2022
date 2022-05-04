acea4fab93472af204050f24e5df3b8f5f3f07fd
Clemens Schmid <clemens@nevrome.de>
2022-05-04 11:46:28 +0200

    update of crossvalidation script, now that nuggets can be set per ancestry component

--- code/02_parameter_estimation/03_crossvalidation/crossvalidation.R
+++ code/02_parameter_estimation/03_crossvalidation/crossvalidation.R
@@ -6,11 +6,11 @@ args <- unlist(strsplit(commandArgs(trailingOnly = TRUE), " "))
 run <- args[1]
 ds_for_this_run <- as.numeric(args[2])
 dt_for_this_run <- as.numeric(args[3])
-g_for_this_run <- as.numeric(args[4])
 
 #### data ####
 
 load("data/genotype_data/janno_final.RData")
+load("data/parameter_exploration/variogram/estimated_nuggets.RData")
 
 interpol_comparison <- mobest::crossvalidate(
   independent = mobest::create_spatpos(
@@ -20,21 +20,34 @@ interpol_comparison <- mobest::crossvalidate(
     z = janno_final$Date_BC_AD_Median_Derived
   ),
   dependent = mobest::create_obs(
-    C1 = janno_final$C1,
-    C2 = janno_final$C2,
-    C3 = janno_final$C3
+    C1_mds_u = janno_final$C1_mds_u,
+    C2_mds_u = janno_final$C2_mds_u,
+    C3_mds_u = janno_final$C3_mds_u
   ),
-  kernel = mobest::create_kernset_cross(
-    ds = ds_for_this_run*1000,
-    dt = dt_for_this_run, 
-    g = g_for_this_run
+  kernel = mobest::create_kernset_multi(
+    mobest::create_kernset(
+      C1_mds_u = mobest::create_kernel(
+        ds_for_this_run*1000, ds_for_this_run*1000, dt_for_this_run, 
+        estimated_nuggets$nugget[estimated_nuggets$dependent_var_id == "C1_mds_u"]
+      ),
+      C2_mds_u = mobest::create_kernel(
+        ds_for_this_run*1000, ds_for_this_run*1000, dt_for_this_run,
+        estimated_nuggets$nugget[estimated_nuggets$dependent_var_id == "C2_mds_u"]
+      ),
+      C3_mds_u = mobest::create_kernel(
+        ds_for_this_run*1000, ds_for_this_run*1000, dt_for_this_run, 
+        estimated_nuggets$nugget[estimated_nuggets$dependent_var_id == "C3_mds_u"]
+      )
+    ),
+    .names = paste0("kernel_", run)
   ),
   iterations = 10,
   groups = 10,
   quiet = F
 )
 
-interpol_comparison$ds <- interpol_comparison$ds/1000
+interpol_comparison$dsx <- interpol_comparison$dsx/1000
+interpol_comparison$dsy <- interpol_comparison$dsy/1000
 
 save(interpol_comparison, file = paste0(
   "data/parameter_exploration/crossvalidation/interpol_comparison_", 
