c539ac52e29893f9941c6d03ff39cf06c192c281
Clemens Schmid <clemens@nevrome.de>
2022-05-04 14:38:13 +0200

    adjusted code to analyse the crossvalidation results

--- code/02_parameter_estimation/03_crossvalidation/modify_crossvalidation_results.R
+++ code/02_parameter_estimation/03_crossvalidation/modify_crossvalidation_results.R
@@ -12,22 +12,19 @@ interpol_comparison_raw <- lapply(
   }
 ) %>% dplyr::bind_rows()
 
-# interpol_comparison <- dplyr::slice_sample(interpol_comparison_raw, n = 100000)
-
-# scale nugget correctly
-interpol_comparison <- interpol_comparison_raw %>%
-  dplyr::mutate(g = g/100)
+# interpol_comparison_raw <- dplyr::slice_sample(interpol_comparison_raw, n = 100000)
 
 # calculate squared Euclidean distances
-interpol_comparison_with_CVdist <- interpol_comparison %>%
+interpol_comparison_with_CVdist <- interpol_comparison_raw %>%
   tidyr::pivot_wider(
-    id_cols = c("id", "mixing_iteration", "ds", "dt", "g"),
-    names_from = "dependent_var",
-    values_from = "difference"
+    id_cols = c("id", "mixing_iteration", "dsx", "dsy", "dt"),
+    names_from = "dependent_var_id",
+    values_from = "difference",
+    names_prefix = "CV_diff_"
   ) %>%
   dplyr::mutate(
-    CVdist2 = sqrt(C1_dist^2 + C2_dist^2),
-    CVdist3 = sqrt(C1_dist^2 + C2_dist^2 + C3_dist^2)
+    CVdiff2 = sqrt(CV_diff_C1_mds_u^2 + CV_diff_C2_mds_u^2),
+    CVdiff3 = sqrt(CV_diff_C1_mds_u^2 + CV_diff_C2_mds_u^2 + CV_diff_C3_mds_u^2)
   ) %>%
   tidyr::pivot_longer(
     cols = tidyselect::starts_with("C"),
@@ -37,7 +34,7 @@ interpol_comparison_with_CVdist <- interpol_comparison %>%
 
 # group difference by kernel and dependent_dist
 interpol_comparison_group <- interpol_comparison_with_CVdist %>%
-  dplyr::group_by(ds, dt, g, dependent_var) %>%
+  dplyr::group_by(dsx, dsy, dt, dependent_var) %>%
   dplyr::summarise(
     mean_squared_difference = mean(difference^2),
     .groups = "drop"
@@ -46,14 +43,10 @@ interpol_comparison_group <- interpol_comparison_with_CVdist %>%
 # find best kernel
 best_kernel <- interpol_comparison_group %>% 
   dplyr::filter(
-    dependent_var == "CVdist2" & g == min(g) |
-      dependent_var == "CVdist3" & g == max(g)) %>% 
-  dplyr::group_split(dependent_var, g) %>%
-  purrr::map(function(x) {
-    x %>% 
-      dplyr::filter(mean_squared_difference == min(mean_squared_difference)) %>%
-      magrittr::extract(1,)
-  })
+    dependent_var == "CVdiff2" |
+      dependent_var == "CVdiff3") %>% 
+  dplyr::group_by(dependent_var) %>%
+  dplyr::slice_min(order_by = mean_squared_difference, n = 1)
 
 save(interpol_comparison, file = "data/parameter_exploration/crossvalidation/interpol_comparison.RData")
 save(interpol_comparison_group, file = "data/parameter_exploration/crossvalidation/interpol_comparison_group.RData")
