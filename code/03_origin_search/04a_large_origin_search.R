ec621ccb0dd9410b1db787547b44c8c461247476
Clemens Schmid <clemens@nevrome.de>
2022-05-05 14:08:09 +0200

    rewrote the large origin analysis

--- code/03_origin_search/04a_large_origin_search.R
+++ code/03_origin_search/04a_large_origin_search.R
@@ -11,68 +11,67 @@ age_resampling_run <- as.numeric(args[1])
 #### data ####
 
 load("data/genotype_data/janno_final.RData")
-load("data/spatial/search_area.RData")
-load("data/origin_search/default_kernel.RData")
-load("data/origin_search/retrospection_distance.RData")
+load("data/spatial/extended_area.RData")
+load("data/origin_search/default_kernset_mds2.RData")
+load("data/origin_search/retrospection_distances.RData")
 
-#### prepare model grid ####
-
-model_grid <- mobest::create_model_grid(
-  independent = mobest::create_spatpos_multi(
-    id = janno_final$Poseidon_ID,
-    x = list(janno_final$x),
-    y = list(janno_final$y),
-    z = list(Map(function(x) {x[age_resampling_run]}, janno_final$Date_BC_AD_Sample) %>% unlist()),
-    it = "age_sample"
-  ),
-  dependent = mobest::create_obs(
-    C1 = janno_final$C1,
-    C2 = janno_final$C2
-  ),
-  kernel = default_kernel,
-  prediction_grid = list(
-    scs100_tl50 = mobest::prediction_grid_for_spatiotemporal_area(
-      search_area,
-      spatial_cell_size = 100000,
-      temporal_layers = seq(-8000, 1000, 50)
-    )
-  )
-)
-
-#### run interpolation on model grid ####
-
-interpol_grid <- mobest::run_model_grid(model_grid)
-
-#### spatial origin ####
+janno_final$z <- purrr::map_int(janno_final$Date_BC_AD_Sample, function(x) {x[age_resampling_run]})
 
 janno_search <- janno_final %>%
-  dplyr::mutate(
-    search_z = sapply(janno_final$Date_BC_AD_Sample, function(x){ x[age_resampling_run] })
-  ) %>% dplyr::filter(
+  dplyr::filter(
     region_id != "Other region",
-    search_z >= -7300 &
-      search_z <= 1500
-    )
+    Date_BC_AD_Median_Derived >= -7300 &
+      Date_BC_AD_Median_Derived <= 1500
+  ) %>%
+  dplyr::slice_head(n = 5)
 
-origin_grid <- mobest::search_spatial_origin(
-  independent = mobest::create_spatpos_multi(
-    id = janno_search$Poseidon_ID,
-    x = list(janno_search$x),
-    y = list(janno_search$y),
-    z = list(janno_search$search_z),
-    it = "age_sample"
-  ),
-  dependent = mobest::create_obs(
-    C1 = janno_search$C1,
-    C2 = janno_search$C2
-  ),
-  interpol_grid = interpol_grid,
-  rearview_distance = retrospection_distance
+spatial_pred_grid <- mobest::create_prediction_grid(
+  extended_area,
+  spatial_cell_size = 100000
 )
 
-origin_grid$age_resampling_run <- age_resampling_run
+#### calculate locate probability ####
 
-#### mobility proxy ####
+origin_grid <- mobest::locate_multi(
+  independent = mobest::create_spatpos_multi(
+    mobest::create_spatpos(
+      id = janno_final$Poseidon_ID,
+      x = janno_final$x,
+      y = janno_final$y,
+      z = janno_final$z
+    ),
+    .names = paste0("age_resampling_run_", age_resampling_run)
+  ),
+  dependent = mobest::create_obs_multi(
+    MDS2 = mobest::create_obs(
+      C1_mds_u = janno_final$C1_mds_u,
+      C2_mds_u = janno_final$C2_mds_u
+    )
+  ),
+  kernel = mobest::create_kernset_multi(
+    MDS2 = default_kernset_mds2
+  ),
+  search_independent = mobest::create_spatpos_multi(
+    mobest::create_spatpos(
+      id = janno_search$Poseidon_ID,
+      x = janno_search$x,
+      y = janno_search$y,
+      z = janno_search$z
+    ),
+    .names = paste0("age_resampling_run_", age_resampling_run)
+  ),
+  search_dependent = mobest::create_obs_multi(
+    MDS2 = mobest::create_obs(
+      C1_mds_u = janno_search$C1_mds_u,
+      C2_mds_u = janno_search$C2_mds_u
+    )
+  ),
+  search_space_grid = spatial_pred_grid,
+  search_time = -retrospection_distances["default"],
+  search_time_mode = "relative"
+)
+
+#### save result ####
 
 save(origin_grid, file = paste0(
   "data/origin_search/age_resampling+one_kernel_setting/run_", age_resampling_run, ".RData"
