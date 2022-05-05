c6604f423f91705e9e47686f5e58eed2ec0f3a2a
Clemens Schmid <clemens@nevrome.de>
2022-05-05 15:24:55 +0200

    starting to prepare the workflow to summarize the origin vectors

--- code/03_origin_search/04c_compile_large_origin_search_runs.R
+++ code/03_origin_search/04c_compile_large_origin_search_runs.R
@@ -19,29 +19,38 @@ origin_grid <- lapply(
 ) %>% dplyr::bind_rows()
 
 # join with janno_final 
-origin_grid_modified <- origin_grid %>% 
-  dplyr::mutate(
-    spatial_distance = spatial_distance/1000
-  ) %>%
-  dplyr::left_join(
-    janno_final %>% dplyr::select(Poseidon_ID, region_id),
-    by = c("search_id" = "Poseidon_ID")
-  )
+# origin_grid_modified <- origin_grid %>% 
+#   dplyr::mutate(
+#     spatial_distance = spatial_distance/1000
+#   ) %>%
+#   dplyr::left_join(
+#     janno_final %>% dplyr::select(Poseidon_ID, region_id),
+#     by = c("search_id" = "Poseidon_ID")
+#   )
 
 # define age groups
-age_groups_limits <- seq(-7250, 1750, 500)
+# age_groups_limits <- seq(-7250, 1750, 500)
+# 
+# origin_grid_modified <- origin_grid_modified %>%
+#   dplyr::mutate(
+#     search_z_cut = age_groups_limits[cut(search_z, age_groups_limits, labels = F)] + 250,
+#   )
 
-origin_grid_modified <- origin_grid_modified %>%
-  dplyr::mutate(
-    search_z_cut = age_groups_limits[cut(search_z, age_groups_limits, labels = F)] + 250,
-  )
+prob_product <- mobest::multiply_dependent_probabilities(origin_grid)
 
-# means for search points across age resampling
-origin_grid_mean <- mobest::average_origin_searchid(origin_grid_modified)
+ov <- mobest::determine_origin_vectors(prob_product, independent_table_id)
+
+ov_with_regions <- dplyr::left_join(
+  ov,
+  janno_final %>% dplyr::select(Poseidon_ID, region_id),
+  by = c("search_id" = "Poseidon_ID")
+)
 
 # moving window
-moving_origin_grid <- mobest::average_origin_moving_window(
-  origin_grid_modified,
+
+origin_summary <- mobest::summarize_origin_vectors(
+  ov_with_regions,
+  region_id,
   window_start = -8000,
   window_stop = 2000,
   window_width = 400,
@@ -49,11 +58,20 @@ moving_origin_grid <- mobest::average_origin_moving_window(
 )
 
 # no data windows
-no_data_windows <- mobest::no_data_windows(moving_origin_grid, window_step = 50)
+no_data_windows <- mobest::find_no_data_windows(origin_summary, region_id)
+
+library(ggplot2)
+
+origin_summary %>%
+  ggplot() +
+  geom_point(
+    aes(z, undirected_mean_spatial_distance)
+  ) +
+  facet_wrap(~region_id)
 
 #### save output ####
 
-save(origin_grid_modified, file = "data/origin_search/origin_grid_modified.RData")
-save(origin_grid_mean, file = "data/origin_search/origin_grid_mean.RData")
-save(moving_origin_grid, file = "data/origin_search/moving_origin_grid.RData")
-save(no_data_windows, file = "data/origin_search/no_data_windows.RData")
+# save(origin_grid_modified, file = "data/origin_search/origin_grid_modified.RData")
+# save(origin_grid_mean, file = "data/origin_search/origin_grid_mean.RData")
+# save(moving_origin_grid, file = "data/origin_search/moving_origin_grid.RData")
+# save(no_data_windows, file = "data/origin_search/no_data_windows.RData")
