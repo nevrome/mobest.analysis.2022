4b238556069a7cf525b0d2e7fd36088dd7427ed3
Clemens Schmid <clemens@nevrome.de>
2022-05-06 17:16:43 +0200

    thinking about the consolidation of the origin search

--- code/03_origin_search/04c_compile_large_origin_search_runs.R
+++ code/03_origin_search/04c_compile_large_origin_search_runs.R
@@ -6,50 +6,25 @@ load("data/genotype_data/janno_final.RData")
 
 #### load, merge and modify data ####
 
-# load resampling runs
-origin_grid <- lapply(
+origin_vectors <- lapply(
   list.files(
-    "data/origin_search/age_resampling+one_kernel_setting", 
+    "data/origin_search/age_resampling+one_kernel_setting/age_resampling+one_kernel_setting", 
     pattern = "^run_[0-9]+",
     full.names = T
   ), function(x) {
     load(x)
-    origin_grid
+    origin_vectors
   }
 ) %>% dplyr::bind_rows()
 
-# join with janno_final 
-# origin_grid_modified <- origin_grid %>% 
-#   dplyr::mutate(
-#     spatial_distance = spatial_distance/1000
-#   ) %>%
-#   dplyr::left_join(
-#     janno_final %>% dplyr::select(Poseidon_ID, region_id),
-#     by = c("search_id" = "Poseidon_ID")
-#   )
-
-# define age groups
-# age_groups_limits <- seq(-7250, 1750, 500)
-# 
-# origin_grid_modified <- origin_grid_modified %>%
-#   dplyr::mutate(
-#     search_z_cut = age_groups_limits[cut(search_z, age_groups_limits, labels = F)] + 250,
-#   )
-
-prob_product <- mobest::multiply_dependent_probabilities(origin_grid)
-
-ov <- mobest::determine_origin_vectors(prob_product, independent_table_id)
-
-ov_with_regions <- dplyr::left_join(
-  ov,
+origin_vectors <- dplyr::left_join(
+  origin_vectors_raw,
   janno_final %>% dplyr::select(Poseidon_ID, region_id),
   by = c("search_id" = "Poseidon_ID")
 )
 
-# moving window
-
 origin_summary <- mobest::summarize_origin_vectors(
-  ov_with_regions,
+  origin_vectors,
   region_id,
   window_start = -8000,
   window_stop = 2000,
@@ -57,21 +32,19 @@ origin_summary <- mobest::summarize_origin_vectors(
   window_step = 50
 )
 
-# no data windows
 no_data_windows <- mobest::find_no_data_windows(origin_summary, region_id)
 
-library(ggplot2)
-
-origin_summary %>%
-  ggplot() +
-  geom_point(
-    aes(z, undirected_mean_spatial_distance)
-  ) +
-  facet_wrap(~region_id)
+# library(ggplot2)
+# origin_summary %>%
+#   ggplot() +
+#   geom_point(
+#     aes(z, undirected_mean_spatial_distance, color = mean_angle_deg)
+#   ) +
+#   facet_wrap(~region_id) +
+#   scale_color_gradientn(colours = c("red", "green", "blue", "red"))
 
 #### save output ####
 
-# save(origin_grid_modified, file = "data/origin_search/origin_grid_modified.RData")
-# save(origin_grid_mean, file = "data/origin_search/origin_grid_mean.RData")
-# save(moving_origin_grid, file = "data/origin_search/moving_origin_grid.RData")
-# save(no_data_windows, file = "data/origin_search/no_data_windows.RData")
+save(origin_vectors, file = "data/origin_search/origin_vectors.RData")
+save(origin_summary, file = "data/origin_search/origin_summary.RData")
+save(no_data_windows, file = "data/origin_search/no_data_windows.RData")
