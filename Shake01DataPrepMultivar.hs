d3a158e85573e8dc7bf6e227844d33df8d77293e
Clemens Schmid <clemens@nevrome.de>
2022-07-01 16:23:37 +0200

    more work on the multivar shake file - should be able to run now

--- Shake01DataPrepMultivar.hs
+++ Shake01DataPrepMultivar.hs
@@ -10,7 +10,7 @@ import Development.Shake
 main :: IO ()
 main = shakeArgs myShakeOpts $ do
 
-  --want $ [dataGenoSnpSubUnfilteredModern "unfiltered_snp_selection_with_modern_reference_pops.bed"]
+  want $ [dataGeno "janno_final.RData"]
 
   -- 05
 
@@ -24,7 +24,7 @@ main = shakeArgs myShakeOpts $ do
 
   code0105 "03_pca_smartsnp_unfiltered.Rsmartsnp" %$
     [ dataGenoSnpSubFiltered "filtered_snp_selection.bed" ] -->
-    [ dataGenoMultivarPCAFiltered "pca_out.RData" ]
+    [ dataGenoMultivarPCAUnfiltered "pca_out.RData" ]
 
   code0105 "04_pca_smartsnp_filtered.Rsmartsnp" %$
     [ dataGenoSnpSubFiltered "filtered_snp_selection.bed" ] -->
@@ -32,7 +32,7 @@ main = shakeArgs myShakeOpts $ do
 
   code0105 "05_emu_unfiltered.shemu" %$
     [ dataGenoSnpSubFiltered "filtered_snp_selection.bed" ] -->
-    [ dataGenoMultivarEMUFiltered "emu_out.txt.eigenvecs" ]
+    [ dataGenoMultivarEMUUnfiltered "emu_out.txt.eigenvecs" ]
 
   code0105 "06_emu_filtered.shemu" %$
     [ dataGenoSnpSubFiltered "filtered_snp_selection.bed" ] -->
@@ -40,8 +40,26 @@ main = shakeArgs myShakeOpts $ do
 
   code0105 "07_pca_smartsnp_projected_unfiltered.Rsmartsnp" %$
     [ dataGenoSnpSubFiltered "filtered_snp_selection.bed" ] -->
-    [ dataGenoMultivarPCAProjFiltered "pca_out.RData" ]
+    [ dataGenoMultivarPCAProjUnfiltered "pca_out.RData" ]
 
   code0105 "08_pca_smartsnp_projected_filtered.Rsmartsnp" %$
     [ dataGenoSnpSubFiltered "filtered_snp_selection.bed" ] -->
     [ dataGenoMultivarPCAProjFiltered "pca_out.RData" ]
+
+  code01 "06_prepare_final_dataset.R" %$
+    [ dataGenoSnpSubFiltered "filtered_snp_selection.janno"
+    , dataGeno "janno_without_identicals.RData"
+    , dataSpatial "epsg3035.RData"
+    , dataSpatial "mobility_regions.RData"
+    , dataGenoMultivarMDSUnfiltered "mds.mds"
+    , dataGenoMultivarMDSFiltered "mds.mds"
+    , dataGenoMultivarPCAUnfiltered "pca_out.RData"
+    , dataGenoMultivarPCAFiltered "pca_out.RData"
+    , dataGenoMultivarEMUUnfiltered "emu_out.txt.eigenvecs"
+    , dataGenoMultivarEMUFiltered "emu_out.txt.eigenvecs"
+    , dataGenoMultivarPCAProjUnfiltered "pca_out.RData"
+    , dataGenoMultivarPCAProjFiltered "pca_out.RData"
+    ] -->
+    [ dataGeno "janno_final.RData"
+    , dataGeno "multivar_perm_obs_bundles.RData"
+    ]
