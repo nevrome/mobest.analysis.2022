3bb61b6bb3882aebafdf2d4679b17c3c1d8fe11a
Clemens Schmid <clemens@nevrome.de>
2022-07-12 17:53:08 +0200

    started to rework the script that summarises the crossvalidation result

--- code/02_parameter_estimation/03_crossvalidation/modify_crossvalidation_results.R
+++ code/02_parameter_estimation/03_crossvalidation/modify_crossvalidation_results.R
@@ -1,32 +1,37 @@
 library(magrittr)
 
-# read and rbind all crossvalidation run output files
-interpol_comparison <- lapply(
-  list.files(
-    "data/parameter_exploration/crossvalidation", 
-    pattern = "interpol_comparison_[0-9]+",
-    full.names = T
-  ), function(x) {
-    load(x)
-    interpol_comparison
-  }
-) %>% dplyr::bind_rows()
+patterns <- list.files("data/parameter_exploration/crossvalidation") %>%
+  strsplit("_", perl = T) %>%
+  purrr::map(function(x) {x[c(3,4,5)]}) %>%
+  unique
 
-# interpol_comparison <- dplyr::slice_sample(interpol_comparison, n = 100000)
+# apply function to each pattern
+purrr::map_dfr(patterns, function(p) {
+  # read and rbind all crossvalidation run output files
+  cur_interpol_comparison <- lapply(
+    list.files(
+      "data/parameter_exploration/crossvalidation", 
+      pattern = paste0(c("interpol_comparison", p, "[0-9]+"), collapse = "_"),
+      full.names = T
+    ), function(x) {
+      load(x)
+      interpol_comparison
+    }
+  ) %>% dplyr::bind_rows()
+  # group difference by kernel
+  interpol_comparison_group <- cur_interpol_comparison %>%
+    dplyr::group_by(dsx, dsy, dt) %>%
+    dplyr::summarise(
+      mean_squared_difference = mean(difference^2),
+      .groups = "drop"
+    )
+  # find best kernel
+  best_kernel <- interpol_comparison_group %>%
+    dplyr::slice_min(order_by = mean_squared_difference, n = 1)
+  
+  best_kernel
+})
 
-# group difference by kernel and dependent_dist
-interpol_comparison_group <- interpol_comparison %>%
-  dplyr::group_by(dsx, dsy, dt, dependent_var_id) %>%
-  dplyr::summarise(
-    mean_squared_difference = mean(difference^2),
-    .groups = "drop"
-  )
-
-# find best kernel
-best_kernel <- interpol_comparison_group %>%
-  dplyr::group_by(dependent_var_id) %>%
-  dplyr::slice_min(order_by = mean_squared_difference, n = 1) %>%
-  dplyr::ungroup()
 
 save(interpol_comparison, file = "data/parameter_exploration/crossvalidation/interpol_comparison.RData")
 save(interpol_comparison_group, file = "data/parameter_exploration/crossvalidation/interpol_comparison_group.RData")
