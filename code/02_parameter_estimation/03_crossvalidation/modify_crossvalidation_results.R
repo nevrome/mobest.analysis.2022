cdd6d5bd381d33951af8e781930b05e8239d6518
Clemens Schmid <clemens@nevrome.de>
2022-07-13 18:11:46 +0200

    started to work on a mechanism to load the right subset of files

--- code/02_parameter_estimation/03_crossvalidation/modify_crossvalidation_results.R
+++ code/02_parameter_estimation/03_crossvalidation/modify_crossvalidation_results.R
@@ -7,8 +7,8 @@ patterns <- list.files("data/parameter_exploration/crossvalidation") %>%
 
 # apply function to each pattern
 future::plan(future::multisession, workers = 8)
-furrr::future_map_dfr(patterns, function(p) {
-  # read and rbind all crossvalidation run output files
+best_kernels <- furrr::future_map_dfr(patterns, function(p) {
+  # read and rbind all crossval output files for this pattern
   cur_interpol_comparison <- lapply(
     list.files(
       "data/parameter_exploration/crossvalidation", 
@@ -20,18 +20,50 @@ furrr::future_map_dfr(patterns, function(p) {
     }
   ) %>% dplyr::bind_rows()
   # group difference by kernel
-  interpol_comparison_group <- cur_interpol_comparison %>%
+  cur_interpol_comparison_group <- cur_interpol_comparison %>%
     dplyr::group_by(dependent_var_id, dim, multivar_method, multivar_fstate, dsx, dsy, dt) %>%
     dplyr::summarise(
+      g = unique(g),
       mean_squared_difference = mean(difference^2),
       .groups = "drop"
     )
   # find best kernel
-  best_kernel <- interpol_comparison_group %>%
+  best_kernel <- cur_interpol_comparison_group %>%
     dplyr::slice_min(order_by = mean_squared_difference, n = 1)
-  
-  best_kernel
+  return(best_kernel)
 })
+
+methods_and_fstates <- best_kernels %>%
+  dplyr::group_by(multivar_method, multivar_fstate) %>%
+  dplyr::summarise(.groups = "drop")
+
+# multivar comparison
+purrr::pmap(methods_and_fstates, function(method, fstate) {
+  # get best dsx and dsy for this setup
+  ? <- best_kernels %>%
+    dplyr::filter(multivar_method == method, multivar_fstate == fstate)
+  # read and rbind all crossval files for this multivar_method + multivar_fstate with the best kernels
+  cur_interpol_comparison <- lapply(
+    list.files(
+      "data/parameter_exploration/crossvalidation", 
+      pattern = paste(method, fstate, sep = "_"),
+      full.names = T
+    ), function(x) {
+      load(x)
+      
+      
+      loaded_dim <- interpol_comparison$dim %>% unique()
+      loaded_method <- interpol_comparison$multivar_method %>% unique()
+      loaded_fstate <- interpol_comparison$multivar_fstate %>% unique()
+      loaded_dsx <- interpol_comparison$dsx %>% unique()
+      loaded_dt <- interpol_comparison$dt %>% unique()
+      cur_best_kernels %>%
+        dplyr::filter(dim == loaded_dim)
+      if (cur_best_kernels$dsx
+    }
+  ) %>% dplyr::bind_rows()
+})
+
 
 
 save(interpol_comparison, file = "data/parameter_exploration/crossvalidation/interpol_comparison.RData")
