e9bef8ffbd96c87a32a2ed96cc947df2f9516417
Clemens Schmid <clemens@nevrome.de>
2022-07-20 13:57:34 +0200

    update of crossvalidation raster figure

--- code/04_plot_scripts/paper/figure_sup_9_crossvalidation_rasters.R
+++ code/04_plot_scripts/paper/figure_sup_9_crossvalidation_rasters.R
@@ -1,15 +1,25 @@
 library(magrittr)
 library(ggplot2)
 
-load("data/parameter_exploration/crossvalidation/interpol_comparison_group.RData")
-load("data/parameter_exploration/crossvalidation/best_kernel.RData")
+load("data/parameter_exploration/crossvalidation_best_kernels.RData")
+load("data/parameter_exploration/crossvalidation_kernel_comparison.RData")
 
 # for each ancestry component
 ps <- purrr::map2(
-  interpol_comparison_group %>%
-    dplyr::group_split(dependent_var_id),
-  best_kernel %>%
-    dplyr::group_split(dependent_var_id),
+  crossvalidation_kernel_comparison %>%
+    dplyr::filter(
+      multivar_fstate == "u",
+      multivar_method %in% c("mds", "pca_proj"),
+      dim %in% c("C1", "C2", "C3")
+    ) %>%
+    dplyr::group_split(multivar_method, dim),
+  crossvalidation_best_kernels %>%
+    dplyr::filter(
+      multivar_fstate == "u",
+      multivar_method %in% c("mds", "pca_proj"),
+      dim %in% c("C1", "C2", "C3")
+    ) %>%
+    dplyr::group_split(multivar_method, dim),
   function(a, b) {
     ggplot() +
     geom_tile(
@@ -29,8 +39,8 @@ ps <- purrr::map2(
     ) +
     xlab(latex2exp::TeX("$\\sqrt{\\theta_s}$")) +
     ylab(latex2exp::TeX("$\\sqrt{\\theta_t}$")) +
-    scale_x_continuous(breaks = seq(0, 2000, 100)) +
-    scale_y_continuous(breaks = seq(0, 2000, 100)) +
+    scale_x_continuous(breaks = seq(0, 1500, 100)) +
+    scale_y_continuous(breaks = seq(0, 1500, 100)) +
     geom_point(
       data = b,
       aes(x = dsx, y = dt), 
@@ -38,8 +48,8 @@ ps <- purrr::map2(
     ) +
     annotate(
       "text",
-      x = b$dsx + 500, y = b$dt,
-      label = latex2exp::TeX(paste0("$\\sqrt{\\theta_s}$ = ", b$dsx, " | \\sqrt{$\\theta_t}$ = ", b$dt)),
+      x = b$dsx + 300, y = b$dt,
+      label = latex2exp::TeX(paste0("$$\\overset{\\sqrt{\\theta_s} = ", b$dsx, "}{\\sqrt{\\theta_t}$ = ", b$dt, "}")),
       parse = TRUE,
       color = "red",
       size = 4
@@ -50,12 +60,12 @@ ps <- purrr::map2(
 p <- cowplot::plot_grid(plotlist = ps, nrow = 2, ncol = 3, labels = "AUTO")
 
 ggsave(
-  "plots/figure_sup_9_crossvalidation_rasters.pdf",
+  "plots/figure_sup_9_crossvalidation_rasters2.pdf",
   plot = p,
   device = "pdf",
   scale = 0.8,
   dpi = 300,
-  width = 350, height = 300, units = "mm",
+  width = 450, height = 280, units = "mm",
   limitsize = F,
   bg = "white"
 )
