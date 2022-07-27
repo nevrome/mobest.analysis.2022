09e0daa56dc906674bbd05ab34d91eb53fd70af3
Clemens Schmid <clemens@nevrome.de>
2022-07-27 15:38:21 +0200

    added two more dimensions to figure sup 14

--- code/04_plot_scripts/paper/figure_sup_14_pca_proj.R
+++ code/04_plot_scripts/paper/figure_sup_14_pca_proj.R
@@ -43,6 +43,8 @@ p_pca <- function(v1, v2, plot_order_dim, plot_order_factor = 1) {
 C1 <- "C1_pca_proj_u"
 C2 <- "C2_pca_proj_u"
 C3 <- "C3_pca_proj_u"
+C4 <- "C4_pca_proj_u"
+C5 <- "C5_pca_proj_u"
 
 p_C1C2 <- p_pca(C1, C2, C3)
 p_legend <- cowplot::get_legend(p_C1C2)
@@ -51,18 +53,22 @@ p_C1C2 <- p_C1C2 + theme(legend.position = "none")
 p_C1C3 <- p_pca(C1, C3, C2, -1) + theme(legend.position = "none")
 p_C3C2 <- p_pca(C3, C2, C1) + theme(legend.position = "none") + scale_x_reverse()
 
+p_C1C4 <- p_pca(C1, C4, C1, -1) + theme(legend.position = "none")
+p_C1C5 <- p_pca(C1, C5, C1, -1) + theme(legend.position = "none")
+
 total <- cowplot::plot_grid(
-  p_C1C2, p_C3C2, p_C1C3, p_legend, nrow = 2, ncol = 2, labels = c("A", "B", "C", NA),
-  rel_heights = c(1,1), align = "hv", axis = "l"
+  p_C1C2, p_C3C2, p_C1C3, p_legend, p_C1C4, p_C1C5,
+  nrow = 3, ncol = 2, labels = c("A", "B", "C", NA),
+  rel_heights = c(1,1,1), align = "hv", axis = "l"
 )
 
 ggsave(
-  paste0("plots/figure_sup_14_pca_proj_3.pdf"),
+  paste0("plots/figure_sup_14_pca_proj.pdf"),
   plot = total,
   device = "pdf",
   scale = 0.9,
   dpi = 300,
-  width = 320, height = 300, units = "mm",
+  width = 320, height = 400, units = "mm",
   limitsize = F,
   bg = "white"
 )
