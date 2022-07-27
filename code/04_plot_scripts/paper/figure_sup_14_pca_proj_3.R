d57f33bbf8c1e322b50e6380a78ca5293de9031c
Clemens Schmid <clemens@nevrome.de>
2022-07-27 15:18:38 +0200

    transformed sup fig 14 to a proper pca version of the plot

--- code/04_plot_scripts/paper/figure_sup_14_pca_proj_3.R
+++ code/04_plot_scripts/paper/figure_sup_14_pca_proj_3.R
@@ -5,7 +5,7 @@ load("data/genotype_data/janno_final.RData")
 load("data/plot_reference_data/region_id_shapes.RData")
 load("data/plot_reference_data/age_colors_gradient.RData")
 
-p_mds <- function(v1, v2, plot_order_dim, plot_order_factor = 1) {
+p_pca <- function(v1, v2, plot_order_dim, plot_order_factor = 1) {
   ggplot() +
     geom_point(
       data = janno_final %>% dplyr::arrange(.data[[plot_order_dim]] * plot_order_factor),
@@ -20,9 +20,7 @@ p_mds <- function(v1, v2, plot_order_dim, plot_order_factor = 1) {
       values = region_id_shapes
     ) +
     age_colors_gradient +
-    coord_fixed() +
-    scale_y_continuous(breaks = seq(-0.1, 0.1, 0.02)) +
-    scale_x_continuous(breaks = seq(-0.1, 0.1, 0.02)) +
+    #coord_fixed() +
     theme_bw() +
     theme(
       legend.position = "bottom",
@@ -42,25 +40,24 @@ p_mds <- function(v1, v2, plot_order_dim, plot_order_factor = 1) {
     )
 }
 
-C1 <- "C1_mds_u"#"C1_pca_proj_u"
-C2 <- "C2_mds_u"#"C2_pca_proj_u"
-C3 <- "C3_mds_u"#"C3_pca_proj_u"
+C1 <- "C1_pca_proj_u"
+C2 <- "C2_pca_proj_u"
+C3 <- "C3_pca_proj_u"
 
-p_C1C2 <- p_mds(C1, C2, C3)
+p_C1C2 <- p_pca(C1, C2, C3)
 p_legend <- cowplot::get_legend(p_C1C2)
 
 p_C1C2 <- p_C1C2 + theme(legend.position = "none")
-p_C1C3 <- p_mds(C1, C3, C2, -1) + theme(legend.position = "none")
-p_C3C2 <- p_mds(C3, C2, C1) + theme(legend.position = "none") + scale_x_reverse()
+p_C1C3 <- p_pca(C1, C3, C2, -1) + theme(legend.position = "none")
+p_C3C2 <- p_pca(C3, C2, C1) + theme(legend.position = "none") + scale_x_reverse()
 
 total <- cowplot::plot_grid(
-  cowplot::plot_grid(p_C1C2, p_C3C2, ncol = 2, labels = c("A", "B")),
-  cowplot::plot_grid(p_C1C3, p_legend, ncol = 2, labels = c("C", NA)), 
-  nrow = 2, rel_heights = c(1,0.635)
+  p_C1C2, p_C3C2, p_C1C3, p_legend, nrow = 2, ncol = 2, labels = c("A", "B", "C", NA),
+  rel_heights = c(1,1), align = "hv", axis = "l"
 )
 
 ggsave(
-  paste0("plots/figure_sup_14_mds3.pdf"),
+  paste0("plots/figure_sup_14_pca_proj_3.pdf"),
   plot = total,
   device = "pdf",
   scale = 0.9,
