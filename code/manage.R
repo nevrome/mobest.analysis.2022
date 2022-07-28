07c487ecf24d7c475b9c013c29f9d1f06234fa78
Clemens Schmid <clemens@nevrome.de>
2022-07-28 18:37:48 +0200

    more targeted result download

--- code/manage.R
+++ code/manage.R
@@ -24,7 +24,11 @@ down("plots/")
 down("tables/")
 down("data/genotype_data/poseidon_extracted/")
 down("data/")
-down("data/parameter_exploration/crossvalidation/")
+
+down("data/parameter_exploration/distance_products.RData")
+down("data/parameter_exploration/crossvalidation_best_kernels.RData")
+down("data/parameter_exploration/crossvalidation_kernel_comparison.RData")
+down("data/parameter_exploration/crossvalidation_multivar_comparison.RData")
 
 #### run all plotting scripts ####
 
