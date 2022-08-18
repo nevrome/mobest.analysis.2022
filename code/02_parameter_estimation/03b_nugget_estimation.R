637a8267149c657cb0ca32066fde507e9170aa54
Clemens Schmid <clemens@nevrome.de>
2022-08-18 17:53:28 +0200

    switched to nugget culculation on residuals

--- code/02_parameter_estimation/03b_nugget_estimation.R
+++ code/02_parameter_estimation/03b_nugget_estimation.R
@@ -4,9 +4,17 @@ load("data/genotype_data/janno_final.RData")
 load("data/parameter_exploration/targeted/all_distances.RData")
 
 d_all_long <- d_all %>% tidyr::pivot_longer(
-  cols = c(C1_mds_u_dist, C2_mds_u_dist, C3_mds_u_dist),
+  cols = c(C1_mds_u_dist_resid, C2_mds_u_dist_resid, C3_mds_u_dist_resid),
   names_to = "dist_type", values_to = "dist_val"
-)
+) %>%
+  dplyr::mutate(
+    dist_type = dplyr::recode(
+      dist_type,
+      C1_mds_u_dist_resid = "C1_mds_u",
+      C2_mds_u_dist_resid = "C2_mds_u",
+      C3_mds_u_dist_resid = "C3_mds_u"
+    )
+  )
 
 lower_left_variogram <- d_all_long %>%
   # filter for small temporal and spatial pairwise distances
@@ -15,9 +23,9 @@ lower_left_variogram <- d_all_long %>%
   dplyr::mutate(
     # rescaling of the dist val to a relative proportion
     dist_val_adjusted = dplyr::case_when(
-      dist_type == "C1_mds_u_dist" ~ 0.5*(dist_val^2/stats::var(janno_final$C1_mds_u)),
-      dist_type == "C2_mds_u_dist" ~ 0.5*(dist_val^2/stats::var(janno_final$C2_mds_u)),
-      dist_type == "C3_mds_u_dist" ~ 0.5*(dist_val^2/stats::var(janno_final$C3_mds_u)),
+      dist_type == "C1_mds_u" ~ 0.5*(dist_val^2/stats::var(janno_final$C1_mds_u)),
+      dist_type == "C2_mds_u" ~ 0.5*(dist_val^2/stats::var(janno_final$C2_mds_u)),
+      dist_type == "C3_mds_u" ~ 0.5*(dist_val^2/stats::var(janno_final$C3_mds_u)),
     )
   )
 
