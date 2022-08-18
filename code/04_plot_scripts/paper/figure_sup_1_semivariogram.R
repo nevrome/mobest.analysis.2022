f0270e8311df1a53a6ee30d5d824ed257c7ebac0
Clemens Schmid <clemens@nevrome.de>
2022-08-18 17:05:27 +0200

    update of figure

--- code/04_plot_scripts/paper/figure_sup_1_semivariogram.R
+++ code/04_plot_scripts/paper/figure_sup_1_semivariogram.R
@@ -1,11 +1,11 @@
 library(ggplot2)
 library(magrittr)
 
-load("data/parameter_exploration/variogram/binned_distances.RData")
+load("data/parameter_exploration/targeted/binned_distances.RData")
 
 d_binned_long <- d_binned %>%
   tidyr::pivot_longer(
-    cols = tidyselect::one_of(c("C1_dist", "C2_dist", "C1_dist_resid", "C2_dist_resid")),
+    cols = tidyselect::one_of(c("C1_mds_u_dist", "C2_mds_u_dist", "C1_mds_u_dist_resid", "C2_mds_u_dist_resid")),
     names_to = "distance_type", values_to = "distance_value"
   ) %>%
   dplyr::mutate(
@@ -15,7 +15,7 @@ d_binned_long <- d_binned %>%
     ),
     distance_type = sub("_resid", "", distance_type),
     distance_type = sub("_dist", "", distance_type),
-    distance_type = factor(distance_type, levels = c("C1", "C2"))
+    distance_type = factor(distance_type, levels = c("C1_mds_u", "C2_mds_u"))
   ) %>%
   dplyr::mutate(
     detrended = factor(detrended, levels = c("not detrended", "detrended (residuals)"))
@@ -45,10 +45,11 @@ ps <- lapply(
       legend.text = element_text(size = 7, angle = 45, hjust = 0.9)
     ) +
     guides(
-      fill = guide_colorbar(title = "distance:", barwidth = 6)
+      fill = guide_colorbar(title = "half mean squared distance:", barwidth = 6)
     ) +
     xlab("spatial distance: 100km bins") +
-    ylab("temporal distance: 100y bins")
+    ylab("temporal distance: 100y bins") +
+    scale_x_continuous(breaks = c(1000, 2000, 3000, 4000, 5000))
 
 })
   
