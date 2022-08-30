a9309576ee04b27882ed389eb2e2595fccb3919e
Clemens Schmid <clemens@nevrome.de>
2022-08-30 22:36:56 +0200

    more hacky data prep for mobest-hs

--- code/prep_mobesths.R
+++ code/prep_mobesths.R
@@ -1,20 +1,5 @@
 library(magrittr)
 
-load("data/spatial/epsg3035.RData")
-
-load("data/genotype_data/janno_final.RData")
-readr::write_csv(
-  x = cbind(
-      janno_final$Poseidon_ID,
-      janno_final %>% update_coords("x", "y"),
-      janno_final$Date_BC_AD_Median_Derived,
-      janno_final$C1_mds_u,
-      janno_final$C2_mds_u
-  ),
-  file = "~/Desktop/janno.csv",
-  col_names = FALSE
-)
-
 update_coords <- function(df, x, y, incrs = epsg3035, outcrs = "+proj=robin") {
   df_sf <- df %>%
     sf::st_as_sf(coords = c(x, y), crs = incrs) %>%
@@ -23,6 +8,37 @@ update_coords <- function(df, x, y, incrs = epsg3035, outcrs = "+proj=robin") {
     as.data.frame() %>%
     set_colnames(c(x, y))
 }
+
+load("data/spatial/epsg3035.RData")
+load("data/genotype_data/janno_final.RData")
+load("data/origin_search/packed_origin_vectors.RData")
+
+samples <- janno_final %>%
+  dplyr::left_join(
+    packed_origin_vectors %>%
+      dplyr::filter(multivar_method == "mds2" & search_time == -667),
+    by = c("Poseidon_ID" = "search_id") 
+  )
+
+samples$field_x[is.na(samples$field_x)] <- 0
+samples$field_y[is.na(samples$field_y)] <- 0
+
+samples$field_x <- samples$field_x * 1000
+samples$field_y <- samples$field_y * 1000
+
+readr::write_csv(
+  x = cbind(
+      samples$Poseidon_ID,
+      samples %>% update_coords("x", "y"),
+      samples$Date_BC_AD_Median_Derived,
+      samples$C1_mds_u,
+      samples$C2_mds_u,
+      purrr::map_chr(samples$Group_Name, (\(x) x[[1]])),
+      samples %>% update_coords("field_x", "field_y")
+  ),
+  file = "~/Desktop/samples.csv",
+  col_names = FALSE
+)
 
 sf::read_sf("~/Downloads/ne_50m_coastline/ne_50m_coastline.shp") %>%
   sf::st_transform("+proj=robin") %$%
