810dbf1366ab6e471d11e92e575a40b973ee3693
Clemens Schmid <clemens@nevrome.de>
2022-09-05 18:29:17 +0200

    work on the retrospection comparison figure

--- code/04_plot_scripts/paper/figure_sup_22_mobility_curve_comparison.R
+++ code/04_plot_scripts/paper/figure_sup_22_mobility_curve_comparison.R
@@ -1,42 +1,59 @@
 library(magrittr)
-
-get_mean_points <- function(x, run) {
-  x %>%
-    dplyr::select(mean_search_z, search_id, region_id, directed_mean_spatial_distance) %>%
-    dplyr::mutate(run = run)
-}
-
-load("data/origin_search/origin_grid_mean.RData")
-mds2 <- origin_grid_mean %>% get_mean_points("Default: 2D MDS")
-load("data/origin_search/origin_grid_mean_mds3.RData")
-mds3 <- origin_grid_mean %>% get_mean_points("3D MDS")
-load("data/origin_search/origin_grid_derived_data_retro_low.RData")
-low_retro <- origin_grid_derived_data$origin_grid_mean %>% get_mean_points("Lower rearview distance")
-load("data/origin_search/origin_grid_derived_data_retro_high.RData")
-high_retro <- origin_grid_derived_data$origin_grid_mean %>% get_mean_points("Higher rearview distance")
-
-comb <- dplyr::bind_rows(mds3, low_retro, high_retro) %>%
-  dplyr::left_join(mds2 %>% dplyr::select(search_id, spacedist = directed_mean_spatial_distance)) %>%
-  dplyr::mutate(diff_to_mds2 = directed_mean_spatial_distance - spacedist) %>%
-  # order by divergence, to highlight stronger colours in plot
-  dplyr::group_by(run, region_id) %>%
-  dplyr::arrange(abs(diff_to_mds2), .by_group = TRUE) %>%
-  dplyr::ungroup()
-
 library(ggplot2)
 
-p <- ggplot() +
+load("data/origin_search/packed_origin_vectors.RData")
+
+prep <- packed_origin_vectors %>%
+  dplyr::select(search_id, region_id, multivar_method, search_time, search_z, ov_dist) %>%
+  dplyr::filter(
+    region_id %in% c("Britain and Ireland", "Central Europe", "Italy", "Southeastern Europe")
+  )
+
+ovs <- dplyr::left_join(
+    prep,
+    prep %>% dplyr::select(-region_id, -search_z),
+    by = c("search_id")
+  ) %>%
+  dplyr::filter(
+    multivar_method.x == "mds2" & search_time.x == -667 &
+      ( (multivar_method.y == "mds2" & search_time.y != -667 & search_time.x != search_time.y) |
+          (multivar_method.y == "pca5" & search_time.y == -667 & search_time.x == search_time.y) )
+  ) %>% 
+  dplyr::mutate(
+    run_raw = paste(multivar_method.y, search_time.y),
+    run = dplyr::case_when(
+      run_raw == "mds2 -1015" ~ "MDS2 High rearview distance",
+      run_raw == "mds2 -378" ~ "MDS2 Low rearview distance",
+      run_raw == "pca5 -667" ~ "PCA5"
+    ),
+    ovs_dist = ov_dist.y - ov_dist.x
+  ) %>%
+  # sort by ovs_dist
+  dplyr::arrange(dplyr::desc(ovs_dist))
+
+p <- ovs %>% ggplot() +
   facet_grid(rows = dplyr::vars(region_id), cols = dplyr::vars(run)) +
+  geom_segment(
+    mapping = aes(x = search_z, xend = search_z, y = ov_dist.x, yend = ov_dist.y, color = ovs_dist),
+    alpha = 0.7,
+    size = 1
+  ) +
+  # geom_point(
+  #   mapping = aes(
+  #     x = search_z, y = ov_dist.y, color = ovs_dist
+  #   ),
+  #   alpha = 1,
+  #   size = 0.3,
+  #   shape = 20
+  # ) +
   geom_point(
-    data = comb,
     mapping = aes(
-      x = mean_search_z, y = directed_mean_spatial_distance, fill = diff_to_mds2
+      x = search_z, y = ov_dist.x
     ),
     alpha = 1,
-    size = 1.7,
-    shape = 21,
-    colour = "black",
-    stroke = 0.1
+    size = 0.3,
+    shape = 20,
+    color = "black"
   ) +
   theme_bw() +
   theme(
@@ -47,13 +64,13 @@ p <- ggplot() +
     ylim = c(-100, 3000)
   ) +
   scale_x_continuous(breaks = seq(-7000, 1000, 2000)) +
-  scale_fill_gradient2(
+  scale_color_gradient2(
     low = "#0571b0",
     mid = "white",
     high = "#ca0020"
   ) +
   guides(
-    fill = guide_colorbar(title = "Divergence from default [km]    ", barwidth = 20, barheight = 1.5),
+    color = guide_colorbar(title = "Divergence from MDS2 [km]    ", barwidth = 20, barheight = 1.5),
   ) +
   xlab("time [years calBC/calAD]") +
   ylab("spatial distance to \"origin point\" (directed mean) [km]") 
@@ -64,6 +81,6 @@ ggsave(
   device = "pdf",
   scale = 0.6,
   dpi = 300,
-  width = 430, height = 390, units = "mm",
+  width = 430, height = 300, units = "mm",
   limitsize = F
 )
