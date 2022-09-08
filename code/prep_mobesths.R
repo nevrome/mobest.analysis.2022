8584efaf0912fc177386c4bd49a726b298b8098c
Clemens Schmid <clemens@nevrome.de>
2022-09-08 18:39:12 +0200

    wokring on the data prep script

--- code/prep_mobesths.R
+++ code/prep_mobesths.R
@@ -1,12 +1,19 @@
 library(magrittr)
 
 update_coords <- function(df, x, y, incrs = epsg3035, outcrs = "+proj=robin") {
-  df_sf <- df %>%
+  df_selected <- df %>% dplyr::select(.data[[x]], .data[[y]]) %>% dplyr::mutate(i = seq_len(nrow(.)))
+  df_without_na <- df_selected %>%
+    dplyr::filter(!is.na(.data[[x]]) & !is.na(.data[[y]]))
+  df_transformed <- df_without_na %>%
     sf::st_as_sf(coords = c(x, y), crs = incrs) %>%
-    sf::st_transform(outcrs)
-  sf::st_coordinates(df_sf) %>%
+    sf::st_transform(outcrs) %>%
+    sf::st_coordinates() %>%
     as.data.frame() %>%
-    set_colnames(c(x, y))
+    set_colnames(c(x, y)) %>%
+    dplyr::mutate(i = df_without_na$i)
+  df_selected[,"i"] %>%
+    dplyr::left_join(df_transformed, by = "i") %>%
+    dplyr::select(-i)
 }
 
 load("data/spatial/epsg3035.RData")
@@ -16,28 +23,42 @@ load("data/origin_search/packed_origin_vectors.RData")
 samples <- janno_final %>%
   dplyr::left_join(
     packed_origin_vectors %>%
-      dplyr::filter(multivar_method == "mds2" & search_time == -667),
+      dplyr::mutate(
+        search_time_mode = dplyr::case_when(
+          search_time == -1015 ~ "high",
+          search_time == -667 ~ "default",
+          search_time == -378 ~ "low"
+        )
+      ) %>%
+      dplyr::filter(ifelse(multivar_method == "pca5", search_time_mode == "default", TRUE)) %>%
+      #dplyr::filter(multivar_method == "mds2" & search_time == -667)
+      tidyr::pivot_wider(
+        id_cols = search_id,
+        names_from = tidyselect::all_of(c("multivar_method", "search_time_mode")),
+        values_from = tidyselect::all_of(c("field_x", "field_y"))
+      ),
     by = c("Poseidon_ID" = "search_id") 
   )
-
-samples$field_x[is.na(samples$field_x)] <- 0
-samples$field_y[is.na(samples$field_y)] <- 0
 
 samples$field_x <- samples$field_x * 1000
 samples$field_y <- samples$field_y * 1000
 
+composite_dataset <- tibble::tibble(
+  sampleID = samples$Poseidon_ID,
+  sampleGroup = purrr::map_chr(samples$Group_Name, (\(x) x[[1]])),
+  samples %>% update_coords("x", "y") %>% set_names(c("sampleX", "sampleY")),
+  sampleZ = samples$Date_BC_AD_Median_Derived,
+  sampleC1MDS = samples$C1_mds_u,
+  sampleC2MDS = samples$C2_mds_u,
+  sampleC1PCA = samples$C1_pca_proj_u,
+  sampleC2PCA = samples$C2_pca_proj_u,
+  samples %>% update_coords("field_x", "field_y") %>% set_names(c("sampleFieldX", "sampleFieldY"))
+)
+
 readr::write_csv(
-  x = cbind(
-      samples$Poseidon_ID,
-      samples %>% update_coords("x", "y"),
-      samples$Date_BC_AD_Median_Derived,
-      samples$C1_mds_u,
-      samples$C2_mds_u,
-      purrr::map_chr(samples$Group_Name, (\(x) x[[1]])),
-      samples %>% update_coords("field_x", "field_y")
-  ),
-  file = "~/Desktop/samples.csv",
-  col_names = FALSE
+  x = composite_dataset,
+  file = "~/agora/mobest-hs/assets/data/samples.csv",
+  col_names = TRUE
 )
 
 sf::read_sf("~/Downloads/ne_50m_coastline/ne_50m_coastline.shp") %>%
